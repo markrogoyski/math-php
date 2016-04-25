3c9d9fa7dc143987164780d50537f6c4f5f1f06d
Mark Rogoyski <markrogoyski@gmail.com>
2016-04-25 02:22:50 +0000

    Add unit tests for relative frequency distribution.

--- tests/Statistics/DistributionTest.php
+++ tests/Statistics/DistributionTest.php
@@ -25,7 +25,26 @@ class DistributionTest extends \PHPUnit_Framework_TestCase {
         [ 'agree', 'disagree', 'agree', 'agree', 'no opinion', 'agree', 'disagree' ],
         [ 'agree' => 4, 'disagree' => 2, 'no opinion' => 1 ],
       ],
+    ];
+  }
 
+  /**
+   * @dataProvider dataProviderForRelativeFrequency
+   */
+  public function testRelativeFrequency( array $values, array $frequencies ) {
+    $this->assertEquals( $frequencies, Distribution::relativeFrequency($values), '', 0.0001 );
+  }
+
+  public function dataProviderForRelativeFrequency() {
+    return [
+      [
+        [ 'A', 'A', 'B', 'B', 'B', 'B', 'C', 'C', 'D', 'F' ],
+        [ 'A' => 0.2, 'B' => 0.4, 'C' => 0.2, 'D' => 0.1, 'F' => 0.1 ],
+      ],
+      [
+        [ 1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+        [ 1 => 0.16129032, 2 => 0.09677419, 3 => 0.29032258, 4 => 0.45161290 ],
+      ],
     ];
   }
 }
