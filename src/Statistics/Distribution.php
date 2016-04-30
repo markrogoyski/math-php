0f38e9374d86180094183bb96a9019914850fe06
Mark Rogoyski <markrogoyski@gmail.com>
2016-04-30 05:26:54 +0000

    Add cumulative frequency and cumulative relative frequency distributions.

--- src/Statistics/Distribution.php
+++ src/Statistics/Distribution.php
@@ -48,4 +48,42 @@ class Distribution {
     }
     return $relative_frequencies;
   }
+
+  /**
+   * Cumulative frequency distribution
+   *
+   * The values of the input array will be the keys of the result array.
+   * The cumulative frequency of the values will be the value of the result array for that key.
+   *
+   * @param array $values Ex: ( A, A, A, A, A, A, B, B, B, C )
+   * @return array cumulative frequency distribution Ex: ( A => 6, B => 9, C => 10 )
+   */
+  public static function cumulativeFrequency( array $values ): array {
+    $running_total          = 0;
+    $cumulative_frequencies = array();
+    foreach ( self::frequency($values) as $value => $frequency ) {
+      $running_total += $frequency;
+      $cumulative_frequencies[$value] = $running_total;
+    }
+    return $cumulative_frequencies;
+  }
+
+  /**
+   * Cumulative relative frequency distribution
+   * Cumulative frequency distribution relative to the sample size.
+   *
+   * The values of the input array will be the keys of the result array.
+   * The cumulative frequency of the values will be the value of the result array for that key.
+   *
+   * @param array $values Ex: ( A, A, A, A, A, A, B, B, B, C )
+   * @return array cumulative relative frequency distribution Ex: ( A => 0.6, B => 0.9, C => 1 )
+   */
+  public static function cumulativeRelativeFrequency( array $values ): array {
+    $sample_size            = count($values);
+    $cumulative_frequencies = self::cumulativeFrequency($values);
+    return array_map(
+      function($frequency) use ($sample_size) { return $frequency / $sample_size; },
+      $cumulative_frequencies
+    );
+  }
 }
