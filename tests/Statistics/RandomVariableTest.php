037a5aa9f9d86216b2925d55206e7e98d5274c1c
Mark Rogoyski <markrogoyski@gmail.com>
2016-05-15 02:22:55 +0000

    Add unit tests.

--- tests/Statistics/RandomVariableTest.php
+++ tests/Statistics/RandomVariableTest.php
@@ -216,6 +216,35 @@ class RandomVariableTest extends \PHPUnit_Framework_TestCase {
       [ 2, 0.9953222650189527341621 ],
       [ 3.4, 0.9999984780066371377146 ],
       [ 0.154, 0.1724063976196591819236 ],
+      [ -2.31, -0.9989124231037000500402 ],
+      [ -1.034, -0.856340111375020118952 ],
+    ];
+  }
+
+  /**
+   * @dataProvider dataProviderForComplementaryErrorFunction
+   */
+  public function testComplementaryErrorFunction( $x, $error ) {
+    $this->assertEquals( $error, RandomVariable::complementaryErrorFunction($x), '', 0.0001 );
+  }
+
+  /**
+   * @dataProvider dataProviderForComplementaryErrorFunction
+   */
+  public function testErfc( $x, $error ) {
+    $this->assertEquals( $error, RandomVariable::complementaryErrorFunction($x), '', 0.0001 );
+  }
+
+  public function dataProviderForComplementaryErrorFunction() {
+    return [
+      [ 0, 1 ],
+      [ 1, 0.1572992070502851306588 ],
+      [ -1, 1.842700792949714869341 ],
+      [ 2, 0.004677734981047265837931 ],
+      [ 3.4, 1.521993362862285361757E-6 ],
+      [ 0.154, 0.8275936023803408180764 ],
+      [ -2.31, 1.99891242310370005004 ],
+      [ -1.034, 1.856340111375020118952 ],
     ];
   }
 }
