b113099c2013441fc26101eeb5a417e44d25d99d
Mark Rogoyski <markrogoyski@gmail.com>
2016-05-15 07:08:02 +0000

    Update Z score tables.

--- tests/Probability/StandardNormalTableTest.php
+++ tests/Probability/StandardNormalTableTest.php
@@ -5,47 +5,20 @@ require_once( __DIR__ . '/../../src/Probability/StandardNormalTable.php' );
 class StandardNormalTableTest extends \PHPUnit_Framework_TestCase {
 
   /**
-   * @dataProvider dataProviderForGetCumulativeFromMean
+   * @dataProvider dataProviderForZScores
    */
-  public function testGetCumulativeFromMean( $Z, $Φ ) {
-    $this->assertEquals( $Φ, StandardNormalTable::getCumulativeFromMean($Z) );
+  public function testGetZScore( $Z, $Φ ) {
+    $this->assertEquals( $Φ, StandardNormalTable::getZScore($Z), '', 0.0001 );
   }
 
-  public function dataProviderForGetCumulativeFromMean() {
+  public function dataProviderForZScores() {
     return [
-      [ 0, 0.00000 ], [ 0.01, 0.00399 ], [ 0.02, 0.00798 ],
-      [ 0.30, 0.11791 ], [ 0.31, 0.12172 ], [ 0.39, 0.15173 ],
-      [ 2.90, 0.49813 ], [ 2.96, 0.49846 ], [ 3.09, 0.49900 ],
-    ];
-  }
-
-  /**
-   * @dataProvider dataProviderForGetCumulative
-   */
-  public function testGetCumulative( $Z, $Φ ) {
-    $this->assertEquals( $Φ, StandardNormalTable::getCumulative($Z) );
-  }
-
-  public function dataProviderForGetCumulative() {
-    return [
-      [ 0, 0.50000 ], [ 0.01, 0.50399 ], [ 0.02, 0.50798 ],
-      [ 0.30, 0.61791 ], [ 0.31, 0.62172 ], [ 0.39, 0.65173 ],
-      [ 2.90, 0.99813 ], [ 2.96, 0.99846 ], [ 3.09, 0.99900 ],
-    ];
-  }
-
-  /**
-   * @dataProvider dataProviderForGetComplementaryCumulative
-   */
-  public function testGetComplementaryCumulative( $Z, $Φ ) {
-    $this->assertEquals( $Φ, StandardNormalTable::getComplementaryCumulative($Z) );
-  }
-
-  public function dataProviderForGetComplementaryCumulative() {
-    return [
-      [ 0, 0.50000 ], [ 0.01, 0.49601 ], [ 0.02, 0.49202 ],
-      [ 0.30, 0.38209 ], [ 0.31, 0.37828 ], [ 0.39, 0.34827 ],
-      [ 2.90, 0.00187 ], [ 2.96, 0.00154 ], [ 3.09, 0.00100 ],
+      [ 0, 0.5000 ], [ 0.01, 0.5040 ], [ 0.02, 0.5080 ],
+      [ 0.30, 0.6179 ], [ 0.31, 0.6217 ], [ 0.39, 0.6517 ],
+      [ 2.90, 0.9981 ], [ 2.96, 0.9985 ], [ 3.09, 0.9990 ],
+      [ -0, 0.5000 ], [ -0.01, 0.4960 ], [ -0.02, 0.4920 ],
+      [ -0.30, 0.3821 ], [ -0.31, 0.3783 ], [ -0.39, 0.3483 ],
+      [ -2.90, 0.0019 ], [ -2.96, 0.0015 ], [ -3.09, 0.0010 ],
     ];
   }
 }
