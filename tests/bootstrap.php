ca059556d0a57606b63142fcdf611190f755c5f5
Mark Rogoyski <markrogoyski@gmail.com>
2016-05-16 05:50:36 +0000

    Fix test bootstrap include path.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,6 @@
 
 if ( file_exists( __DIR__ . '/../vendor/autoload.php' ) ) {
   require_once( __DIR__ . '/../vendor/autoload.php' );
-} else if ( file_exists( __DIR__ . '/../../autoload.php' ) ) {
-  require_once( __DIR__ . '/../../autoload.php' );
+} else if ( file_exists( __DIR__ . '/../../../autoload.php' ) ) {
+  require_once( __DIR__ . '/../../../autoload.php' );
 }
