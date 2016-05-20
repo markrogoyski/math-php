285fb93c14e39149dde059f199f884fde24d3784
Mark Rogoyski <markrogoyski@gmail.com>
2016-05-20 06:05:21 +0000

    Add gamma function.

--- src/Arithmetic/Functions.php
+++ src/Arithmetic/Functions.php
@@ -1,6 +1,8 @@
 <?php
 namespace Math\Arithmetic;
 
+use Math\Probability\Combinatorics;
+
 class Functions {
 
   /**
@@ -28,5 +30,42 @@ class Functions {
    */
   public static function sgn( float $x ): int {
     return self::signum($x);
+  }
+
+  /**
+   * Gamma function
+   * https://en.wikipedia.org/wiki/Gamma_function
+   *
+   * For postive integers:
+   *  Γ(n) = (n - 1)!
+   *
+   * For positive real numbers -- approximation:
+   *                   ___
+   *         __       / 1  /         1      \ n
+   *  Γ(n)≈ √2π ℯ⁻ⁿ  /  - | n + ----------- | 
+   *                √   n  \    12n - 1/10n /
+   *
+   * https://www.wolframalpha.com/input/?i=Gamma(n)&lk=3
+   *
+   * @param number $n
+   * @return number
+   */
+  public static function gamma($n) {
+    if ( $n == 0 ) {
+      return \INF;
+    }
+    if ( is_int($n) && $n < 0 ) {
+      return -\INF;
+    }
+    if ( is_int($n) && $n > 0 ) {
+      return Combinatorics::factorial( $n - 1 );
+    }
+
+    $√2π                    = sqrt( 2 * \M_PI );
+    $ℯ⁻ⁿ                    = pow( \M_E, -$n );
+    $√1／n                  = sqrt( 1 / $n );
+    $⟮n ＋ 1／⟮12n − 1／10n⟯⟯ⁿ = pow( $n + 1 / (12*$n - 1/(10*$n)), $n );
+
+    return $√2π * $ℯ⁻ⁿ * $√1／n * $⟮n ＋ 1／⟮12n − 1／10n⟯⟯ⁿ;
   }
 }
