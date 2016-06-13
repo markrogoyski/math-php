d0e17f53cb46be7d5c8fd72b2b6acaf73b68f903
Mark Rogoyski <markrogoyski@gmail.com>
2016-06-13 06:18:48 +0000

    Add pareto distribution.

--- src/Probability/Distribution.php
+++ src/Probability/Distribution.php
@@ -374,4 +374,55 @@ class Distribution
 
         return 1/2 + 1/2 * RandomVariable::erf($⟮ln x − μ⟯ / $√2σ);
     }
+
+    /**
+     * Pareto distribution - probability density function
+     *
+     * https://en.wikipedia.org/wiki/Pareto_distribution
+     *
+     *          abᵃ
+     * P(x) =  ----  for x ≥ b
+     *         xᵃ⁺¹
+     *
+     * P(x) = 0      for x < b
+     *
+     * @param  number $a shape parameter
+     * @param  number $b scale parameter
+     * @param  number $x
+     */
+    public static function paretoPDF($a, $b, $x)
+    {
+        if ($x < $b) {
+            return 0;
+        }
+
+        $abᵃ  = $a * $b**$a;
+        $xᵃ⁺¹ = pow($x, $a + 1);
+
+        return $abᵃ / $xᵃ⁺¹;
+    }
+
+    /**
+     * Pareto distribution - cumulative distribution function
+     *
+     * https://en.wikipedia.org/wiki/Pareto_distribution
+     *
+     *             / b \ᵃ
+     * D(x) = 1 - |  -  | for x ≥ b
+     *             \ x /
+     *
+     * D(x) = 0           for x < b
+     *
+     * @param  number $a shape parameter
+     * @param  number $b scale parameter
+     * @param  number $x
+     */
+    public static function paretoCDF($a, $b, $x)
+    {
+        if ($x < $b) {
+            return 0;
+        }
+
+        return 1 - pow($b / $x, $a);
+    }
 }
