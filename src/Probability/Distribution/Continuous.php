c13e313611964a90492f3766512f333fb1a5b434
Mark Rogoyski <markrogoyski@gmail.com>
2016-06-14 04:40:47 +0000

    Refactor.

--- src/Probability/Distribution/Continuous.php
+++ src/Probability/Distribution/Continuous.php
@@ -7,6 +7,30 @@ use Math\Statistics\RandomVariable;
 class Continuous
 {
     /**
+     * Continuous uniform distribution
+     * Computes the probability of a specific interval within the continuous uniform distribution.
+     * https://en.wikipedia.org/wiki/Uniform_distribution_(continuous)
+     *
+     * x₂ − x₁
+     * -------
+     *  b − a
+     *
+     * @param number $a lower boundary of the distribution
+     * @param number $b upper boundary of the distribution
+     * @param number $x₁ lower boundary of the probability interval
+     * @param number $x₂ upper boundary of the probability interval
+     *
+     * @return number probability of specific interval
+     */
+    public static function continuousUniform(float $a, float $b, float $x₁, float $x₂)
+    {
+        if (( $x₁ < $a || $x₁ > $b ) || ( $x₂ < $a || $x₂ > $b )) {
+            throw new \Exception('x values are outside of the distribution.');
+        }
+        return ( $x₂ - $x₁ ) / ( $b - $a );
+    }
+
+    /**
      * Exponential distribution - probability density function
      * https://en.wikipedia.org/wiki/Exponential_distribution
      *
