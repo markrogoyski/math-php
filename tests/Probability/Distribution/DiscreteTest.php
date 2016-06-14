0c3d7bfa9154de3edacf4f643c248eca6e466f3d
Mark Rogoyski <markrogoyski@gmail.com>
2016-06-14 05:55:46 +0000

    Add unit tests.

--- tests/Probability/Distribution/DiscreteTest.php
+++ tests/Probability/Distribution/DiscreteTest.php
@@ -201,4 +201,36 @@ class DiscreteTest extends \PHPUnit_Framework_TestCase
         $this->setExpectedException('\Exception');
         Discrete::poissonPMF(2, -1);
     }
+
+    /**
+     * @dataProvider dataProviderForMultinomialPMF
+     */
+    public function testMultinomialPMF(array $frequencies, array $probabilities, $pmf)
+    {
+        $this->assertEquals($pmf, Discrete::multinomialPMF($frequencies, $probabilities), '', 0.001);
+    }
+
+    public function dataProviderForMultinomialPMF()
+    {
+        return [
+            [ [1, 1], [0.5, 0.5], 0.5 ],
+            [ [1, 1], [0.4, 0.6], 0.48 ],
+            [ [7, 2, 3], [0.40, 0.35, 0.25], 0.0248 ],
+            [ [1, 2, 3], [0.2, 0.3, 0.5], 0.135 ],
+            [ [2, 3, 3, 2], [0.25, 0.25, 0.25, 0.25], 0.024 ],
+            [ [5, 2], [0.4, 0.6], 0.07741440000000005 ],
+        ];
+    }
+
+    public function testMultinomialPMFExceptionCountFrequenciesAndProbabilitiesDoNotMatch()
+    {
+        $this->setExpectedException('\Exception');
+        Discrete::multinomialPMF([1, 2,3], [0.3, 0.4, 0.2, 0.1]);
+    }
+
+    public function testMultinomialPMFExceptionProbabilitiesDoNotAddUpToOne()
+    {
+        $this->setExpectedException('\Exception');
+        Discrete::multinomialPMF([1, 2,3], [0.3, 0.2, 0.1]);
+    }
 }
