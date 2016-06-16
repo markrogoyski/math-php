9fb97ae341373766ab1b4f0cad6f8b369e7ec500
Mark Rogoyski <markrogoyski@gmail.com>
2016-06-16 05:52:15 +0000

    Add geometric shifted CDF.

--- src/Probability/Distribution/Discrete.php
+++ src/Probability/Distribution/Discrete.php
@@ -219,4 +219,33 @@ class Discrete
         $⟮1 − p⟯ᵏ⁻¹ = pow(1 - $p, $k - 1);
         return $⟮1 − p⟯ᵏ⁻¹ * $p;
     }
+
+    /**
+     * Shifted geometric distribution - cumulative distribution function
+     * 
+     * The probability distribution of the number X of Bernoulli trials needed
+     * to get one success, supported on the set { 1, 2, 3, ...}
+     * https://en.wikipedia.org/wiki/Geometric_distribution
+     * 
+     * k trials where k ∈ {1, 2, 3, ...}
+     *
+     * pmf = 1 - (1 - p)ᵏ
+     *
+     * @param  int   $k number of trials     k ≥ 1
+     * @param  float $p success probability  0 < p ≤ 1
+     *
+     * @return float
+     */
+    public static function geometricShiftedCDF(int $k, float $p): float
+    {
+        if ($k < 1) {
+            throw new \Exception('k must be an int ≥ 1');
+        }
+        if ($p <= 0 || $p > 1) {
+            throw new \Exception('p must be 0 < p ≤ 1');
+        }
+
+        $⟮1 − p⟯ᵏ = pow(1 - $p, $k);
+        return 1 - $⟮1 − p⟯ᵏ;
+    }
 }
