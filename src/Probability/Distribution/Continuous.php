c4a28e6a44c0ba72a1ad697e0afb3e365d7b1af3
Mark Rogoyski <markrogoyski@gmail.com>
2016-06-17 05:00:18 +0000

    Add Laplace distribution.

--- src/Probability/Distribution/Continuous.php
+++ src/Probability/Distribution/Continuous.php
@@ -362,4 +362,92 @@ class Continuous
 
         return exp(-pow($x / $λ, $k));
     }
+
+    /**
+     * Laplace distribution - probability density function
+     *
+     * https://en.wikipedia.org/wiki/Laplace_distribution
+     *
+     *            1      /  |x - μ| \
+     * f(x|μ,b) = -- exp| - -------  |
+     *            2b     \     b    /
+     *
+     * @param  number $μ location parameter
+     * @param  number $b scale parameter (diversity)  b > 0
+     * @param  number $x
+     *
+     * @return  float
+     */
+    public static function laplacePDF($μ, $b, $x): float
+    {
+        if ($b <= 0) {
+            throw new \Exception('b must be > 0');
+        }
+
+        return (1 / (2 * $b)) * exp(-( abs($x - $μ)/$b ));
+    }
+
+    /**
+     * Laplace distribution - lower cumulative distribution function
+     *
+     * https://en.wikipedia.org/wiki/Laplace_distribution
+     *
+     *        1     / x - μ \
+     * F(x) = - exp|  ------ |       if x < μ
+     *        2     \   b   /
+     *
+     *            1     /  x - μ \
+     * F(x) = 1 - - exp| - ------ |  if x ≥ μ
+     *            2     \    b   /
+     *
+     * @param  number $μ location parameter
+     * @param  number $b scale parameter (diversity)  b > 0
+     * @param  number $x
+     *
+     * @return  float
+     */
+    public static function laplaceLowerCDF($μ, $b, $x): float
+    {
+        if ($b <= 0) {
+            throw new \Exception('b must be > 0');
+        }
+
+        if ($x < $μ) {
+            return (1/2) * exp(($x - $μ) / $b);
+        }
+
+        return 1 - (1/2) * exp(-($x - $μ) / $b);
+    }
+
+    /**
+     * Laplace distribution - upper cumulative distribution function
+     *
+     * https://en.wikipedia.org/wiki/Laplace_distribution
+     *
+     *           /  1     / x - μ \ \
+     * F(x) = 1 | - - exp|  ------ | |       if x < μ
+     *           \  2     \   b   / /
+     *
+     *           /      1     /  x - μ \ \
+     * F(x) = 1 | - 1 - - exp| - ------ | |  if x ≥ μ
+     *           \      2     \    b   / /
+     *
+     * @param  number $μ location parameter
+     * @param  number $b scale parameter (diversity)  b > 0
+     * @param  number $x
+     *
+     * @return  float
+     */
+    public static function laplaceUpperCDF($μ, $b, $x): float
+    {
+        if ($b <= 0) {
+            throw new \Exception('b must be > 0');
+        }
+
+        if ($x < $μ) {
+            return 1 - ((1/2) * exp(($x - $μ) / $b));
+        }
+
+        return 1 - (1 - (1/2) * exp(-($x - $μ) / $b));
+    }
 }
