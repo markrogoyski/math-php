f3d8573547de2aec3f43d246e8cd0de67ff19549
Mark Rogoyski <markrogoyski@gmail.com>
2016-06-30 04:18:03 +0000

    Add logistic distribution.

--- src/Probability/Distribution/Continuous.php
+++ src/Probability/Distribution/Continuous.php
@@ -450,4 +450,90 @@ class Continuous
 
         return 1 - (1 - (1/2) * exp(-($x - $μ) / $b));
     }
+
+    /**
+     * Logistic distribution - probability density function
+     *
+     * https://en.wikipedia.org/wiki/Logistic_distribution
+     *
+     *                     /  x - μ \
+     *                 exp| - -----  |
+     *                     \    s   /
+     * f(x; μ, s) = -----------------------
+     *                /        /  x - μ \ \ ²
+     *              s| 1 + exp| - -----  | |
+     *                \        \    s   / /
+     *
+     * @param number $μ location parameter
+     * @param number $s scale parameter > 0
+     * @param number $x
+     *
+     * @return float
+     */
+    public static function logisticPDF($μ, $s, $x)
+    {
+        if ($s <= 0) {
+            throw new \Exception('Scale parameter s must be > 0');
+        }
+
+        $ℯ＾⁻⁽x⁻μ⁾／s = exp(-($x - $μ) / $s);
+
+        return $ℯ＾⁻⁽x⁻μ⁾／s / ($s * pow(1 + $ℯ＾⁻⁽x⁻μ⁾／s, 2));
+    }
+
+    /**
+     * Logistic distribution - lower cumulative distribution function
+     *
+     * https://en.wikipedia.org/wiki/Logistic_distribution
+     *
+     *                      1
+     * f(x; μ, s) = -------------------
+     *                      /  x - μ \ 
+     *              1 + exp| - -----  |
+     *                      \    s   /
+     *
+     * @param number $μ location parameter
+     * @param number $s scale parameter
+     * @param number $x
+     *
+     * @return float
+     */
+    public static function logisticLowerCDF($μ, $s, $x)
+    {
+        if ($s <= 0) {
+            throw new \Exception('Scale parameter s must be > 0');
+        }
+
+        $ℯ＾⁻⁽x⁻μ⁾／s = exp(-($x - $μ) / $s);
+
+        return 1 / (1 + $ℯ＾⁻⁽x⁻μ⁾／s);
+    }
+
+    /**
+     * Logistic distribution - upper cumulative distribution function
+     *
+     * https://en.wikipedia.org/wiki/Logistic_distribution
+     *
+     *                         1
+     * f(x; μ, s) = 1 - -------------------
+     *                         /  x - μ \ 
+     *                 1 + exp| - -----  |
+     *                         \    s   /
+     *
+     * @param number $μ location parameter
+     * @param number $s scale parameter
+     * @param number $x
+     *
+     * @return float
+     */
+    public static function logisticUpperCDF($μ, $s, $x)
+    {
+        if ($s <= 0) {
+            throw new \Exception('Scale parameter s must be > 0');
+        }
+
+        $ℯ＾⁻⁽x⁻μ⁾／s = exp(-($x - $μ) / $s);
+
+        return 1 - (1 / (1 + $ℯ＾⁻⁽x⁻μ⁾／s));
+    }
 }
