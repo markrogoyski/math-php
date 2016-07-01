ac3fcbf95a1ecfb9625e86aadb63914f5ef14ffd
Mark Rogoyski <markrogoyski@gmail.com>
2016-07-01 06:17:45 +0000

    Add beta distribution.

--- src/Probability/Distribution/Continuous.php
+++ src/Probability/Distribution/Continuous.php
@@ -3,6 +3,7 @@ namespace Math\Probability\Distribution;
 
 use Math\Probability\Combinatorics;
 use Math\Statistics\RandomVariable;
+use Math\Functions\Special;
 
 class Continuous
 {
@@ -553,5 +554,35 @@ class Continuous
         $⟮x／α⟯⁻ᵝ = pow($x / $α, -$β);
 
         return 1 / (1 + $⟮x／α⟯⁻ᵝ);
+    }
+
+    /**
+     * Beta distribution - probability density function
+     * https://en.wikipedia.org/wiki/Beta_distribution
+     *
+     *       xᵃ⁻¹(1 - x)ᵝ⁻¹
+     * pdf = --------------
+     *           B(α,β)
+     *
+     * @param number $α shape parameter α > 0
+     * @param number $β shape parameter β > 0
+     * @param number $x x ∈ (0,1)
+     *
+     * @return float
+     */
+    public static function betaPDF($α, $β, $x): float
+    {
+        if ($α <= 0 || $β <= 0) {
+            throw new \Exception('α and β must be > 0');
+        }
+        if ($x < 0 || $x > 1) {
+            throw new \Exception('x must be between 0 and 1');
+        }
+
+        $xᵃ⁻¹     = pow($x, $α - 1);
+        $⟮1 − x⟯ᵝ⁻¹ = pow(1 - $x, $β - 1);
+        $B⟮α、β⟯    = Special::beta($α, $β);
+
+        return ($xᵃ⁻¹ * $⟮1 − x⟯ᵝ⁻¹) / $B⟮α、β⟯;
     }
 }
