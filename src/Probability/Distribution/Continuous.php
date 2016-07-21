3e80f10eb7bdcb1d786d055cd3bb1e70cf097a14
Kevin Nowaczyk <beakerboy99@yahoo.com>
2016-07-21 14:55:46 -0400

    Removed Uniform PDF and CDF

--- src/Probability/Distribution/Continuous.php
+++ src/Probability/Distribution/Continuous.php
@@ -8,57 +8,6 @@ use Math\Functions\Special;
 class Continuous
 {
     /**
-     * Continuous uniform distribution - probability desnsity function
-     * https://en.wikipedia.org/wiki/Uniform_distribution_(continuous)
-     *
-     *         1
-     * pmf = -----  for a ≤ x ≤ b 
-     *       b - a
-     *
-     * pmf = 0      for x < a, x > b
-     *
-     * @param number $a lower boundary of the distribution
-     * @param number $b upper boundary of the distribution
-     * @param number $x percentile
-     */
-    public static function uniformPDF($a, $b, $x)
-    {
-        if ($x < $a || $x > $b) {
-            return 0;
-        }
-
-        return 1 / ($b - $a);
-    }
-    
-    /**
-     * Continuous uniform distribution - cumulative distribution function
-     * https://en.wikipedia.org/wiki/Uniform_distribution_(continuous)
-     *
-     * cdf = 0      for x < a
-     * 
-     *       x - a
-     * cdf = -----  for a ≤ x < b 
-     *       b - a
-     *
-     * cdf = 1      x ≥ b
-     *
-     * @param number $a lower boundary of the distribution
-     * @param number $b upper boundary of the distribution
-     * @param number $x percentile
-     */
-    public static function uniformCDF($a, $b, $x)
-    {
-        if ($x < $a) {
-            return 0;
-        }
-        if ($x >= $b) {
-            return 1;
-        }
-
-        return ($x - $a) / ($b - $a);
-    }
-
-    /**
      * Continuous uniform distribution - Interval
      * Computes the probability of a specific interval within the continuous uniform distribution.
      * https://en.wikipedia.org/wiki/Uniform_distribution_(continuous)
