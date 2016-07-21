4373cf1cce7a53d88e7c5d7b6dbd5d9b60fd4d9b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2016-07-21 15:28:14 -0400

    Moved LogLogistic to separate file

--- src/Probability/Distribution/Continuous.php
+++ src/Probability/Distribution/Continuous.php
@@ -32,55 +32,6 @@ class Continuous
     }
 
     /**
-     * Log-logistic distribution - probability density function
-     * Also known as the Fisk distribution.
-     * https://en.wikipedia.org/wiki/Log-logistic_distribution
-     *
-     *              (β/α)(x/α)ᵝ⁻¹
-     * f(x; α, β) = -------------
-     *              (1 + (x/α)ᵝ)²
-     *
-     * @param number $α scale parameter (α > 0)
-     * @param number $β shape parameter (β > 0)
-     * @param number $x (x > 0)
-     */
-    public static function logLogisticPDF($α, $β, $x)
-    {
-        if ($α <= 0 || $β <= 0 || $x <= 0) {
-            throw new \Exception('All parameters must be > 0');
-        }
-
-        $⟮β／α⟯⟮x／α⟯ᵝ⁻¹  = ($β / $α) * pow($x / $α, $β - 1);
-        $⟮1 ＋ ⟮x／α⟯ᵝ⟯² = pow(1 + ($x / $α)**$β, 2);
-
-        return $⟮β／α⟯⟮x／α⟯ᵝ⁻¹ / $⟮1 ＋ ⟮x／α⟯ᵝ⟯²;
-    }
-
-    /**
-     * Log-logistic distribution - cumulative distribution function
-     * Also known as the Fisk distribution.
-     * https://en.wikipedia.org/wiki/Log-logistic_distribution
-     *
-     *                   1
-     * F(x; α, β) = -----------
-     *              1 + (x/α)⁻ᵝ
-     *
-     * @param number $α scale parameter (α > 0)
-     * @param number $β shape parameter (β > 0)
-     * @param number $x (x > 0)
-     */
-    public static function logLogisticCDF($α, $β, $x)
-    {
-        if ($α <= 0 || $β <= 0 || $x <= 0) {
-            throw new \Exception('All parameters must be > 0');
-        }
-
-        $⟮x／α⟯⁻ᵝ = pow($x / $α, -$β);
-
-        return 1 / (1 + $⟮x／α⟯⁻ᵝ);
-    }
-
-    /**
      * Beta distribution - probability density function
      * https://en.wikipedia.org/wiki/Beta_distribution
      *
