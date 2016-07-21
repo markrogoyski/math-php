aad18dbb57abd72dd0246b70f8c162f4c8bd69dc
Kevin Nowaczyk <beakerboy99@yahoo.com>
2016-07-21 14:49:58 -0400

    Create FDistribution.php

---
+++ src/Probability/Distribution/FDistribution.php
@@ -0,0 +1,14 @@
+<?php
+namespace Math\Probability\Distribution;
+use Math\Functions\Special;
+
+class FDistribution extends Distribution {
+  public static function PDF($x, $d1, $d2) {
+    return sqrt( ($d1 * $x) ** $d1 * $d2 ** $d2/ ($d1 * $x + $d2) ** ($d1 + $d2)) / $x / Special::beta($d1 / 2, $d2 / 2);
+  }
+  
+  public static function CDF($x, $d1, $d2){
+    $beta_x = $d1 * $x / ($d1 * $x + $d2);
+    return Special::regularized_incomplete_beta($beta_x, $d1 / 2, $d2 / 2);
+  }
+}
