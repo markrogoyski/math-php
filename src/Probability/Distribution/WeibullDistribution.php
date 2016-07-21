d9cf0149fab86d82dcbbdf3075df7a5a9a83ff6c
Kevin Nowaczyk <beakerboy99@yahoo.com>
2016-07-21 15:20:26 -0400

    Create WeibullDistribution.php

---
+++ src/Probability/Distribution/WeibullDistribution.php
@@ -0,0 +1,52 @@
+<?php
+namespace math\Probability\Distribution
+
+class WeibullDistribution extends ContinuousNew{
+    /**
+     * Weibull distribution - probability density function
+     *
+     * https://en.wikipedia.org/wiki/Weibull_distribution
+     *
+     *        k  /x\ ᵏ⁻¹        ᵏ
+     * f(x) = - | - |    ℯ⁻⁽x/λ⁾   for x ≥ 0
+     *        λ  \λ/
+     *
+     * f(x) = 0                    for x < 0
+     *
+     * @param number $k shape parameter
+     * @param number $λ scale parameter
+     * @param number $x percentile (value to evaluate)
+     * @return float
+     */
+    public static function PDF($k, $λ, $x)
+    {
+        if ($x < 0) {
+            return 0;
+        }
+        $k／λ      = $k / $λ;
+        $⟮x／λ⟯ᵏ⁻¹  = pow($x / $λ, $k - 1);
+        $ℯ⁻⁽x／λ⁾ᵏ = exp(-pow($x / $λ, $k));
+        return $k／λ * $⟮x／λ⟯ᵏ⁻¹ * $ℯ⁻⁽x／λ⁾ᵏ;
+    }
+    /**
+     * Weibull distribution - cumulative distribution function
+     * From 0 to x (lower CDF)
+     * https://en.wikipedia.org/wiki/Weibull_distribution
+     *
+     * f(x) = 1 - ℯ⁻⁽x/λ⁾ for x ≥ 0
+     * f(x) = 0           for x < 0
+     *
+     * @param number $k shape parameter
+     * @param number $λ scale parameter
+     * @param number $x percentile (value to evaluate)
+     * @return float
+     */
+    public static function CDF($k, $λ, $x)
+    {
+        if ($x < 0) {
+            return 0;
+        }
+        $ℯ⁻⁽x／λ⁾ᵏ = exp(-pow($x / $λ, $k));
+        return 1 - $ℯ⁻⁽x／λ⁾ᵏ;
+    }
+}
