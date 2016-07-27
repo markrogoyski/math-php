bef9ff1f944f966ad53c9c5d361bf9e03378569a
Mark Rogoyski <markrogoyski@gmail.com>
2016-07-27 00:52:36 +0000

    Add unit tests for Map functions.

---
+++ tests/Functions/Map/MultiTest.php
@@ -0,0 +1,345 @@
+<?php
+namespace Math\Functions\Map;
+
+class MultiTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @dataProvider dataProviderForAddTwoArrays
+     */
+    public function testAddTwoArrays(array $xs, array $ys, array $sums)
+    {
+        $this->assertEquals($sums, Multi::add($xs, $ys));
+    }
+
+    public function dataProviderForAddTwoArrays()
+    {
+        return [
+            [
+                [1, 2, 3, 4],
+                [2, 3, 4, 5],
+                [3, 5, 7, 9],
+            ],
+            [
+                [1, 2, 3, 4],
+                [6, 6, 6, 6],
+                [7, 8, 9, 10],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderForAddMulti
+     */
+    public function testAddMulti(array $sums, array ...$arrays)
+    {
+        $this->assertEquals($sums, Multi::add(...$arrays));
+    }
+
+    public function dataProviderForAddMulti()
+    {
+        return [
+            [
+                [3, 5, 7, 9],
+                [1, 2, 3, 4],
+                [2, 3, 4, 5],
+                
+            ],
+            [
+                [7, 8, 9, 10],
+                [1, 2, 3, 4],
+                [6, 6, 6, 6],
+                
+            ],
+            [
+                [6, 7, 9, 10],
+                [1, 2, 3, 4],
+                [2, 2, 2, 2],
+                [3, 3, 4, 4],
+            ]
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderForSubtractTwoArrays
+     */
+    public function testSubtractTwoArrays(array $xs, array $ys, array $differences)
+    {
+        $this->assertEquals($differences, Multi::subtract($xs, $ys));
+    }
+
+    public function dataProviderForSubtractTwoArrays()
+    {
+        return [
+            [
+                [1, 2, 3, 4],
+                [2, 3, 4, 5],
+                [-1, -1, -1, -1],
+            ],
+            [
+                [1, 2, 3, 4],
+                [6, 6, 6, 6],
+                [-5, -4, -3, -2],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderForSubtractMulti
+     */
+    public function testSubtractMulti(array $differences, array ...$arrays)
+    {
+        $this->assertEquals($differences, Multi::subtract(...$arrays));
+    }
+
+    public function dataProviderForSubtractMulti()
+    {
+        return [
+            [
+                [-1, -1, -1, -1],
+                [1, 2, 3, 4],
+                [2, 3, 4, 5],
+                
+            ],
+            [
+                [-5, -4, -3, -2],
+                [1, 2, 3, 4],
+                [6, 6, 6, 6],
+                
+            ],
+            [
+                [3, 3, 4, 4],
+                [6, 7, 9, 10],
+                [1, 2, 3, 4],
+                [2, 2, 2, 2],
+            ]
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderForMultiplyTwoArrays
+     */
+    public function testMultiplyTwoArrays(array $xs, array $ys, array $products)
+    {
+        $this->assertEquals($products, Multi::multiply($xs, $ys));
+    }
+
+    public function dataProviderForMultiplyTwoArrays()
+    {
+        return [
+            [
+                [1, 2, 3, 4],
+                [2, 3, 4, 5],
+                [2, 6, 12, 20],
+            ],
+            [
+                [1, 2, 3, 4],
+                [6, 6, 6, 6],
+                [6, 12, 18, 24],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderForMultiplyMulti
+     */
+    public function testMultiplyMulti(array $differences, array ...$arrays)
+    {
+        $this->assertEquals($differences, Multi::multiply(...$arrays));
+    }
+
+    public function dataProviderForMultiplyMulti()
+    {
+        return [
+            [
+                [2, 6, 12, 20],
+                [1, 2, 3, 4],
+                [2, 3, 4, 5],
+                
+            ],
+            [
+                [6, 12, 18, 24],
+                [1, 2, 3, 4],
+                [6, 6, 6, 6],
+                
+            ],
+            [
+                [12, 28, 54, 80],
+                [6, 7, 9, 10],
+                [1, 2, 3, 4],
+                [2, 2, 2, 2],
+            ]
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderForDivideTwoArrays
+     */
+    public function testDivideTwoArrays(array $xs, array $ys, array $quotients)
+    {
+        $this->assertEquals($quotients, Multi::divide($xs, $ys));
+    }
+
+    public function dataProviderForDivideTwoArrays()
+    {
+        return [
+            [
+                [5, 10, 15, 20],
+                [5, 5, 5, 5],
+                [1, 2, 3, 4],
+            ],
+            [
+                [5, 10, 15, 20],
+                [2.5, 20, 3, 4],
+                [2, 0.5, 5, 5],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderForDivideMulti
+     */
+    public function testDivideMulti(array $quotients, array ...$arrays)
+    {
+        $this->assertEquals($quotients, Multi::divide(...$arrays));
+    }
+
+    public function dataProviderForDivideMulti()
+    {
+        return [
+            [
+                [1, 2, 3, 4],
+                [5, 10, 15, 20],
+                [5, 5, 5, 5],
+            ],
+            [
+                [2, 0.5, 5, 5],
+                [5, 10, 15, 20],
+                [2.5, 20, 3, 4],
+            ],
+            [
+                [5, 20, 1, 8],
+                [100, 80, 25, 64],
+                [10, 2, 5, 2],
+                [2, 2, 5, 4],
+            ]
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderForMaxTwoArrays
+     */
+    public function testMaxTwoArrays(array $xs, array $ys, array $maxes)
+    {
+        $this->assertEquals($maxes, Multi::max($xs, $ys));
+    }
+
+    public function dataProviderForMaxTwoArrays()
+    {
+        return [
+            [
+                [1, 5, 3, 6],
+                [5, 5, 5, 5],
+                [5, 5, 5, 6],
+            ],
+            [
+                [5, 10, 15, 20],
+                [2.5, 20, 3, 4],
+                [5, 20, 15, 20],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderForMaxMulti
+     */
+    public function testMaxMulti(array $maxes, array ...$arrays)
+    {
+        $this->assertEquals($maxes, Multi::max(...$arrays));
+    }
+
+    public function dataProviderForMaxMulti()
+    {
+        return [
+            [
+                [5, 10, 15, 20],
+                [5, 10, 15, 20],
+                [5, 5, 5, 5],
+            ],
+            [
+                [5, 20, 15, 20],
+                [5, 10, 15, 20],
+                [2.5, 20, 3, 4],
+            ],
+            [
+                [100, 80, 55, 664],
+                [100, 80, 25, 64],
+                [10, 2, 55, 2],
+                [2, 2, 5, 664],
+            ]
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderForMin
+     */
+    public function testMin(array $xs, array $ys, array $maxes)
+    {
+        $this->assertEquals($maxes, Multi::min($xs, $ys));
+    }
+
+    public function dataProviderForMin()
+    {
+        return [
+            [
+                [1, 5, 3, 6],
+                [5, 5, 5, 5],
+                [1, 5, 3, 5],
+            ],
+            [
+                [5, 10, 15, 20],
+                [2.5, 20, 3, 4],
+                [2.5, 10, 3, 4],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderForMinMulti
+     */
+    public function testMinMulti(array $mins, array ...$arrays)
+    {
+        $this->assertEquals($mins, Multi::min(...$arrays));
+    }
+
+    public function dataProviderForMinMulti()
+    {
+        return [
+            [
+                [5, 5, 5, 5],
+                [5, 10, 15, 20],
+                [5, 5, 5, 5],
+            ],
+            [
+                [2.5, 10, 3, 4],
+                [5, 10, 15, 20],
+                [2.5, 20, 3, 4],
+            ],
+            [
+                [2, 2, 5, 2],
+                [100, 80, 25, 64],
+                [10, 2, 55, 2],
+                [2, 2, 5, 664],
+            ]
+        ];
+    }
+
+    public function testCheckArrayLengthsException()
+    {
+        $xs = [1, 2, 3];
+        $ys = [1, 2];
+
+        $this->setExpectedException('\Exception');
+        Multi::add($xs, $ys);
+    }
+}
---
+++ tests/Functions/Map/SingleTest.php
@@ -0,0 +1,93 @@
+<?php
+namespace Math\Functions\Map;
+
+class SingleTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider dataProviderForSquare
+     */
+    public function testSquare(array $xs, array $squares)
+    {
+        $this->assertEquals($squares, Single::square($xs));
+    }
+
+    public function dataProviderForSquare()
+    {
+        return [
+            [
+                [1, 2, 3, 4],
+                [1, 4, 9, 16],
+            ],
+            [
+                [7, 8, 9, 10],
+                [49, 64, 81, 100],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderForCube
+     */
+    public function testCube(array $xs, array $cubes)
+    {
+        $this->assertEquals($cubes, Single::cube($xs));
+    }
+
+    public function dataProviderForCube()
+    {
+        return [
+            [
+                [1, 2, 3, 4],
+                [1, 8, 27, 64],
+            ],
+            [
+                [7, 8, 9, 10],
+                [343, 512, 729, 1000],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderForPow
+     */
+    public function testPow(array $xs, $n, array $pows)
+    {
+        $this->assertEquals($pows, Single::pow($xs, $n));
+    }
+
+    public function dataProviderForPow()
+    {
+        return [
+            [
+                [1, 2, 3, 4], 5,
+                [1, 32, 243, 1024],
+            ],
+            [
+                [7, 8, 9, 10], 4,
+                [2401, 4096, 6561, 10000],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderForSqrt
+     */
+    public function testSqrt(array $xs, array $sqrts)
+    {
+        $this->assertEquals($sqrts, Single::sqrt($xs));
+    }
+
+    public function dataProviderForSqrt()
+    {
+        return [
+            [
+                [4, 9, 16, 25],
+                [2, 3, 4, 5],
+            ],
+            [
+                [64, 81, 100, 144],
+                [8, 9, 10, 12],
+            ],
+        ];
+    }
+}
