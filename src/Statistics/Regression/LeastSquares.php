564a52d44edec967dfe9e819d00483bde33ba081
Mark Rogoyski <markrogoyski@gmail.com>
2016-08-03 03:49:38 +0000

    Refactor LeastSquares trait.

--- src/Statistics/Regression/LeastSquares.php
+++ src/Statistics/Regression/LeastSquares.php
@@ -7,25 +7,44 @@ use Math\Functions\Map\Multi;
 
 trait LeastSquares
 {
+    /**
+     * Linear least squares
+     *        _ _   __
+     *        x y - xy
+     *   m = _________
+     *        _     __
+     *       (x)² - x²
+     *
+     *       _    _
+     *   b = y - mx
+     *
+     * @param  array $ys y values
+     * @param  array $xs x values
+     *
+     * @todo Use matrix operations once Matrix class is completed
+     *       $X = new Matrix($xs);
+     *       $Y = new Matrix($ys);
+     *       $(XᵀX)⁻¹Xᵀy = $X->transpose->mult($X)->inverse()->(mult($X)->transpose())->mult($y);
+     *       return $(XᵀX)⁻¹Xᵀy;
+     *
+     * @return array [m, b]
+     */
     function leastSquares($ys, $xs)
     {
         // Averages used in m (slope) calculation
         $x   = Average::mean($xs);
         $y   = Average::mean($ys);
         $xy  = Average::mean(Multi::multiply($ys, $xs));
-        $⟮x⟯² = $x ** 2;
+        $⟮x⟯² = $x**2;
         $x²  = Average::mean(Single::square($xs));
+
         // Calculate slope (m) and y intercept (b)
         $m = (($x * $y) - $xy) / ($⟮x⟯² - $x²);
         $b = $y - ($m * $x);
+
         return [
             'm' => $m,
             'b' => $b,
         ];
-        
-        // $X = new Matrix($xs);
-        // $Y = new Matrix($ys);
-        // $(XᵀX)⁻¹Xᵀy = $X->transpose->mult($X)->inverse()->(mult($X)->transpose())->mult($y);
-        // return $(XᵀX)⁻¹Xᵀy;
     }
 }
