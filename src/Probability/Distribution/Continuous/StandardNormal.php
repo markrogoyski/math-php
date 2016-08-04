41a5feea3e95d9cf0c9b5dc81b4ad9f765140292
Mark Rogoyski <markrogoyski@gmail.com>
2016-08-04 02:42:32 +0000

    Refactor StandardNormal distribution.

--- src/Probability/Distribution/Continuous/StandardNormal.php
+++ src/Probability/Distribution/Continuous/StandardNormal.php
@@ -1,15 +1,47 @@
 <?php
 namespace Math\Probability\Distribution\Continuous;
 
-class StandardNormal
+/**
+ * Standard normal distribution
+ * The simplest case of a normal distribution.
+ * This is a special case when μ = 0 and σ = 1,
+ */
+class StandardNormal extends Continuous
 {
+    /**
+     * Mean is always 0
+     * @var int
+     */
+    const μ = 0;
+
+    /**
+     * Standard deviation is always 1
+     * @var int
+     */
+    const σ = 1;
+
+    /**
+     * Probability density function
+     *
+     * @param number $x random variable
+     *
+     * @return float f(x|μ,σ)
+     */
     public static function PDF($z)
     {
-        return Normal::PDF($z, 0, 1);
+        return Normal::PDF($z, self::μ, self::σ);
     }
 
+    /**
+     * Cumulative distribution function
+     * P value for a z score.
+     *
+     * @param number $x random variable
+     *
+     * @return float f(x|μ,σ)
+     */
     public static function CDF($z)
     {
-        return Normal::CDF($z, 0, 1);
+        return Normal::CDF($z, self::μ, self::σ);
     }
 }
