0f873e12a0f41563d0fd79713758b027533ac13c
Kevin Nowaczyk <beakerboy99@yahoo.com>
2016-08-05 09:23:23 -0400

    Update Uniform.php

--- src/Probability/Distribution/Continuous/Uniform.php
+++ src/Probability/Distribution/Continuous/Uniform.php
@@ -51,4 +51,12 @@ class Uniform extends Continuous
         }
         return ($x - $a) / ($b - $a);
     }
+    
+        /**
+     * Returns the mean of the distribution
+     */
+    public static function getMean($a, $b, $x)
+    {
+        return ($a + $b) / 2;
+    }
 }
