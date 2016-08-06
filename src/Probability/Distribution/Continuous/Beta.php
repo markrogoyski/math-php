f32ac33973e2fe0dcbf834b245c41cd396a40fce
Kevin Nowaczyk <beakerboy99@yahoo.com>
2016-08-06 07:47:01 -0400

    Update Beta.php

--- src/Probability/Distribution/Continuous/Beta.php
+++ src/Probability/Distribution/Continuous/Beta.php
@@ -15,7 +15,7 @@ class Beta extends Continuous
      * (a,b] = a <  x <= b
      * [a,b] = a <= x <= b
      */ 
-    public static $distribution_limits = [
+    protected $distribution_limits = [
         [ // x ∈ (0,1)
             'parameter' => 'x',
             'lower_endpoint' => '(',
