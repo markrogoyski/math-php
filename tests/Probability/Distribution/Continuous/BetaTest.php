91a1285810dc7e10f743a151697b3eb0abfd4d36
Mark Rogoyski <markrogoyski@gmail.com>
2016-08-07 03:43:00 +0000

    Add unit tests for Beta distribution CDF and mean.

--- tests/Probability/Distribution/Continuous/BetaTest.php
+++ tests/Probability/Distribution/Continuous/BetaTest.php
@@ -57,4 +57,62 @@ class BetaTest extends \PHPUnit_Framework_TestCase
         list($α, $β, $x) = [1, 1, 4];
         Beta::PDF($α, $β, $x);
     }
+
+    /**
+     * @dataProvider dataProviderForCDF
+     */
+    public function testCDF($α, $β, $x, $cdf)
+    {
+        $this->assertEquals($cdf, Beta::CDF($α, $β, $x), '', 0.001);
+    }
+
+    public function dataProviderForCDF()
+    {
+        return [
+            [1, 1, 0, 0],
+            [1, 1, 0.01, 0.01],
+            [1, 1, 0.15, 0.15],
+            [1, 1, 0.81, 0.81],
+            [1, 1, 0.99, 0.99],
+            [1, 1, 1, 1],
+
+            [2, 3, 0, 0],
+            [2, 3, 0.01, 5.9203E-4],
+            [2, 3, 0.15, 0.10951875],
+            [2, 3, 0.81, 0.97647363],
+            [2, 3, 0.99, 0.99999603],
+            [2, 3, 1, 1],
+
+            [3, 2, 0, 0],
+            [3, 2, 0.01, 3.97E-6],
+            [3, 2, 0.15, 0.01198125],
+            [3, 2, 0.81, 0.83436237],
+            [3, 2, 0.99, 0.99940797],
+            [3, 2, 1, 1],
+
+            [10, 50, 0, 0],
+            [10, 50, 0.01, 4.0195552E-10],
+            [10, 50, 0.15, 0.39087049],
+            [10, 50, 0.81, 1],
+            [10, 50, 0.99, 1],
+            [10, 50, 1, 1],
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderForMean
+     */
+    public function testMean($α, $β, $μ)
+    {
+        $this->assertEquals($μ, Beta::mean($α, $β), '', 0.0001);
+    }
+
+    public function dataProviderForMean()
+    {
+        return [
+            [1, 1, 0.5],
+            [1, 2, 0.3333],
+            [2, 1, 0.6666],
+        ];
+    }
 }
