d4f76e4594089f25707e661e2005b272dc585454
Kevin Nowaczyk <beakerboy99@yahoo.com>
2016-08-08 10:10:37 -0400

    Added T and F statistics

--- src/Statistics/Regression/LeastSquares.php
+++ src/Statistics/Regression/LeastSquares.php
@@ -4,6 +4,8 @@ namespace Math\Statistics\Regression;
 use Math\Statistics\Average;
 use Math\Functions\Map\Single;
 use Math\Functions\Map\Multi;
+use Math\Probability\Distribution\Continuous\F;
+use Math\Probability\Distribution\Continuous\StudentT;
 
 trait LeastSquares
 {
@@ -65,4 +67,71 @@ trait LeastSquares
             'b' => $sb,
         ];
     }
+    
+    /**
+     * The t values associated with each of the regression parameters
+     * 
+     * t = β / se
+     */
+    function TValues()
+    {
+        $se = this->standardErrors();
+        return [$this->m / $se['m'], $this->b / $se['b']];
+    }
+    
+    /**
+     * The probabilty associated with each parameter's t value
+     */
+    function TProbability()
+    {
+        // Degrees of Freedom.
+        $df = $this->n - 2;
+        
+        $ts = array_map(
+            function ($t) use ($df){
+                return StudentT::CDF($t, $df);
+            },
+            $this->TValues
+        );
+        return $ts;
+    }
+    
+    /**
+     * The F statistic of the regression
+     */
+    function FStatistic()
+    {
+        // The number of regression parameters.
+        $p = 2;
+        $n = $this->n;
+        
+        $SSr = $this->sumOfSquaresRegression();
+        $SSe = $this->sumOfSquaresResidual();
+        
+        // Mean of Squares for model.
+        $msm = $SSR / ($p - 1);
+        
+        // Mean of Squares for Error
+        $mse = $SSe / ($n - $p);
+        
+        $F = $msm / $mse;
+        
+        return $F;
+    }
+    
+    /**
+     * The probabilty associated with the regression F Statistic
+     */
+    function FProbability()
+    {
+        $F = $this->FStatistic();
+        $n = $this->n;
+        // Degrees of Freedom.
+        $df = $this->n - 2;
+        
+        $v1 = $n - $df - 1;
+        $v2 = $df;
+        return (F::CDF($F, $v1, $v2));
+    }
+    
 }
