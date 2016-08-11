b29c84b7734e6463b8cd16d82ce5ea27f6ce9a37
Kevin Nowaczyk <beakerboy99@yahoo.com>
2016-08-11 11:02:12 -0400

    Create LineweaverBurk.php

---
+++ src/Statistics/Regression/LineweaverBurk.php
@@ -0,0 +1,67 @@
+<?php
+namespace Math\Statistics\Regression;
+
+use Math\Functions\Map\Single;
+
+/**
+ * Use the Lineweaver-Burk method to fit an equation of the form
+ *       V * x
+ * y = ----------
+ *       K + x
+ *
+ * The equation is linearized and fit using Least Squares
+ */
+class LineweaverBurk extends Regression
+{
+    use LeastSquares;
+    /**
+     * Calculate the regression parameters by least squares on linearized data
+     * y⁻¹ = K * V⁻¹ * x⁻¹ + V⁻¹
+     */
+    public function calculate()
+    {
+        // Linearize the relationship by taking the inverse of both x and y
+        $x’ = Single::pow($this->xs, -1);
+        $y’ = Single::pow($this->ys, -1);
+        
+        // Perform Least Squares Fit
+        $parameters = $this->leastSquares($y’, $x’);
+        
+        // Translate the linearized parameters back.
+        $this->V = 1 / parameters['b'];
+        $this->K = $parameters['m'] * $this->V;
+    }
+    /**
+     * Get regression parameters (V and K)
+     *
+     * @return array [ V => number, K => number ]
+     */
+    public function getParameters(): array
+    {
+        return [
+            'V' => $this->V,
+            'K' => $this->K,
+        ];
+    }
+    /**
+     * Get regression equation (y = V * X / (K + X))
+     *
+     * @return string
+     */
+    public function getEquation(): string
+    {
+        return sprintf('y = %fx/(%f+x)', $this->V, $this->K);
+    }
+   /**
+    * Evaluate the power curve equation from power law regression parameters for a value of x
+    * y = V * X / (K + X)
+    *
+    * @param number $x
+    *
+    * @return number y evaluated
+    */
+    public function evaluate($x)
+    {
+        return $this->V * $x / ($this->K + $x);
+    }
+}
