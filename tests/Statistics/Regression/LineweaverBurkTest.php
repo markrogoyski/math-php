20c303be1c15d112eda6079446e854b7b8f357f0
Kevin Nowaczyk <beakerboy99@yahoo.com>
2016-08-11 13:18:43 -0400

    Create LineweaverBurkTest.php

---
+++ tests/Statistics/Regression/LineweaverBurkTest.php
@@ -0,0 +1,43 @@
+<?php
+namespace Math\Statistics\Regression;
+class LineweaverBurkTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider dataProviderForEquation
+     * Equation matches pattern y = V * X / (K + x)
+     */
+    public function testGetEquation(array $points)
+    {
+        $regression = new LineweaverBurk($points);
+        $this->assertRegExp('/^y = \d+[.]\d+x\/\(\d+[.]\d+\+x$/', $regression->getEquation());
+    }
+    
+    public function dataProviderForEquation()
+    {
+        return [
+            [
+                [ [.038, .05], [.194, .127], [.425, .094], [.626, .2122], [1.253, .2729], [2.5, .2665], [3.740, .3317], 
+            ]
+        ];
+    }
+    
+    /**
+     * @dataProvider dataProviderForParameters
+     */
+    public function testGetParameters(array $points, $V, $K)
+    {
+        $regression = new LineweaverBurk($points);
+        $parameters = $regression->getParameters();
+        $this->assertEquals($a, $parameters['V'], '', 0.0001);
+        $this->assertEquals($b, $parameters['K'], '', 0.0001);
+    }
+    public function dataProviderForParameters()
+    {
+        return [
+            [
+                [ [.038, .05], [.194, .127], [.425, .094], [.626, .2122], [1.253, .2729], [2.5, .2665], [3.740, .3317],
+                .0.222903511, 0.13447224,
+            ],
+        ];
+    }
+}
