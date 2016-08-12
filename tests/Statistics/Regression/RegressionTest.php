15dc105f2667906e78b234af380d6e84041d9ba6
Mark Rogoyski <markrogoyski@gmail.com>
2016-08-12 00:35:48 +0000

    Add unit tests for Regression.

--- tests/Statistics/Regression/RegressionTest.php
+++ tests/Statistics/Regression/RegressionTest.php
@@ -89,4 +89,61 @@ class RegressionTest extends \PHPUnit_Framework_TestCase
 
         ];
     }
+
+    /**
+     * @dataProvider dataProviderForYHat()
+     */
+    public function testYHat(array $points, array $yhat)
+    {
+        $regression = new Linear($points);
+        $this->assertEquals($yhat, $regression->yHat());
+    }
+
+    public function dataProviderForYHat()
+    {
+        return [
+            [
+                [ [1,2], [2,3], [4,5], [5,7], [6,8] ], // m = 1.2209302325581, b = 0.60465116279069
+                [ 1.82558139534879, 3.04651162790689, 5.48837209302309, 6.70930232558119, 7.93023255813929] // evaluate y = mx + b
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderForSumOfSquaresRegression
+     */
+    public function testSumOfSquaresRegression(array $points, $SSreg)
+    {
+        $regression = new Linear($points);
+        $this->assertEquals($SSreg, $regression->sumOfSquaresRegression());
+    }
+
+    public function dataProviderForSumOfSquaresRegression()
+    {
+        return [
+            [
+                [ [1,2], [2,3], [4,5], [5,7], [6,8] ], // y mean = 5; yhat = [1.82558139534879, 3.04651162790689, 5.48837209302309, 6.70930232558119, 7.93023255813929]
+                25.63953488371927                      // 10.07693347755574 + 3.81611681990299 + 0.23850730124375 + 2.92171444023726 + 8.58626284477953
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderForSumOfSquaresResidual
+     */
+    public function testSumOfSquareResidual(array $points, $SSres)
+    {
+        $regression = new Linear($points);
+        $this->assertEquals($SSres, $regression->sumOfSquaresResidual());
+    }
+
+    public function dataProviderForSumOfSquaresResidual()
+    {
+        return [
+            [
+                [ [1,2], [2,3], [4,5], [5,7], [6,8] ], // yhat = [1.82558139534879, 3.04651162790689, 5.48837209302309, 6.70930232558119, 7.93023255813929]
+                0.36046511627907 // 0.03042184964848 + 0.00216333153055 + 0.23850730124375 + 0.0845051379125 + 0.00486749594379
+            ]
+        ];
+    }
 }
