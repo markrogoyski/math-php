1a1b448c017e2aa28ef4ad12eb1d2be51742b6fe
Kevin Nowaczyk <beakerboy99@yahoo.com>
2016-08-12 08:42:05 -0400

    moved shared functions to a parent

--- src/Statistics/Regression/HanesWoolf.php
+++ src/Statistics/Regression/HanesWoolf.php
@@ -1,6 +1,7 @@
 <?php
 namespace Math\Statistics\Regression;
 use Math\Functions\Map\Multi;
+
 /**
  * Use the Hanes-Woolf method to fit an equation of the form
  *       V * x
@@ -9,17 +10,10 @@ use Math\Functions\Map\Multi;
  *
  * The equation is linearized and fit using Least Squares
  */
-class LineweaverBurk extends Regression
+class HanesWoolf extends MichaelisMenten
 {
     use LeastSquares;
-    /**
-     * @var number V regression parameter
-     */
-    private $V;
-    /**
-     * @var number K regression parameter
-     */
-    private $K;
+
     /**
      * Calculate the regression parameters by least squares on linearized data
      * x / y = x / V + K / V
@@ -27,44 +21,11 @@ class LineweaverBurk extends Regression
     public function calculate()
     {
         // Linearize the relationship by dividing x by y
-        $y’ = Multi::disvide($this->xs, $this->ys);
+        $y’ = Multi::divide($this->xs, $this->ys);
         // Perform Least Squares Fit
         $parameters = $this->leastSquares($y’, $this->xs);
         // Translate the linearized parameters back.
         $this->V = 1 / $parameters['m'];
         $this->K = $parameters['b'] * $this->V;
-    }
-    /**
-     * Get regression parameters (V and K)
-     *
-     * @return array [ V => number, K => number ]
-     */
-    public function getParameters(): array
-    {
-        return [
-            'V' => $this->V,
-            'K' => $this->K,
-        ];
-    }
-    /**
-     * Get regression equation (y = V * X / (K + X))
-     *
-     * @return string
-     */
-    public function getEquation(): string
-    {
-        return sprintf('y = %fx/(%f+x)', $this->V, $this->K);
-    }
-   /**
-    * Evaluate the equation using the regression parameters
-    * y = V * X / (K + X)
-    *
-    * @param number $x
-    *
-    * @return number y evaluated
-    */
-    public function evaluate($x)
-    {
-        return ($this->V * $x) / ($this->K + $x);
     }
 }
