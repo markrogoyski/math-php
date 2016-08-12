840bc14295427d02897b53f1add395a0f974394a
Mark Rogoyski <markrogoyski@gmail.com>
2016-08-12 00:34:31 +0000

    Minor improvement to NegativeBinomial.

--- src/Probability/Distribution/Discrete/NegativeBinomial.php
+++ src/Probability/Distribution/Discrete/NegativeBinomial.php
@@ -2,43 +2,46 @@
 namespace Math\Probability\Distribution\Discrete;
 
 use Math\Probability\Combinatorics;
-use Math\Functions\Support;
+use Math\Functions\{Special, Support};
 
+/**
+ * Negative binomial distribution (Pascal distribution)
+ * https://en.wikipedia.org/wiki/Negative_binomial_distribution
+ */
 class NegativeBinomial extends Discrete
 {
     /**
      * Distribution parameter bounds limits
      * x ∈ [0,∞)
      * r ∈ [0,∞)
-     * P ∈ [0,1]
+     * p ∈ [0,1]
      * @var array
      */
     const LIMITS = [
         'x' => '[0,∞)',
         'r' => '[0,∞)',
-        'P' => '[0,1]',
+        'p' => '[0,1]',
     ];
 
     /**
-     * Negative binomial distribution (Pascal distribution) - probability mass function
-     * https://en.wikipedia.org/wiki/Negative_binomial_distribution
+     * Probability mass function
      *
-     * b(x; r, P) = ₓ₋₁Cᵣ₋₁ Pʳ * (1 - P)ˣ⁻ʳ
+     * b(x; r, p) = ₓ₋₁Cᵣ₋₁ pʳ * (1 - p)ˣ⁻ʳ
      *
      * @param  int   $x number of trials required to produce r successes
      * @param  int   $r number of successful events
-     * @param  float $P probability of success on an individual trial
+     * @param  float $p probability of success on an individual trial
      *
      * @return float
      */
-    public static function PMF(int $x, int $r, float $P): float
+    public static function PMF(int $x, int $r, float $p): float
     {
-        Support::checkLimits(self::LIMITS, ['x' => $x, 'r' => $r, 'P' => $P]);
+        Support::checkLimits(self::LIMITS, ['x' => $x, 'r' => $r, 'p' => $p]);
      
         $ₓ₋₁Cᵣ₋₁   = Combinatorics::combinations($x - 1, $r - 1);
-        $Pʳ        = pow($P, $r);
-        $⟮1 − P⟯ˣ⁻ʳ = pow(1 - $P, $x - $r);
+        $pʳ        = pow($p, $r);
+        $⟮1 − p⟯ˣ⁻ʳ = pow(1 - $p, $x - $r);
     
-        return $ₓ₋₁Cᵣ₋₁ * $Pʳ * $⟮1 − P⟯ˣ⁻ʳ;
+        return $ₓ₋₁Cᵣ₋₁ * $pʳ * $⟮1 − p⟯ˣ⁻ʳ;
     }
 }
