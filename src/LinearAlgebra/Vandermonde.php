6896f7131646755b2b78aab5f567621d62f8e969
Kevin Nowaczyk <beakerboy99@yahoo.com>
2016-08-14 13:41:38 -0400

    Create Vandermonde.php

---
+++ src/LinearAlgebra/Vandermonde.php
@@ -0,0 +1,26 @@
+<?php
+namespace Math\LinearAlgebra;
+
+class Vandermonde extends Matrix
+{
+    /**
+     * Create the Vandermonde Matrix from a simple array.
+     * 
+     * @param $M array (a,b,c,d,e)
+     * @param $n int
+     *
+     */
+    public function __construct(array $M, int $n)
+    {
+        $this->n = $n;
+        $this->m = count($M)
+        
+        $A = [];
+        foreach($M as $row=>$value){
+            for ($i=0;$i<$n; $i++){
+                $A[$row][$i] = $value ** $i;
+            }
+        }
+        $this->A = $A;
+    }
+}
