ea7afa79b92fd13d692371bf0734f742bbd8361f
Mark Rogoyski <markrogoyski@gmail.com>
2016-08-15 05:52:19 +0000

    Add comments and formating improvements to DiagonalMatrix.

--- src/LinearAlgebra/DiagonalMatrix.php
+++ src/LinearAlgebra/DiagonalMatrix.php
@@ -1,9 +1,13 @@
 <?php
 namespace Math\LinearAlgebra;
 
+/**
+ * Diagonal matrix
+ * Elements along the main diagonal are the only non-zero elements (may also be zero).
+ * The off-diagonal elements are all zero
+ */
 class DiagonalMatrix extends Matrix
 {
-
     /**
      * A Diagonal Matrix is constucted from a single-row array.
      * The elements of this array are placed on the diagonal of a
@@ -13,10 +17,11 @@ class DiagonalMatrix extends Matrix
     {
         $this->m = count($D);
         $this->n = $this->m;
+
         $A = [];
-        for($i = 0; $i < $this->m; $i++){
-            for($j = 0; $j < $this->m; $j++){
-                if ($i == $j){
+        for($i = 0; $i < $this->m; $i++) {
+            for($j = 0; $j < $this->m; $j++) {
+                if ($i == $j) {
                     $A[$i][$j] = $D[$i];
                 }
                 else {
