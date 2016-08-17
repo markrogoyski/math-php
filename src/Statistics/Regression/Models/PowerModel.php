a1472bf6de3552747fb54174d22ef2d40001d276
Kevin Nowaczyk <beakerboy99@yahoo.com>
2016-08-17 10:56:49 -0400

    Create PowerModel.php

---
+++ src/Statistics/Regression/Models/PowerModel.php
@@ -0,0 +1,45 @@
+<?php
+namespace Math\Statistics\Regression\Models;
+trait PowerModel
+{
+    static $B = 1; // b parameter index
+    static $A = 0; // m parameter index
+    
+   /**
+    * Evaluate the power curve equation from power law regression parameters for a value of x
+    * y = axᵇ
+    *
+    * @param number $x
+    * @param array $params
+    *
+    * @return number y evaluated
+    */
+    public static function evaluateModel($x, array $params)
+    {
+        $a = $params[self::$A];
+        $b = $params[self::$B];
+        return $a * $x ** $b;
+    }
+    
+        /**
+     * Get regression parameters (a and b)
+     *
+     * @return array [ a => number, b => number ]
+     */
+    public static function getModelParameters($params): array
+    {
+        return [
+            'a' => $params[self::$A],
+            'b' => $params[self::$B],
+        ];
+    }
+    /**
+     * Get regression equation (y = axᵇ) in format y = ax^b
+     *
+     * @return string
+     */
+    public static function getModelEquation($params): string
+    {
+        return sprintf('y = %fx^%f', $params[self::$A], $params[self::$B]);
+    }
+ }
