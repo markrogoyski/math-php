f73b6f7fc36c68b5ca7950c8fe9ef084517c3ac1
Kevin Nowaczyk <beakerboy99@yahoo.com>
2016-08-17 16:28:00 -0400

    Update HanesWoolf.php

--- src/Statistics/Regression/HanesWoolf.php
+++ src/Statistics/Regression/HanesWoolf.php
@@ -23,7 +23,7 @@ class HanesWoolf extends Regression
         // Linearize the relationship by dividing x by y
         $y’ = Multi::divide($this->xs, $this->ys);
         // Perform Least Squares Fit
-        $linear_parameters = $this->leastSquares($y’, $this->xs)->transpose()[0];
+        $linear_parameters = $this->leastSquares($y’, $this->xs)->getColumn(0);
         
         $V = 1 / $linear_parameters[1];
         $K = $linear_parameters[0] * $V;
