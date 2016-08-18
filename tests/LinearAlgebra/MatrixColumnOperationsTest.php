c973642edd786a56295c0e4e5170e78e02e303d4
Mark Rogoyski <markrogoyski@gmail.com>
2016-08-18 20:04:40 +0000

    Refactor Matrix unit tests.

---
+++ tests/LinearAlgebra/MatrixColumnOperationsTest.php
@@ -0,0 +1,292 @@
+<?php
+namespace Math\LinearAlgebra;
+
+class MatrixColumnOperationsTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function setUp()
+    {
+        $this->A = [
+            [1, 2, 3],
+            [2, 3, 4],
+            [4, 5, 6],
+        ];
+        $this->matrix = new Matrix($this->A);
+    }
+
+    /**
+     * @dataProvider dataProviderForColulmnInterchange
+     */
+    public function testColulmnInterchange(array $A, int $nᵢ, int $nⱼ, array $R)
+    {
+        $A = new Matrix($A);
+        $R = new Matrix($R);
+
+        $this->assertEquals($R, $A->columnInterchange($nᵢ, $nⱼ));
+    }
+
+    public function dataProviderForColulmnInterchange()
+    {
+        return [
+            [
+                [
+                    [1, 2, 3],
+                    [2, 3, 4],
+                    [3, 4, 5],
+                ], 0, 1,
+                [
+                    [2, 1, 3],
+                    [3, 2, 4],
+                    [4, 3, 5],
+                ]
+            ],
+            [
+                [
+                    [1, 2, 3],
+                    [2, 3, 4],
+                    [3, 4, 5],
+                ], 1, 2,
+                [
+                    [1, 3, 2],
+                    [2, 4, 3],
+                    [3, 5, 4],
+                ]
+            ],
+            [
+                [
+                    [5, 5],
+                    [4, 4],
+                    [2, 7],
+                    [9, 0],
+                ], 0, 1,
+                [
+                    [5, 5],
+                    [4, 4],
+                    [7, 2],
+                    [0, 9],
+                ]
+            ],
+        ];
+    }
+
+    public function testColumnInterchangeExceptionColumnGreaterThanN()
+    {
+        $A = new Matrix([
+            [1, 2, 3],
+            [2, 3, 4],
+            [3, 4, 5],
+        ]);
+        $this->setExpectedException('\Exception');
+        $A->columnInterchange(4, 5);
+    }
+
+    /**
+     * @dataProvider dataProviderForColumnMultiply
+     */
+    public function testColumnMultiply(array $A, int $nᵢ, $k, array $R)
+    {
+        $A = new Matrix($A);
+        $R = new Matrix($R);
+
+        $this->assertEquals($R, $A->columnMultiply($nᵢ, $k));
+    }
+
+    public function dataProviderForColumnMultiply()
+    {
+        return [
+            [
+                [
+                    [1, 2, 3],
+                    [2, 3, 4],
+                    [3, 4, 5],
+                ], 0, 5,
+                [
+                    [5, 2, 3],
+                    [10, 3, 4],
+                    [15, 4, 5],
+                ]
+            ],
+            [
+                [
+                    [1, 2, 3],
+                    [2, 3, 4],
+                    [3, 4, 5],
+                ], 1, 4,
+                [
+                    [1, 8, 3],
+                    [2, 12, 4],
+                    [3, 16, 5],
+                ]
+            ],
+            [
+                [
+                    [1, 2, 3],
+                    [2, 3, 4],
+                    [3, 4, 5],
+                ], 2, 8,
+                [
+                    [1, 2, 24],
+                    [2, 3, 32],
+                    [3, 4, 40],
+                ]
+            ],
+        ];
+    }
+
+    public function testColumnMultiplyExceptionColumnGreaterThanN()
+    {
+        $A = new Matrix([
+            [1, 2, 3],
+            [2, 3, 4],
+            [3, 4, 5],
+        ]);
+        $this->setExpectedException('\Exception');
+        $A->columnMultiply(4, 5);
+    }
+
+    public function testColumnMultiplyExceptionKIsZero()
+    {
+        $A = new Matrix([
+            [1, 2, 3],
+            [2, 3, 4],
+            [3, 4, 5],
+        ]);
+        $this->setExpectedException('\Exception');
+        $A->columnMultiply(2, 0);
+    }
+
+    /**
+     * @dataProvider dataProviderForColumnAdd
+     */
+    public function testColumnAdd(array $A, int $nᵢ, $nⱼ, int $k, array $R)
+    {
+        $A = new Matrix($A);
+        $R = new Matrix($R);
+
+        $this->assertEquals($R, $A->columnAdd($nᵢ, $nⱼ, $k));
+    }
+
+    public function dataProviderForColumnAdd()
+    {
+        return [
+            [
+                [
+                    [1, 2, 3],
+                    [2, 3, 4],
+                    [3, 4, 5],
+                ], 0, 1, 2,
+                [
+                    [1, 4, 3],
+                    [2, 7, 4],
+                    [3, 10, 5],
+                ]
+            ],
+            [
+                [
+                    [1, 2, 3],
+                    [2, 3, 4],
+                    [3, 4, 5],
+                ], 1, 2, 3,
+                [
+                    [1, 2, 9],
+                    [2, 3, 13],
+                    [3, 4, 17],
+                ]
+            ],
+            [
+                [
+                    [1, 2, 3],
+                    [2, 3, 4],
+                    [3, 4, 5],
+                ], 0, 2, 4,
+                [
+                    [1, 2, 7],
+                    [2, 3, 12],
+                    [3, 4, 17],
+                ]
+            ],
+        ];
+    }
+
+
+    public function testColumnAddExceptionRowGreaterThanN()
+    {
+        $A = new Matrix([
+            [1, 2, 3],
+            [2, 3, 4],
+            [3, 4, 5],
+        ]);
+        $this->setExpectedException('\Exception');
+        $A->columnAdd(4, 5, 2);
+    }
+
+    public function testColumnAddExceptionKIsZero()
+    {
+        $A = new Matrix([
+            [1, 2, 3],
+            [2, 3, 4],
+            [3, 4, 5],
+        ]);
+        $this->setExpectedException('\Exception');
+        $A->columnAdd(1, 2, 0);
+    }
+
+    /**
+     * @dataProvider dataProviderForColumnExclude
+     */
+    public function testColumnExclude(array $A, int $nᵢ, array $R)
+    {
+        $A = new Matrix($A);
+        $R = new Matrix($R);
+
+        $this->assertEquals($R, $A->columnExclude($nᵢ));
+    }
+
+    public function dataProviderForColumnExclude()
+    {
+        return [
+            [
+                [
+                    [1, 2, 3],
+                    [2, 3, 4],
+                    [3, 4, 5],
+                ], 0,
+                [
+                    [2, 3],
+                    [3, 4],
+                    [4, 5],
+                ]
+            ],
+            [
+                [
+                    [1, 2, 3],
+                    [2, 3, 4],
+                    [3, 4, 5],
+                ], 1,
+                [
+                    [1, 3],
+                    [2, 4],
+                    [3, 5],
+                ]
+            ],
+            [
+                [
+                    [1, 2, 3],
+                    [2, 3, 4],
+                    [3, 4, 5],
+                ], 2,
+                [
+                    [1, 2],
+                    [2, 3],
+                    [3, 4],
+                ]
+            ],
+        ];
+    }
+
+    public function testColumnExcludeExceptionColumnDoesNotExist()
+    {
+        $this->setExpectedException('\Exception');
+        $this->matrix->columnExclude(-5);
+    }
+}
