1021382e709af683f93ea78c6cd218a99166d6d2
Mark Rogoyski <markrogoyski@gmail.com>
2016-08-27 23:48:03 +0000

    Refactor and add comments to LOESS.

--- src/Statistics/Regression/LOESS.php
+++ src/Statistics/Regression/LOESS.php
@@ -8,6 +8,7 @@ use Math\LinearAlgebra\VandermondeMatrix;
 /**
  * LOESS - Locally Weighted Scatterplot Smoothing (Local regression)
  *
+ * A non-parametric method for fitting a smooth curve between two variables.
  * https://en.wikipedia.org/wiki/Local_regression
  */
 class LOESS extends NonParametricRegression
@@ -18,31 +19,48 @@ class LOESS extends NonParametricRegression
      * Smoothness parameter
      * @var number
      */
-    protected $q;
+    protected $α;
 
     /**
      * Order of the polynomial fit
      * @var int
      */
-    protected $d;
-    
+    protected $λ;
+
+    /**
+     * Number of points considered in the local regression
+     * @var number
+     */
+    protected $number_of_points;
+
     /**
      * @param array  $points [ [x, y], [x, y], ... ]
-     * @param number $q      Smoothness parameter
-     * @param int    $d      Order of the polynomial to fit
+     * @param number $α      Smoothness parameter (bandwidth)
+     *                       Determines how much of the data is used to fit each local polynomial
+     *                       ((λ + 1) / n, 1]
+     * @param int    $λ      Order of the polynomial to fit
      */
-    public function __construct($points, $q, int $d)
+    public function __construct($points, $α, int $λ)
     {
-        // If q <= (d + 1) / 2 or q > 1 throw exception
-        $this->q = $q;
-        $this->d = $d;
+        $this->α = $α;
+        $this->λ = $λ;
         
         parent::__construct($points);
+
+        // α ∈ ((λ + 1) / n, 1]
+        if (($α <= ($λ + 1) / $this->n) || $α > 1 ) {
+            throw new \Exception('Smoothness parameter α must be between ' . ($λ + 1) / $this->n . " and 1; given $α");
+        }
+
+        // Number of points considered in the local regression
+        $this->number_of_points = min(ceil($this->α * $this->n), $this->n);
     }
-    
+
+
     /**
-     * Use the smoothness parameter $q to determine the subset of data to consider for
-     * local regression. Perform a weighted least squares regression and evaluate $x.
+     * Evaluate for x
+     * Use the smoothness parameter α to determine the subset of data to consider for
+     * local regression. Perform a weighted least squares regression and evaluate x.
      *
      * @param  number $x
      *
@@ -50,23 +68,22 @@ class LOESS extends NonParametricRegression
      */
     public function evaluate($x)
     {
-        $q = $this->q;
-        $d = $this->d;
+        $α = $this->α;
+        $λ = $this->λ;
         $n = $this->n;
-        
+
         // The number of points considered in the local regression
-        $number_of_points = min(ceil($q * $n), $n);
-        $delta_x          = Single::abs(Single::subtract($this->xs, $x));
-        $qth_deltax       = Average::kthSmallest($delta_x, $number_of_points - 1);
-        $arg              = Single::min(Single::divide($delta_x, $qth_deltax * max($q, 1)), 1);
+        $Δx    = Single::abs(Single::subtract($this->xs, $x));
+        $αᵗʰΔx = Average::kthSmallest($Δx, $this->number_of_points - 1);
+        $arg   = Single::min(Single::divide($Δx, $αᵗʰΔx * max($α, 1)), 1);
         
-        // tricube = (1-arg³)³
+        // Kernel function: tricube = (1-arg³)³
         $tricube = Single::cube(Single::multiply(Single::subtract(Single::cube($arg), 1), -1));
         $weights = $tricube;
 
         // Local Regression Parameters
-        $parameters = $this->leastSquares($this->ys, $this->xs, $weights, $d);
-        $X          = new VandermondeMatrix([$x], $d + 1);
+        $parameters = $this->leastSquares($this->ys, $this->xs, $weights, $λ);
+        $X          = new VandermondeMatrix([$x], $λ + 1);
 
         return $X->multiply($parameters)[0][0];
     }
