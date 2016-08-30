f36d3fc1d7c947b1959441072194d45388425ffd
jakobsandberg <jakobsandberg95@gmail.com>
2016-08-30 11:09:30 -0700

    Added comments and description for getPoints(), included exception

--- src/NumericalAnalysis/NumericalIntegration/NumericalIntegration.php
+++ src/NumericalAnalysis/NumericalIntegration/NumericalIntegration.php
@@ -83,17 +83,36 @@ abstract class NumericalIntegration
         return $points;
     }
 
+    /**
+     * Determine where the input $source argument is a callback function, a set
+     * of arrays, or neither. If $source is a callback function, run it through
+     * the functionToPoints() method with the input $args, and set $points to
+     * output array. If $source is a set of arrays, simply set $points to
+     * $source. If $source is neither, throw an Exception.
+     *
+     * @param          $source The source of our approximation. Should be either
+     *                             a callback function or a set of arrays
+     * @param  array   $args   The arguments of our callback function: start,
+     *                             end, and n. Example: [0, 8, 5]. If $source is
+     *                             a set of arrays, $args will default to []
+     *
+     * @return array
+     * @throws Exception if $source is not callable or a set of arrays
+     */
     public static function getPoints($source, array $args = [])
     {
-        $callable = is_callable($source);
-        if ($callable) {
+        if (is_callable($source)) {
+            // To do: add method to verify function is continuous on our interval
             $function = $source;
             $start    = $args[0];
             $end      = $args[1];
             $n        = $args[2];
             $points   = self::functionToPoints($function, $start, $end, $n);
+        } elseif(is_array($source))  {
+            $points   = $source;
         } else {
-            $points = $source;
+            throw new \Exception("Input source is incorrect. You need to input
+                                  either a callback function or a set of arrays")
         }
         return $points;
     }
@@ -115,13 +134,13 @@ abstract class NumericalIntegration
         $end,
         $n
     ) {
-        $array  = [];
+        $points = [];
         $h      = ($end-$start)/($n-1);
         for ($i = 0; $i < $n; $i++) {
-            $xᵢ        = $start + $i*$h;
-            $f⟮xᵢ⟯      = call_user_func_array($function, [$xᵢ]);
-            $array[$i] = [$xᵢ, $f⟮xᵢ⟯];
+            $xᵢ         = $start + $i*$h;
+            $f⟮xᵢ⟯       = call_user_func_array($function, [$xᵢ]);
+            $points[$i] = [$xᵢ, $f⟮xᵢ⟯];
         }
-        return $array;
+        return $points;
     }
 }
