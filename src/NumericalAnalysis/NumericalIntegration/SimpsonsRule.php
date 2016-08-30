93117a1f60919ef5fb42ec6fd056bc0bbf3dcc25
jakobsandberg <jakobsandberg95@gmail.com>
2016-08-30 11:40:54 -0700

    Updated SimpsonsRule to make it compatible with callback functions

--- src/NumericalAnalysis/NumericalIntegration/SimpsonsRule.php
+++ src/NumericalAnalysis/NumericalIntegration/SimpsonsRule.php
@@ -9,10 +9,11 @@ namespace Math\NumericalAnalysis\NumericalIntegration;
  * the definite integral of a function.
  *
  * Simpson's rule belongs to the Newton-Cotes formulas, a group of methods
- * for numerical integration which approximate the integral of a function when
- * the antiderivative of that function is not known or is not explicity given.
- * Instead, we use the Newton-Cotes formulas when we are given a set of inputs
- * and their corresponding outputs for our function. We then use these values to
+ * for numerical integration which approximate the integral of a function. We
+ * can either directly supply a set of inputs and their corresponding outputs for
+ * said function, or if we explicitly know the function, we can define it as a
+ * callback function and then generate a set of points by evaluating that function
+ * at n points between a start and end point. We then use these values to
  * interpolate a Lagrange polynomial. Finally, we integrate the Lagrange
  * polynomial to approximate the integral of our original function.
  *
@@ -26,21 +27,26 @@ class SimpsonsRule extends NumericalIntegration
 {
     /**
      * Use Simpson's Rule to aproximate the definite integral of a
-     * function f(x). Each array in our input contains two numbers which
-     * correspond to coordinates (x, y) or equivalently, (x, f(x)), of the
-     * function f(x) whose definite integral we are approximating.
+     * function f(x). Our input can support either a set of arrays, or a callback
+     * function with arguments (to produce a set of arrays). Each array in our
+     * input contains two numbers which correspond to coordinates (x, y) or
+     * equivalently, (x, f(x)), of the function f(x) whose definite integral we
+     * are approximating.
      *
      * Note: Simpson's method requires that we have an even number of
      * subintervals (we must supply an odd number of points) and also that the
      * size of each subinterval is equal (spacing between each point is equal).
      *
      * The bounds of the definite integral to which we are approximating is
-     * determined by the minimum and maximum values of our x-components in our
-     * input coordinates (arrays).
+     * determined by the our inputs.
      *
-     * Example: approximate([9, 10], [6, 5], [3, 7]) will approximate the
-     * definite integral of the function that produces these coordinates with a
-     * lower bound of 3, and an upper bound of 9.
+     * Example: approximate([0, 10], [3, 5], [10, 7]) will approximate the definite
+     * integral of the function that produces these coordinates with a lower
+     * bound of 0, and an upper bound of 10.
+     *
+     * Example: approximate(function($x) {return $x**2;}, [0, 4 ,5]) will produce
+     * a set of arrays by evaluating the callback at 5 evenly spaced points
+     * between 0 and 4. Then, this array will be used in our approximation.
      *
      * Simpson's Rule:
      *
@@ -54,17 +60,22 @@ class SimpsonsRule extends NumericalIntegration
      *
      *  where h = xᵢ₊₁ - xᵢ
      *
-     * @param  array $points Array of arrays (array of points).
-     *                       Each array (point) contains precisely two numbers,
-     *                       an x and y value.
-     *                       Our input must contain an odd number of arrays.
+     * @param          $source The source of our approximation. Should be either
+     *                         a callback function or a set of arrays. Each array
+     *                         (point) contains precisely two numbers, an x and y.
+     *                         Example array: [[1,2], [2,3], [3,4]].
+     *                         Example callback: function($x) {return $x**2;}
+     * @param  array   $args   The arguments of our callback function: start,
+     *                         end, and n. Example: [0, 8, 5]. If $source is a
+     *                         set of arrays, $args will default to [].
      *
-     *                       Example: [[1,2], [2,3], [3,4]]
-     *
-     * @return number        The approximation to the integral of f(x)
+     * @return number          The approximation to the integral of f(x)
      */
-    public static function approximate(array $points)
+    public static function approximate($source, array $args = [])
     {
+        // get an array of points from our $source argument
+        $points = self::getPoints($source, $args);
+
         // Validate input and sort points
         self::validate($points, $degree = 3);
         self::isSubintervalsEven($points);
@@ -114,7 +125,9 @@ class SimpsonsRule extends NumericalIntegration
     {
         if (count($points) % 2 !== 1) {
             throw new \Exception("There must be an even number of subintervals.
-                                  Provide an input with an odd number of points");
+                                  Your input must either be a set of an odd
+                                  number of points, or a callback function
+                                  evaluated at an odd number of points (odd n)");
         }
 
         return true;
