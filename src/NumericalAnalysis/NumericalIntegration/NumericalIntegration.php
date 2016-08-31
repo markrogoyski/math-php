7d23529653351c5dc964799be052e14a54667ace
jakobsandberg <jakobsandberg95@gmail.com>
2016-08-30 17:28:46 -0700

    Moved getPoints() and functionToPoints() methods above validate() and and sort()

--- src/NumericalAnalysis/NumericalIntegration/NumericalIntegration.php
+++ src/NumericalAnalysis/NumericalIntegration/NumericalIntegration.php
@@ -27,63 +27,6 @@ abstract class NumericalIntegration
     abstract public static function approximate($source, array $args);
 
     /**
-     * Validate that there are enough input arrays (points), that each point array
-     * has precisely two numbers, and that no two points share the same first number
-     * (x-component)
-     *
-     * @param  array  $points Array of arrays (points)
-     * @param  number $degree The miminum number of input arrays
-     *
-     * @return bool
-     * @throws Exception if there are less than two points
-     * @throws Exception if any point does not contain two numbers
-     * @throws Exception if two points share the same first number (x-component)
-     */
-    protected static function validate(array $points, $degree)
-    {
-        if (count($points) < $degree) {
-            throw new \Exception("You need to have at least $degree sets of
-                                  coordinates (arrays) for this technique");
-        }
-
-        $x_coordinates = [];
-        foreach ($points as $point) {
-            if (count($point) !== 2) {
-                throw new \Exception("Each array needs to have have precisely
-                                      two numbers, an x- and y-component");
-            }
-
-            $x_component = $point[self::X];
-            if (in_array($x_component, $x_coordinates)) {
-                throw new \Exception("Not a function. Your input array contains
-                                      more than one coordinate with the same
-                                      x-component.");
-            }
-            array_push($x_coordinates, $x_component);
-        }
-
-        return true;
-    }
-
-    /**
-     * Sorts our coordinates (arrays) by their x-component (first number) such
-     * that consecutive coordinates have an increasing x-component.
-     *
-     * @param  array $points
-     *
-     * @return array
-     */
-    protected static function sort(array $points)
-    {
-        $x = self::X;
-        usort($points, function ($a, $b) use ($x) {
-            return $a[$x] <=> $b[$x];
-        });
-
-        return $points;
-    }
-
-    /**
      * Determine where the input $source argument is a callback function, a set
      * of arrays, or neither. If $source is a callback function, run it through
      * the functionToPoints() method with the input $args, and set $points to
@@ -99,7 +42,7 @@ abstract class NumericalIntegration
      * @return array
      * @throws Exception if $source is not callable or a set of arrays
      */
-    protected static function getPoints($source, array $args = [])
+    public static function getPoints($source, array $args = [])
     {
         if (is_callable($source)) {
             // To do: add method to verify function is continuous on our interval
@@ -141,6 +84,63 @@ abstract class NumericalIntegration
             $f⟮xᵢ⟯       = call_user_func_array($function, [$xᵢ]);
             $points[$i] = [$xᵢ, $f⟮xᵢ⟯];
         }
+        return $points;
+    }
+
+    /**
+     * Validate that there are enough input arrays (points), that each point array
+     * has precisely two numbers, and that no two points share the same first number
+     * (x-component)
+     *
+     * @param  array  $points Array of arrays (points)
+     * @param  number $degree The miminum number of input arrays
+     *
+     * @return bool
+     * @throws Exception if there are less than two points
+     * @throws Exception if any point does not contain two numbers
+     * @throws Exception if two points share the same first number (x-component)
+     */
+    public static function validate(array $points, $degree = 2)
+    {
+        if (count($points) < $degree) {
+            throw new \Exception("You need to have at least $degree sets of
+                                  coordinates (arrays) for this technique");
+        }
+
+        $x_coordinates = [];
+        foreach ($points as $point) {
+            if (count($point) !== 2) {
+                throw new \Exception("Each array needs to have have precisely
+                                      two numbers, an x- and y-component");
+            }
+
+            $x_component = $point[self::X];
+            if (in_array($x_component, $x_coordinates)) {
+                throw new \Exception("Not a function. Your input array contains
+                                      more than one coordinate with the same
+                                      x-component.");
+            }
+            array_push($x_coordinates, $x_component);
+        }
+
+        return true;
+    }
+
+    /**
+     * Sorts our coordinates (arrays) by their x-component (first number) such
+     * that consecutive coordinates have an increasing x-component.
+     *
+     * @param  array $points
+     *
+     * @return array
+     */
+    protected static function sort(array $points)
+    {
+        $x = self::X;
+        usort($points, function ($a, $b) use ($x) {
+            return $a[$x] <=> $b[$x];
+        });
+
         return $points;
     }
 }
