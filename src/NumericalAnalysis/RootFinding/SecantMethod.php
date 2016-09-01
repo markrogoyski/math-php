73808f375dfe2d8fe2641b10360464e8ccddd18e
jakobsandberg <jakobsandberg95@gmail.com>
2016-09-01 15:01:47 -0700

    Updated Secant Method to reflect Root Finding Validation trait

--- src/NumericalAnalysis/RootFinding/SecantMethod.php
+++ src/NumericalAnalysis/RootFinding/SecantMethod.php
@@ -45,36 +45,19 @@ class SecantMethod
     /**
      * Verify the input arguments are valid for correct use of the secant method
      * If the tolerance is less than zero, an Exception will be thrown. If
-     * $p₀ = $p₁, then clearly we cannot run our loop the slope with be
-     * undefined, so we throw an Exception. If $a > $b, we simply reverse them
-     * as if the user input $b = $a and $a = $b so the new $a < $b.
+     * $p₀ = $p₁, then we cannot run our loop because the slope with be
+     * undefined, so we throw an Exception.
      *
-     * @param Callable $function callback function f(x)
      * @param number   $p₀       First initial approximation
      * @param number   $p₁       Second initial approximation
      * @param number   $tol      Tolerance; How close to the actual solution we would like.
      *
-     * @return bool
      * @throws Exception if $tol (the tolerance) is negative
-     * @throws Exception if $a = $b
+     * @throws Exception if $p₀ = $p₁
      */
-    private static function validate(callable $function, $p₀, $p₁, $tol)
+    private static function validate($p₀, $p₁, $tol)
     {
-        if ($tol < 0) {
-            throw new \Exception('Tolerance must be greater than zero.');
-        }
-
-        if (!($p₀ < $p₁)) {
-            if ($p₀ === $p₁) {
-                throw new \Exception('Start point and end point of interval
-                                        cannot be the same.');
-            } else {
-                $temp_p₀ = $p₁;
-                $p₁      = $p₀;
-                $p₀      = $temp_p₀;
-            }
-        }
-
-        return true;
+        RootFindingValidation::tolerance($tol);
+        RootFindingValidation::interval($p₀, $p₁);
     }
 }
