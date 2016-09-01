28291a51208a15ae9a9d0acc8c6f0a0990709244
jakobsandberg <jakobsandberg95@gmail.com>
2016-09-01 15:15:07 -0700

    Added comments, descriptions to root finding validation trait

--- src/NumericalAnalysis/RootFinding/RootFindingValidation.php
+++ src/NumericalAnalysis/RootFinding/RootFindingValidation.php
@@ -4,10 +4,14 @@ namespace Math\NumericalAnalysis\RootFinding;
 /**
  * Common validation methods for root finding techniques
  */
-class RootFindingValidation
+trait RootFindingValidation
 {
     /**
+     * Throw an exception if the tolerance is negative.
      *
+     * @param number $tol Tolerance; How close to the actual solution we would like.
+     *
+     * @throws Exception if $tol (the tolerance) is negative
      */
     public static function tolerance($tol) {
         if ($tol < 0) {
@@ -16,7 +20,12 @@ class RootFindingValidation
     }
 
     /**
+     * Verify that the start and end of of an interval are distinct numbers.
      *
+     * @param number $a The start of the interval
+     * @param number $b The end of the interval
+     *
+     * @throws Exception if $a = $b
      */
     public static function interval($a, $b)
     {
