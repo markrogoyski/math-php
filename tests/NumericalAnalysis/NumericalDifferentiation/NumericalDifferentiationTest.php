5fe1973cf09e26f73de8ac2610e617b77e577b7d
jakobsandberg <jakobsandberg95@gmail.com>
2016-09-07 13:13:16 -0700

    Added test for Numerical Differentiation parent class

---
+++ tests/NumericalAnalysis/NumericalDifferentiation/NumericalDifferentiationTest.php
@@ -0,0 +1,42 @@
+<?php
+namespace Math\NumericalAnalysis\NumericalDifferentiation;
+
+class NumericalDifferentiationTest extends \PHPUnit_Framework_TestCase
+{
+    public function testInstantiateAbstractClassException()
+    {
+        // Instantiating NumericalDifferentiation (an abstract class)
+        $this->setExpectedException('\Error');
+        new NumericalDifferentiation;
+    }
+
+    public function testIncorrectInput()
+    {
+        // The input $source is neither a callback or a set of arrays
+        $this->setExpectedException('\Exception');
+        $x                 = 10;
+        $incorrectFunction = $x**2 + 2 * $x + 1;
+        NumericalDifferentiation::getPoints($incorrectFunction, [0,4,5]);
+    }
+
+    public function testNotCoordinatesException()
+    {
+        // An array doesn't have precisely two numbers (coordinates)
+        $this->setExpectedException('\Exception');
+        NumericalDifferentiation::validate([[0,0], [1,2,3], [2,2]], $degree = 3);
+    }
+
+    public function testNotEnoughArraysException()
+    {
+        // There are not enough arrays in the input
+        $this->setExpectedException('\Exception');
+        NumericalDifferentiation::validate([[0,0]], $degree = 3);
+    }
+
+    public function testNotAFunctionException()
+    {
+        // Two arrays share the same first number (x-component)
+        $this->setExpectedException('\Exception');
+        NumericalDifferentiation::validate([[0,0], [0,5], [1,1]], $degree = 3);
+    }
+}
