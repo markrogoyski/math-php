a7476f47d8a9bb702fba560699b554f23bc76f36
jakobsandberg <jakobsandberg95@gmail.com>
2016-09-07 23:34:31 -0700

    Added test for differentiation

--- tests/Functions/PolynomialsTest.php
+++ tests/Functions/PolynomialsTest.php
@@ -9,8 +9,8 @@ class PolynomialTest extends \PHPUnit_Framework_TestCase
         // p(x) = x² + 2x + 3
 
         $polynomial = new Polynomial([1, 2, 3]);
-        $expected = " x² + 2x + 3";
-        $actual = strval($polynomial);
+        $expected   = " x² + 2x + 3";
+        $actual     = strval($polynomial);
         $this->assertEquals($expected, $actual);
     }
 
@@ -20,8 +20,19 @@ class PolynomialTest extends \PHPUnit_Framework_TestCase
         // p(0) = 3
 
         $polynomial = new Polynomial([1, 2, 3]);
-        $expected = 3;
-        $actual = $polynomial(0);
+        $expected   = 3;
+        $actual     = $polynomial(0);
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testDifferentiation()
+    {
+        // p(x)  = x² + 2x + 3
+        // p'(x) = 2x + 2
+
+        $polynomial = new Polynomial([1, 2, 3]);
+        $expected   = new Polynomial([2, 2]);
+        $actual     = $polynomial->differentiate();
         $this->assertEquals($expected, $actual);
     }
 }
