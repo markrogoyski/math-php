501901f5193e302ddf80f5a5aba20c06caab74a8
Mark Rogoyski <markrogoyski@gmail.com>
2016-09-08 07:16:08 +0000

    Refactor Polynomial toString method to try to make logic simplier. Maybe successful at doing that; maybe not.

--- src/Functions/Polynomial.php
+++ src/Functions/Polynomial.php
@@ -19,7 +19,7 @@ namespace Math\Functions;
  *
  * Example:
  *     $polynomial = new Polynomial([1, -8, 12, 3])
- *     echo $polynomial;    // prints "x³ - 8x² + 12x + 3"
+ *     echo $polynomial;    // prints 'x³ - 8x² + 12x + 3'
  *     echo $polynomial(4); // prints -31
  *
  * https://en.wikipedia.org/wiki/Polynomial
@@ -27,7 +27,12 @@ namespace Math\Functions;
 class Polynomial
 {
     private $degree;
-    private $coefficient;
+    private $coefficients;
+
+    /**
+     * @var array Unicode characters for exponents
+     */
+    const SYMBOLS = ['⁰', '¹', '²', '³', '⁴', '⁵', '⁶', '⁷', '⁸', '⁹'];
 
     /**
      * When a polynomial is instantiated, set the coefficients and degree of
@@ -37,77 +42,67 @@ class Polynomial
      *                           Example: new Polynomial([1, 2, 3]) will create
      *                           a polynomial that looks like x² + 2x + 3.
      */
-    public function __construct(array $coefficient)
+    public function __construct(array $coefficients)
     {
-        $this->degree = count($coefficient) - 1;
-        $this->coefficient = $coefficient;
+        $this->degree       = count($coefficients) - 1;
+        $this->coefficients = $coefficients;
     }
 
     /**
      * When a polynomial is to be treated as a string, return it in a readable format.
      * Example: $polynomial = new Polynomial([1, -8, 12, 3]);
      *          echo $polynomial;
-     *          // prints "x³ - 8x² + 12x + 3"
+     *          // prints 'x³ - 8x² + 12x + 3'
      *
      * @return string A human readable representation of the polynomial
      */
     public function __toString()
     {
-        $polynomial = ""; // Start with an empty polynomial
-        $symbol     = ["⁰", "¹", "²", "³", "⁴", "⁵", "⁶", "⁷", "⁸", "⁹"]; // Unicode characters
+        // Start with an empty polynomial
+        $polynomial = '';
 
-        // Iterate over each coefficient to generate the string for each term
-        for ($i = 0; $i < $this->degree + 1; $i++) {
-            // If coefficient is 0, skip to the next term
-            if ($this->coefficient[$i] == 0) {
+        // Iterate over each coefficient to generate the string for each term and add to the polynomial
+        foreach ($this->coefficients as $i => $coefficient) {
+            if ($coefficient == 0) {
                 continue;
-
-            // Otherwise, use the coefficient as is
-            } else {
-                $coefficient = intval($this->coefficient[$i]);
             }
+
+            // Power of the current term
+            $power = $this->degree - $i;
 
             // Build the exponent of our string as a unicode character
-            $exponent = "";                 // Start with empty exponent
-            $power    = $this->degree - $i; // Power of the current term
+            $exponent = '';
             for ($j = 0; $j < strlen($power); $j++) {
                 $digit     = intval(strval($power)[$j]); // The j-th digit of $power
-                $exponent .= $symbol[$digit];            // The corresponding unicode character
+                $exponent .= self::SYMBOLS[$digit];      // The corresponding unicode character
             };
 
-            // Drop redundant ¹ term from monomials
-            if ($exponent == "¹") {
-                $exponent = "";
-            }
-
             // Get the sign for the term
-            if ($coefficient > 0) {
-                if ($power == $this->degree) {
-                    $sign = ""; // If the first term is positive, drop the redundant + sign
-                } else {
-                    $sign = "+";
-                }
-            } else {
-                $sign = "-";
-            }
+            $sign = ($coefficient > 0) ? '+' : '-';
 
             // Drop the sign from the coefficient, as it is handled by $sign
             $coefficient = abs($coefficient);
 
+            // Drop coefficients that equal 1 (and -1)
+            if ($coefficient == 1) {
+                $coefficient = '';
+            }
+
             // Generate the $term string
+            // No x term is power = 0;
             if ($power == 0) {
-                $term = "{$sign} {$coefficient}"; // No x term if $power = 0
+                $term = "{$sign} {$coefficient}";
             } else {
-                // Drop coefficients that equal 1 (and -1)
-                if ($coefficient == 1) {
-                    $coefficient = "";
-                }
-                $term = "{$sign} {$coefficient}x{$exponent} "; // Nonzero x term
+                $term = "{$sign} {$coefficient}x{$exponent} ";
             }
 
             // Add the current term to the polynomial
             $polynomial .= $term;
         }
+
+        // Cleanup front and back; drop redundant ¹ and ⁰ terms from monomials
+        $polynomial = trim(str_replace(['x¹ ','x⁰ '], 'x ', $polynomial), '+ ');
+        $polynomial = preg_replace('/^\-\s/', '-', $polynomial);
 
         return $polynomial;
     }
@@ -126,8 +121,8 @@ class Polynomial
     public function __invoke($x₀)
     {
         // Set object parameters as local variables so they can be used with the use function
-        $degree = $this->degree;
-        $coefficient = $this->coefficient;
+        $degree       = $this->degree;
+        $coefficients = $this->coefficients;
 
         // Start with the zero polynomial
         $polynomial = function ($x) {
@@ -137,8 +132,8 @@ class Polynomial
         // Iterate over each coefficient to create a callback function for each term
         for ($i = 0; $i < $degree + 1; $i++) {
             // Create a callback function for the current term
-            $term = function ($x) use ($degree, $coefficient, $i) {
-                return $coefficient[$i] * $x**($degree - $i);
+            $term = function ($x) use ($degree, $coefficients, $i) {
+                return $coefficients[$i] * $x**($degree - $i);
             };
             // Add the new term to the polynomial
             $polynomial = Arithmetic::add($polynomial, $term);
