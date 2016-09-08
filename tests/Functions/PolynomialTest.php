1faf8c4861acf752cae7e72f8f55a0ac173d2aa2
Mark Rogoyski <markrogoyski@gmail.com>
2016-09-08 06:09:46 +0000

    Refactor Polynomial unit test and add more test cases.

--- tests/Functions/PolynomialTest.php
+++ tests/Functions/PolynomialTest.php
@@ -14,14 +14,43 @@ class PolynomialTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($expected, $actual);
     }
 
-    public function testEval()
+    /**
+     * @dataProvider dataProviderForEval
+     */
+    public function testEval(array $coefficients, $x, $expected)
     {
-        // p(x) = x² + 2x + 3
-        // p(0) = 3
+        $polynomial = new Polynomial($coefficients);
+        $evaluated  = $polynomial($x);
+        $this->assertEquals($expected, $evaluated);
+    }
 
-        $polynomial = new Polynomial([1, 2, 3]);
-        $expected = 3;
-        $actual = $polynomial(0);
-        $this->assertEquals($expected, $actual);
+    public function dataProviderForEval()
+    {
+        return [
+            [
+                [1, 2, 3], // p(x) = x² + 2x + 3
+                0, 3       // p(0) = 3
+            ],
+            [
+                [1, 2, 3], // p(x) = x² + 2x + 3
+                1, 6       // p(1) = 6
+            ],
+            [
+                [1, 2, 3], // p(x) = x² + 2x + 3
+                2, 11      // p(2) = 11
+            ],
+            [
+                [1, 2, 3], // p(x) = x² + 2x + 3
+                3, 18      // p(3) = 18
+            ],
+            [
+                [1, 2, 3], // p(x) = x² + 2x + 3
+                4, 27      // p(4) = 27
+            ],
+            [
+                [1, 2, 3], // p(x) = x² + 2x + 3
+                -1, 2     // p(-1) = 2
+            ],
+        ];
     }
 }
