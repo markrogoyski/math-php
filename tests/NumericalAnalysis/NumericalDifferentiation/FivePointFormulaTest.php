fee0c294865ba6c0e5754eebde5966f6220ca3a8
jakobsandberg <jakobsandberg95@gmail.com>
2016-09-07 18:23:40 -0700

    Added test with points

--- tests/NumericalAnalysis/NumericalDifferentiation/FivePointFormulaTest.php
+++ tests/NumericalAnalysis/NumericalDifferentiation/FivePointFormulaTest.php
@@ -117,4 +117,55 @@ class FivePointFormulaTest extends \PHPUnit_Framework_TestCase
         $actual = FivePointFormula::differentiate($target, $f, $a, $b, $n);
         $this->assertEquals($expected, $actual, '', $tol);
     }
+
+    public function testPointsZeroError()
+    {
+        // f(x) = 13x² -92x + 96
+        $f = function ($x) {
+            return 13 * $x**2 - 92 * $x + 96;
+        };
+
+        $points = [[0, $f(0)], [1, $f(1)], [2, $f(2)], [3, $f(3)], [4, $f(4)]];
+
+        /*
+        *                                      h²
+        * Error term for the Midpoint Formula: - f⁽³⁾(ζ₁)
+        *                                      6
+        *
+        *     where ζ₁ lies between x₀ - h and x₀ + h
+        *
+        *                                      h²
+        * Error term for the Endpoint Formula: - f⁽³⁾(ζ₀)
+        *                                      3
+        *
+        *     where ζ₀ lies between x₀ and x₀ + 2h
+        */
+
+        // f'(x)   = 26x - 92
+        // f''(x)  = 26
+        // f⁽³⁾(x) = 0
+        // Thus, our error is zero in both formulas for our function $f
+
+        $f’ = function ($x) {
+            return 26 * $x - 92;
+        };
+
+        // Check that the endpoint formula agrees with f'(x) at x = 0
+        $target = 0;
+        $expected = $f’($target);
+        $actual = FivePointFormula::differentiate($target, $points);
+        $this->assertEquals($expected, $actual);
+
+        // Check that the midpoint formula agrees with f'(x) at x = 2
+        $target = 2;
+        $expected = $f’($target);
+        $actual = FivePointFormula::differentiate($target, $points);
+        $this->assertEquals($expected, $actual);
+
+        // Check that the (backward) endpoint formula agrees with f'(x) at x = 4
+        $target = 4;
+        $expected = $f’($target);
+        $actual = FivePointFormula::differentiate($target, $points);
+        $this->assertEquals($expected, $actual);
+    }
 }
