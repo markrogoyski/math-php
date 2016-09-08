a91b8447fcfbe836e4bedaeb1aa82267b453ceff
jakobsandberg <jakobsandberg95@gmail.com>
2016-09-07 22:29:16 -0700

    Added constructor method

--- src/Functions/Polynomial.php
+++ src/Functions/Polynomial.php
@@ -1,3 +1,15 @@
 <?php
 
 namespace Math\Functions;
+
+class Polynomial
+{
+    private $degree;
+    private $coefficient;
+
+    public function __construct(array $coefficient)
+    {
+        $this->degree = count($coefficient) - 1;
+        $this->coefficient = $coefficient;
+    }
+}
