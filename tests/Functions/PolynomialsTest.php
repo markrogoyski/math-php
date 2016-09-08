227a23299aa055381db8ad989d08e9816a6932bd
jakobsandberg <jakobsandberg95@gmail.com>
2016-09-07 22:39:17 -0700

    Added test for converting polynomial to string

--- tests/Functions/PolynomialsTest.php
+++ tests/Functions/PolynomialsTest.php
@@ -4,5 +4,13 @@ namespace Math\Functions;
 
 class PolynomialTest extends \PHPUnit_Framework_TestCase
 {
+    public function testString()
+    {
+        // p(x) = x² + 2x + 3
 
+        $polynomial = new Polynomial([1, 2, 3]);
+        $expected = " x² + 2x + 3";
+        $actual = strval($polynomial);
+        $this->assertEquals($expected, $actual);
+    }
 }
