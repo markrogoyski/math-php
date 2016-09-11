1f2ac63433276cf71a7e421459c01125771e2525
jakobsandberg <jakobsandberg95@gmail.com>
2016-09-11 08:49:15 -0700

    Added degree getter method to class description

--- src/Functions/Polynomial.php
+++ src/Functions/Polynomial.php
@@ -16,6 +16,7 @@ namespace Math\Functions;
  * Current features:
  *     o Print a human readable representation of a polynomial
  *     o Evaluate a polynomial at any real number
+ *     o Return the degree of the current polynomial
  *     o Polynomial differentiation (exact)
  *     o Polynomial integration (indefinite integral)
  *     o Polynomial addition
@@ -25,6 +26,7 @@ namespace Math\Functions;
  *     $polynomial = new Polynomial([1, -8, 12, 3]);
  *     echo $polynomial;                        // prints 'x³ - 8x² + 12x + 3'
  *     echo $polynomial(4);                     // prints -31
+ *     echo $polynomial->getDegree();           // prints 3
  *     echo $polynomial->differentiate();       // prints '3x² - 16x + 12'
  *     echo $polynomial->integrate();           // prints '0.25x⁴ - 2.6666666666667x³ + 6x² + 3x'
  *     echo $polynomial->add($polynomial);      // prints '2x³ - 16x² + 24x + 6'
