f2f6a6f4d8de1dd54a56243613bd9bcc5793fe0b
jakobsandberg <jakobsandberg95@gmail.com>
2016-09-11 20:16:34 -0700

    Added coefficients getter method to class description

--- src/Functions/Polynomial.php
+++ src/Functions/Polynomial.php
@@ -17,6 +17,7 @@ namespace Math\Functions;
  *     o Print a human readable representation of a polynomial
  *     o Evaluate a polynomial at any real number
  *     o Return the degree of a polynomial
+ *     o Return the coefficients of a polynomial
  *     o Polynomial differentiation (exact)
  *     o Polynomial integration (indefinite integral)
  *     o Polynomial addition
@@ -27,6 +28,7 @@ namespace Math\Functions;
  *     echo $polynomial;                        // prints 'x³ - 8x² + 12x + 3'
  *     echo $polynomial(4);                     // prints -31
  *     echo $polynomial->getDegree();           // prints 3
+ *     print_r($polynomial->getCoefficients()); // prints [1, -8, 12, 3]
  *     echo $polynomial->differentiate();       // prints '3x² - 16x + 12'
  *     echo $polynomial->integrate();           // prints '0.25x⁴ - 2.6666666666667x³ + 6x² + 3x'
  *     echo $polynomial->add($polynomial);      // prints '2x³ - 16x² + 24x + 6'
