72e8acfe7c92d8502e856147ac4de2629d16db6c
jakobsandberg <jakobsandberg95@gmail.com>
2016-09-13 11:43:07 -0700

    Added test for 3-piece function, with a test evaluation in each piece

--- tests/Functions/PiecewiseTest.php
+++ tests/Functions/PiecewiseTest.php
@@ -16,6 +16,43 @@ class PiecewiseTest extends \PHPUnit_Framework_TestCase
 
     public function dataProviderForEval()
     {
-        
+        return [
+            [
+                [
+                  [-100, -2],                   // f interval: [-100, -2]
+                  [-2, 2],                      // g interval: [-2, 2]
+                  [2, 100]                      // h interval: [2, 100]
+                ], [
+                  function ($x) { return -$x }, // f(x) = -x
+                  function ($x) { return 2 },   // g(x) = 2
+                  function ($x) { return $x }   // h(x) = x
+                ],
+                -27, 27       // p(-27) = f(-27) = -(-27) = 27
+            ],
+            [
+                [
+                  [-100, -2],                   // f interval: [-100, -2]
+                  [-2, 2],                      // g interval: [-2, 2]
+                  [2, 100]                      // h interval: [2, 100]
+                ], [
+                  function ($x) { return -$x }, // f(x) = -x
+                  function ($x) { return 2 },   // g(x) = 2
+                  function ($x) { return $x }   // h(x) = x
+                ],
+                1, 2       // p(1) = g(1) = 2
+            ],
+            [
+                [
+                  [-100, -2],                   // f interval: [-100, -2]
+                  [-2, 2],                      // g interval: [-2, 2]
+                  [2, 100]                      // h interval: [2, 100]
+                ], [
+                  function ($x) { return -$x }, // f(x) = -x
+                  function ($x) { return 2 },   // g(x) = 2
+                  function ($x) { return $x }   // h(x) = x
+                ],
+                20, 20       // p(20) = h(20) = 20
+            ],
+        ];
     }
 }
