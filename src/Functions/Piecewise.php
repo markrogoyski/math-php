094155d1ddff365c92384e33af7317dd9afc5761
jakobsandberg <jakobsandberg95@gmail.com>
2016-09-13 12:41:29 -0700

    Added __toString() method

--- src/Functions/Piecewise.php
+++ src/Functions/Piecewise.php
@@ -18,66 +18,10 @@ class Piecewise
         $this->functions = $functions;
     }
 
-    /**
-     * When a polynomial is to be treated as a string, return it in a readable format.
-     * Example: $polynomial = new Polynomial([1, -8, 12, 3]);
-     *          echo $polynomial;
-     *          // prints 'x³ - 8x² + 12x + 3'
-     *
-     * @return string A human readable representation of the polynomial
-     */
     public function __toString()
     {
-        // Start with an empty polynomial
-        $polynomial = '';
 
-        // Iterate over each coefficient to generate the string for each term and add to the polynomial
-        foreach ($this->coefficients as $i => $coefficient) {
-            if ($coefficient == 0) {
-                continue;
-            }
-
-            // Power of the current term
-            $power = $this->degree - $i;
-
-            // Build the exponent of our string as a unicode character
-            $exponent = '';
-            for ($j = 0; $j < strlen($power); $j++) {
-                $digit     = intval(strval($power)[$j]); // The j-th digit of $power
-                $exponent .= self::SYMBOLS[$digit];      // The corresponding unicode character
-            };
-
-            // Get the sign for the term
-            $sign = ($coefficient > 0) ? '+' : '-';
-
-            // Drop the sign from the coefficient, as it is handled by $sign
-            $coefficient = abs($coefficient);
-
-            // Drop coefficients that equal 1 (and -1) if they are not the 0th-degree term
-            if ($coefficient == 1 and $this->degree - $i != 0) {
-                $coefficient = '';
-            }
-
-            // Generate the $term string. No x term if power = 0.
-            if ($power == 0) {
-                $term = "{$sign} {$coefficient}";
-            } else {
-                $term = "{$sign} {$coefficient}x{$exponent} ";
-            }
-
-            // Add the current term to the polynomial
-            $polynomial .= $term;
-        }
-
-        // Cleanup front and back; drop redundant ¹ and ⁰ terms from monomials
-        $polynomial = trim(str_replace(['x¹ ','x⁰ '], 'x ', $polynomial), '+ ');
-        $polynomial = preg_replace('/^\-\s/', '-', $polynomial);
-
-        $polynomial = ($polynomial !== '') ? $polynomial : '0';
-
-        return $polynomial;
     }
-
 
     public function __invoke($x₀)
     {
