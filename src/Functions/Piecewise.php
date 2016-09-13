b2ff03109e171420e418940dd61742fe9d9c3f25
jakobsandberg <jakobsandberg95@gmail.com>
2016-09-13 12:09:44 -0700

    Added __invoke method() to Piecewise

--- src/Functions/Piecewise.php
+++ src/Functions/Piecewise.php
@@ -78,39 +78,12 @@ class Piecewise
         return $polynomial;
     }
 
-    /**
-     * When a polynomial is being evaluated at a point x₀, build a callback
-     * function and return the value of the callback function at x₀
-     * Example: $polynomial = new Polynomial([1, -8, 12, 3]);
-     *          echo $polynomial(4);
-     *          // prints -13
-     *
-     * @param number $x₀ The value at which we are evaluting our polynomial
-     *
-     * @return number The result of our polynomial evaluated at $x₀
-     */
+
     public function __invoke($x₀)
     {
-        // Set object parameters as local variables so they can be used with the use function
-        $degree       = $this->degree;
-        $coefficients = $this->coefficients;
-
-        // Start with the zero polynomial
-        $polynomial = function ($x) {
-            return 0;
-        };
-
-        // Iterate over each coefficient to create a callback function for each term
-        for ($i = 0; $i < $degree + 1; $i++) {
-            // Create a callback function for the current term
-            $term = function ($x) use ($degree, $coefficients, $i) {
-                return $coefficients[$i] * $x**($degree - $i);
-            };
-            // Add the new term to the polynomial
-            $polynomial = Arithmetic::add($polynomial, $term);
-        }
-
-        return $polynomial($x₀);
+        $range = inPiece ($x₀, $this->intervals);
+        $function = $this->functions[$range];
+        echo $function($x₀);
     }
 
     public function inPiece ($x, $intervals)
