c34b560dc338899f77e1d87b64ac4abf38f8e677
Mark Rogoyski <markrogoyski@gmail.com>
2016-09-13 22:05:05 +0000

    Add comments and refactor NoncentralT distribution.

--- src/Probability/Distribution/Continuous/NoncentralT.php
+++ src/Probability/Distribution/Continuous/NoncentralT.php
@@ -17,6 +17,7 @@ class NoncentralT extends Continuous
      * x ∈ (-∞,∞)
      * ν ∈ (0,∞)
      * t ∈ (-∞,∞)
+     * μ ∈ (-∞,∞)
      * @var array
      */
     const LIMITS = [
@@ -25,9 +26,10 @@ class NoncentralT extends Continuous
         't' => '(-∞,∞)',
         'μ' => '(-∞,∞)',
     ];
-    
+
     /**
      * Probability density function
+     *
      *   /v\                /   μ²\
      *  | - |              |-1*--- |                        / ν    3     μ²x²   \            / ν + 1    1     μ²x²   \   \
      *   \2/                \   2 /           /         ₁F₁|  - ;  - ; --------- |       ₁F₁|  ----- ;  - ; --------- |   |
@@ -36,60 +38,123 @@ class NoncentralT extends Continuous
      *  ν           (ν / 2)                  |                        / ν + 1 \                         / ν    \          |
      * 2  * (ν + x²)         * Γ(ν / 2)      |           (ν + x²) * Γ|  ------ |          √(ν + x²) * Γ|  - + 1 |         |
      *                                        \                       \   2   /                         \ 2    /         /
+     *
      * @param number $x percentile
      * @param int    $ν degrees of freedom > 0
+     *
+     * @return number
      */
     public static function PDF($x, int $ν, $μ)
     {
         Support::checkLimits(self::LIMITS, ['x' => $x, 'ν' => $ν, 'μ' => $μ]);
+
         $part1 =  $ν ** ($ν / 2) * Special::gamma($ν + 1) * exp(-1 * $μ**2 / 2) / 2**$ν / ($ν + $x**2)**($ν / 2) / Special::gamma($ν / 2);
+
         $F1 = $ν / 2 + 1;
         $F2 = 3 / 2;
         $F3 = $μ**2 * $x**2 / 2 / ($ν + $x**2);
         $inner_part1 = sqrt(2) * $μ * $x * Special::confluentHypergeometric($F1, $F2, $F3) / ($ν + $x**2) / Special::gamma(($ν + 1) / 2);
+
         $F1 = ($ν + 1) / 2;
         $F2 = 1 / 2;
         $inner_part2 = Special::confluentHypergeometric($F1, $F2, $F3) / sqrt($ν + $x**2) / Special::gamma($ν / 2 + 1);
+
         return $part1 * ($inner_part1 + $inner_part2);
     }
-    
+
+    /**
+     * Cumulative distribution function
+     *
+     * Fᵥ,ᵤ(x) = Fᵥ,ᵤ(x),      if x ≥ 0
+     *         = 1 - Fᵥ,₋ᵤ(x)  if x < 0
+     *
+     * @param number $x
+     * @param int    $ν Degrees of freedom
+     * @param number $μ Noncentrality parameter
+     *
+     * @return number
+     */
     public static function CDF($x, int $ν, $μ)
     {
         if ($x >= 0) {
             return self::F($x, $ν, $μ);
         }
-        return 1 - self::F($x, $ν, -1 * $μ);
+        return 1 - self::F($x, $ν, -$μ);
     }
-    
+
+    /**
+     * F used within CDF
+     *                          _                                        _
+     *                   1  ∞  |        /    1  ν \          /       ν \  |
+     * Fᵥ,ᵤ(x) = Φ(-μ) + -  ∑  |  pⱼIy | j + -, -  | + qⱼIy | j + 1, -  | |
+     *                   2 ʲ⁼⁰ |_       \    2  2 /          \       2 / _|
+     *
+     *  where
+     *   Φ       = cumulative distribution function of the standard normal distribution
+     *   Iy(a,b) = regularized incomplete beta function
+     *
+     *          x²
+     *   y = ------
+     *       x² + ν
+     *
+     *        1      /  μ² \   / μ² \ʲ
+     *   pⱼ = -- exp| - -   | |  -   |
+     *        j!     \  2  /   \ 2  /
+     *
+     *              μ          /  μ² \   / μ² \ʲ
+     *   qⱼ = ------------ exp| - -   | |  -   |
+     *        √2Γ(j + 3/2)     \  2  /   \ 2  /
+     *
+     * @param number $x
+     * @param int    $ν Degrees of freedom
+     * @param number $μ Noncentrality parameter
+     *
+     * @return number
+     */
     private static function F($x, int $ν, $μ)
     {
         Support::checkLimits(self::LIMITS, ['x' => $x, 'ν' => $ν, 'μ' => $μ]);
-        $Φ = StandardNormal::CDF(-1 * $μ);
+
+        $Φ = StandardNormal::CDF(-$μ);
         $y = $x**2/($x**2 + $ν);
+
         $sum = $Φ;
         $tol = .00000001;
-        $j = 0;
+        $j   = 0;
+
         do {
             $exp = exp(-1 * $μ**2 / 2) * ($μ**2 / 2)**$j;
-            $p = 1 / Combinatorics::factorial($j) * $exp;
-            $q = $μ / sqrt(2) / Special::gamma($j + 3 / 2) * $exp;
-            $i = $j + 1 / 2;
-            $k = $ν / 2;
-            $I1 = Special::regularizedIncompleteBeta($y, $j + 1 / 2, $ν / 2);
-            $I2 = Special::regularizedIncompleteBeta($y, $j + 1, $ν / 2);
-            $delta = $p * $I1 + $q * $I2;
-            $sum += $delta / 2;
-            $j += 1;
+            $pⱼ  = 1 / Combinatorics::factorial($j) * $exp;
+            $qⱼ  = $μ / sqrt(2) / Special::gamma($j + 3 / 2) * $exp;
+            $I1  = Special::regularizedIncompleteBeta($y, $j + 1 / 2, $ν / 2);
+            $I2  = Special::regularizedIncompleteBeta($y, $j + 1, $ν / 2);
+
+            $delta = $pⱼ * $I1 + $qⱼ * $I2;
+            $sum  += $delta / 2;
+            $j    += 1;
         } while ($delta / $sum > $tol || $j < 10);
+
         return $sum;
     }
-    
+
     /**
      * Mean of the distribution
+     *            _
+     *           /ν Γ((ν - 1)/2)
+     * E[T] = μ / - ------------    if ν > 1
+     *         √  2    Γ(ν/2)
+     *
+     *      = Does not exist        if ν ≤ 1
+     *
+     * @param int    $ν Degrees of freedom
+     * @param number $μ Noncentrality parameter
+     *
+     * @return number
      */
     public static function mean(int $ν, $μ)
     {
         Support::checkLimits(self::LIMITS, ['ν' => $ν, 'μ' => $μ]);
+
         return $μ * sqrt($ν / 2) * Special::gamma(($ν - 1) / 2) / Special::gamma($ν / 2);
     }
 }
