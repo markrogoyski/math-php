c3f18a5c8428d78ba78499d8b2eba24109516d17
Mark Rogoyski <markrogoyski@gmail.com>
2016-09-15 06:14:01 +0000

    Whitespace.

--- src/Finance.php
+++ src/Finance.php
@@ -22,15 +22,15 @@ class Finance
      */
     public static function pmt(float $rate, int $periods, float $present_value, float $future_value = 0, bool $beginning = false): float
     {
-      $when = 0;
-      if ($beginning) {
-        $when = 1;
-      }
-      if ($rate == 0) {
-        return - ($future_value + $present_value) / $periods;
-      }
-      return - ($future_value + ($present_value * pow(1 + $rate, $periods)))
-                 /
-               ((1 + $rate*$when) / $rate * (pow(1 + $rate, $periods) - 1));
+        $when = 0;
+        if ($beginning) {
+          $when = 1;
+        }
+        if ($rate == 0) {
+          return - ($future_value + $present_value) / $periods;
+        }
+        return - ($future_value + ($present_value * pow(1 + $rate, $periods)))
+            /
+            ((1 + $rate*$when) / $rate * (pow(1 + $rate, $periods) - 1));
     }
 }
