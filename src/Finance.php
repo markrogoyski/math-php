40c40395578590f2634897623b96a7e5f9c5df52
Mark Rogoyski <markrogoyski@gmail.com>
2016-09-18 06:23:49 +0000

    Minor improvements.

--- src/Finance.php
+++ src/Finance.php
@@ -45,22 +45,21 @@ class Finance
      *  pmt(0.04/4, 20*4, 1000000, 0, false)
      *
      * @param  float $rate
-     * @param  int $periods
+     * @param  int   $periods
      * @param  float $present_value
      * @param  float $future_value
-     * @param  bool beginning
+     * @param  bool  $beginning adjust the payment to the beginning or end of the period
      *
      * @return float
      */
     public static function pmt(float $rate, int $periods, float $present_value, float $future_value = 0, bool $beginning = false): float
     {
-        $when = 0;
-        if ($beginning) {
-            $when = 1;
-        }
+        $when = $beginning ? 1 : 0;
+
         if ($rate == 0) {
             return - ($future_value + $present_value) / $periods;
         }
+
         return - ($future_value + ($present_value * pow(1 + $rate, $periods)))
             /
             ((1 + $rate*$when) / $rate * (pow(1 + $rate, $periods) - 1));
