ddbd3b4c818e1f76a2cef131d16458920b2f9f84
jakobsandberg <jakobsandberg95@gmail.com>
2016-09-19 11:05:05 -0700

    Added test that cubic spline agrees at input points

--- tests/NumericalAnalysis/Interpolation/CubicSpline.php
+++ tests/NumericalAnalysis/Interpolation/CubicSpline.php
@@ -3,5 +3,35 @@ namespace Math\NumericalAnalysis\Interpolation;
 
 class CubicSplineTest extends \PHPUnit_Framework_TestCase
 {
+    public function testPolynomialAgrees()
+    {
+        $points = [[0, 0], [1, 5], [3, 2], [7, 10], [10, -4]];
 
+        $p = CubicSpline::interpolate($points);
+
+        // Assure p(0) = 0 agrees with input [0, 0]
+        $expected = 0;
+        $actual = $p(0);
+        $this->assertEquals($expected, $actual);
+
+        // Assure p(1) = 5 agrees with input [1, 5]
+        $expected = 5;
+        $actual = $p(1);
+        $this->assertEquals($expected, $actual);
+
+        // Assure p(3) = 2 agrees with input [3, 2]
+        $expected = 2;
+        $actual = $p(3);
+        $this->assertEquals($expected, $actual);
+
+        // Assure p(7) = 10 agrees with input [7, 10]
+        $expected = 10;
+        $actual = $p(7);
+        $this->assertEquals($expected, $actual);
+
+        // Assure p(10) = -4 agrees with input [10, -4]
+        $expected = -4;
+        $actual = $p(10);
+        $this->assertEquals($expected, $actual);
+    }
 }
