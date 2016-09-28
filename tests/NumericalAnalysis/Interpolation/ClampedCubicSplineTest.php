8840d6e37f951ae989c0024eb0cf8029a9054e09
jakobsandberg <jakobsandberg95@gmail.com>
2016-09-27 20:23:17 -0700

    Added test to ensure output agrees with points

--- tests/NumericalAnalysis/Interpolation/ClampedCubicSplineTest.php
+++ tests/NumericalAnalysis/Interpolation/ClampedCubicSplineTest.php
@@ -3,13 +3,13 @@ namespace Math\NumericalAnalysis\Interpolation;
 
 use Math\Functions\Polynomial;
 
-class NaturalCubicSplineTest extends \PHPUnit_Framework_TestCase
+class ClampedCubicSplineTest extends \PHPUnit_Framework_TestCase
 {
     public function testPolynomialAgrees()
     {
-        $points = [[0, 0], [1, 5], [3, 2], [7, 10], [10, -4]];
+        $points = [[0, 0, 1], [1, 5, -2], [3, 2, 0], [7, 10, 3], [10, -4, 3]];
 
-        $p = NaturalCubicSpline::interpolate($points);
+        $p = ClampedCubicSpline::interpolate($points);
 
         // Assure p(0) = 0 agrees with input [0, 0]
         $expected = 0;
@@ -35,127 +35,5 @@ class NaturalCubicSplineTest extends \PHPUnit_Framework_TestCase
         $expected = -4;
         $actual = $p(10);
         $this->assertEquals($expected, $actual);
-    }
-
-    public function testSolveZeroError()
-    {
-        // f(x) = 8x³ -13x² -92x + 96
-        $f = new Polynomial([8, -13, -92, 96]);
-
-        // The error in the Cubic Spline Interpolating Polynomial is proportional
-        // to the max value of the 4th derivative. Thus, if our input Function
-        // is a 3rd-degree polynomial, the fourth derivative will be zero, and
-        // thus we will have zero error.
-
-        $a = 0;
-        $b = 10;
-        $n = 50;
-        $tol = 0;
-        $roundoff = 0.0001; // round off error
-
-        $p = NaturalCubicSpline::interpolate($f, $a, $b, $n);
-
-        // Check that p(x) agrees with f(x) at x = 0
-        $expected = $f(0);
-        $actual = $p(0);
-        $this->assertEquals($expected, $actual, '', $tol + $roundoff);
-
-        // Check that p(x) agrees with f(x) at x = 2
-        $expected = $f(2);
-        $actual = $p(2);
-        $this->assertEquals($expected, $actual, '', $tol + $roundoff);
-
-        // Check that p(x) agrees with f(x) at x = 4
-        $expected = $f(4);
-        $actual = $p(4);
-        $this->assertEquals($expected, $actual, '', $tol + $roundoff);
-
-        // Check that p(x) agrees with f(x) at x = 6
-        $expected = $f(6);
-        $actual = $p(6);
-        $this->assertEquals($expected, $actual, '', $tol + $roundoff);
-
-        // Check that p(x) agrees with f(x) at x = 8
-        $expected = $f(8);
-        $actual = $p(8);
-        $this->assertEquals($expected, $actual, '', $tol + $roundoff);
-
-        // Check that p(x) agrees with f(x) at x = 10
-        $expected = $f(10);
-        $actual = $p(10);
-        $this->assertEquals($expected, $actual, '', $tol + $roundoff);
-
-        // Check that p(x) agrees with f(x) at x = 7.32 (not a node)
-        $expected = $f(10);
-        $actual = $p(10);
-        $this->assertEquals($expected, $actual, '', $tol + $roundoff);
-    }
-
-    public function testSolveNonzeroError()
-    {
-        // f(x) = x⁴ + 8x³ -13x² -92x + 96
-        $f = new Polynomial([1, 8, -13, -92, 96]);
-
-        // The error is bounded by:
-        // |f(x)-p(x)| = tol <= (1/4!) * h⁴ * max f⁽⁴⁾(x)
-        // where h = max hᵢ
-
-        // f'(x)  = 4x³ +24x² -26x - 92
-        // f''(x) = 12x² - 48x - 26
-        // f'''(x) = 24x - 48
-        // f⁽⁴⁾(x) = 24
-
-        $a = 0;
-        $b = 10;
-        $n = 51;
-
-        // So, tol <= (1/24) * (1/5)⁴ * 24 = (1/5)⁴
-
-        $tol = 0.2**4;
-        $roundoff = 0.000001; // round off error
-
-        $p = NaturalCubicSpline::interpolate($f, $a, $b, $n);
-
-        // Check that p(x) agrees with f(x) at x = 0
-        $target = 0;
-        $expected = $f($target);
-        $x = $p($target);
-        $this->assertEquals($expected, $x, '', $tol + $roundoff);
-
-        // Check that p(x) agrees with f(x) at x = 2
-        $target = 2;
-        $expected = $f($target);
-        $x = $p($target);
-        $this->assertEquals($expected, $x, '', $tol + $roundoff);
-
-        // Check that p(x) agrees with f(x) at x = 4
-        $target = 4;
-        $expected = $f($target);
-        $x = $p($target);
-        $this->assertEquals($expected, $x, '', $tol + $roundoff);
-
-        // Check that p(x) agrees with f(x) at x = 6
-        $target = 6;
-        $expected = $f($target);
-        $x = $p($target);
-        $this->assertEquals($expected, $x, '', $tol + $roundoff);
-
-        // Check that p(x) agrees with f(x) at x = 8
-        $target = 8;
-        $expected = $f($target);
-        $x = $p($target);
-        $this->assertEquals($expected, $x, '', $tol + $roundoff);
-
-        // Check that p(x) agrees with f(x) at x = 10
-        $target = 10;
-        $expected = $f($target);
-        $x = $p($target);
-        $this->assertEquals($expected, $x, '', $tol + $roundoff);
-
-        // Check that p(x) agrees with f(x) at x = 7.32 (not a node)
-        $target = 7.32;
-        $expected = $f($target);
-        $x = $p($target);
-        $this->assertEquals($expected, $x, '', $tol + $roundoff);
     }
 }
