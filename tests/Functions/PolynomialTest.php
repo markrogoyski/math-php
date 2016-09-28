37643abc78665482a4d6e01c914b49e5e5d34f2b
jakobsandberg <jakobsandberg95@gmail.com>
2016-09-28 10:52:16 -0700

    Added test for variable of polynomial

--- tests/Functions/PolynomialTest.php
+++ tests/Functions/PolynomialTest.php
@@ -9,8 +9,6 @@ class PolynomialTest extends \PHPUnit_Framework_TestCase
      */
     public function testString(array $coefficients, string $expected)
     {
-        // p(x) = x² + 2x + 3
-
         $polynomial = new Polynomial($coefficients);
         $string     = strval($polynomial);
         $this->assertEquals($expected, $string);
@@ -59,7 +57,6 @@ class PolynomialTest extends \PHPUnit_Framework_TestCase
                 [0, 2, 0],       // p(x) = 2x
                 '2x',
             ],
-
             [
                 [0, -2, 3],       // p(x) = -2x + 3
                 '-2x + 3',
@@ -98,6 +95,104 @@ class PolynomialTest extends \PHPUnit_Framework_TestCase
             ],
             [
                 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],       // p(x) = x¹¹ + 2x¹⁰ + 3x⁹ + 4x⁸ + 5x⁷ + 6x⁶ + 7x⁵ + 8x⁴ + 9x³ + 10x² + 11x + 12
+                'x¹¹ + 2x¹⁰ + 3x⁹ + 4x⁸ + 5x⁷ + 6x⁶ + 7x⁵ + 8x⁴ + 9x³ + 10x² + 11x + 12',
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderForVariable
+     */
+    public function testVariable(array $args, string $expected)
+    {
+        $coefficients = $args[0];
+        $variable     = $args[1] ?? "";
+        $polynomial   = new Polynomial($coefficients);
+        $string       = strval($polynomial);
+        $this->assertEquals($expected, $string);
+    }
+
+    public function dataProviderForVariable()
+    {
+        return [
+            [
+                [[1, 2, 3]],       // p(x) = x² + 2x + 3
+                'x² + 2x + 3',
+            ],
+            [
+                [[2, 3, 4]],       // p(x) = 2x² + 3x + 4
+                '2x² + 3x + 4',
+            ],
+            [
+                [[-1, -2, -3]],       // p(x) = -x² - 2x - 3
+                '-x² - 2x - 3',
+            ],
+            [
+                [[-2, -3, -4]],       // p(x) = -2x² - 3x - 4
+                '-2x² - 3x - 4',
+            ],
+            [
+                [[0, 2, 3]],       // p(x) = 2x + 3
+                '2x + 3',
+            ],
+            [
+                [[1, 0, 3]],       // p(x) = x² + 3
+                'x² + 3',
+            ],
+            [
+                [[1, 2, 0]],       // p(x) = x² + 2x
+                'x² + 2x',
+            ],
+            [
+                [[0, 0, 3]],       // p(x) = 3
+                '3',
+            ],
+            [
+                [[1, 0, 0]],       // p(x) = x²
+                'x²',
+            ],
+            [
+                [[0, 2, 0]],       // p(x) = 2x
+                '2x',
+            ],
+            [
+                [[0, -2, 3]],       // p(x) = -2x + 3
+                '-2x + 3',
+            ],
+            [
+                [[-1, 0, 3]],       // p(x) = -x² + 3
+                '-x² + 3',
+            ],
+            [
+                [[1, -2, 0]],       // p(x) = x² - 2x
+                'x² - 2x',
+            ],
+            [
+                [[0, 0, -3]],       // p(x) = -3
+                '-3',
+            ],
+            [
+                [[-1, 0, 0]],       // p(x) = -x²
+                '-x²',
+            ],
+            [
+                [[0, -2, 0]],       // p(x) = -2x
+                '-2x',
+            ],
+            [
+                [[0, 0, 0]],       // p(x) = 0
+                '0',
+            ],
+            [
+                [[0, 0, 1]],       // p(x) = 1
+                '1',
+            ],
+            [
+                [[0, 0, 5]],       // p(x) = 5
+                '5',
+            ],
+            [
+                [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]],       // p(x) = x¹¹ + 2x¹⁰ + 3x⁹ + 4x⁸ + 5x⁷ + 6x⁶ + 7x⁵ + 8x⁴ + 9x³ + 10x² + 11x + 12
                 'x¹¹ + 2x¹⁰ + 3x⁹ + 4x⁸ + 5x⁷ + 6x⁶ + 7x⁵ + 8x⁴ + 9x³ + 10x² + 11x + 12',
             ],
         ];
@@ -252,7 +347,7 @@ class PolynomialTest extends \PHPUnit_Framework_TestCase
         $coefficients = $polynomial->getCoefficients();
         $this->assertEquals($expected, $coefficients);
     }
-    
+
     public function dataProviderForGetCoefficients()
     {
         return [
