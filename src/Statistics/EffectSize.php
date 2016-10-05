ac56e48974a06c30651e8b5b4d274b27d9ac7a2e
Mark Rogoyski <markrogoyski@gmail.com>
2016-10-05 07:00:25 +0000

    Add effect size cohen's q.

--- src/Statistics/EffectSize.php
+++ src/Statistics/EffectSize.php
@@ -114,4 +114,32 @@ class EffectSize
     {
         return $measure_of_variance_explained / (1 - $measure_of_variance_explained);
     }
+
+    /**
+     * Cohen's q
+     *
+     * The difference between two Fisher transformed Pearson regression coefficients.
+     * https://en.wikipedia.org/wiki/Effect_size
+     *
+     *     1     1 + r₁   1     1 + r₂
+     * q = - log ------ - - log ------
+     *     2     1 - r₁   2     1 - r₂
+     *
+     * where r₁ and r₂ are the regressions being compared
+     *
+     * @param number $r₁
+     * @param number $r₂
+     *
+     * @return number
+     */
+    public static function cohensQ($r₁, $r₂)
+    {
+        if ($r₁ >= 1 || $r₂ >= 1) {
+            throw new \Exception('r must be less than 0');
+        }
+
+        $½ = 0.5;
+
+        return abs(($½ * log((1 + $r₁) / (1 - $r₁))) - ($½ * log((1 + $r₂) / (1 - $r₂))));
+    }
 }
