2257f6fd25c4bf0ec8eff3484ebed0f14692048b
Mark Rogoyski <markrogoyski@gmail.com>
2016-10-05 04:38:39 +0000

    Add effect size to statistics.

---
+++ src/Statistics/EffectSize.php
@@ -0,0 +1,34 @@
+<?php
+namespace MathPHP\Statistics;
+
+/**
+ * Effect size is a quantitative measure of the strength of a phenomenon.
+ * https://en.wikipedia.org/wiki/Effect_size
+ */
+class EffectSize
+{
+    /**
+     * η² (Eta squared)
+     *
+     * Eta-squared describes the ratio of variance explained in the dependent
+     * variable by a predictor while controlling for other predictors, making
+     * it analogous to the r².
+     * 
+     *      SSB
+     * η² = ---
+     *      SST
+     *
+     * where:
+     *  SSB = sum of squares between (treatment)
+     *  SST = sum of squares total
+     *
+     * @param  number $SSB Sum of squares between (treatment)
+     * @param  number $SST Sum of squares total
+     *
+     * @return number
+     */
+    public static function etaSquared($SSB, $SST)
+    {
+        return $SSB / $SST;
+    }
+}
