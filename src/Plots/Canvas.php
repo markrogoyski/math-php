98e5ffe217792155e90a42033b9d59c1bfbe577b
jakobsandberg <jakobsandberg95@gmail.com>
2016-10-08 23:37:14 -0700

    Added constructor and parameter visibility

--- src/Plots/Canvas.php
+++ src/Plots/Canvas.php
@@ -4,5 +4,14 @@ namespace MathPHP\Plots;
 
 class Canvas
 {
+    protected $width;
+    protected $height;
+    protected $canvas;
 
+    public function __construct($width = 600, $height = 600)
+    {
+        $this->width  = $width;
+        $this->height = $height;
+        $this->canvas = imagecreate($width, $height);
+    }
 }
