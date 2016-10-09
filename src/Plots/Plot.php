6208f65f81e6fcb8930b8644f941bd70f9e0169e
jakobsandberg <jakobsandberg95@gmail.com>
2016-10-08 23:40:59 -0700

    Added constructor to Plot and method visibility

--- src/Plots/Plot.php
+++ src/Plots/Plot.php
@@ -4,11 +4,19 @@ namespace MathPHP\Plots;
 
 class Plot extends Canvas
 {
+    private $padding;
+
+    public function __construct($padding)
+    {
+        $this->padding = $padding;
+    }
+
     public function draw($canvas, $width, $height)
     {
+        $padding = $this->padding;
         $black = imagecolorallocate($canvas, 0, 0, 0);
         header('Content-type: image/png');
-        // Draw something here
+        imagerectangle($canvas, $padding, $padding, $width - $padding, $height - $padding, $black);
         return $canvas;
     }
 }
