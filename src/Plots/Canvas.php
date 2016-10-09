fdb8063d17af2b10fbd62e3239da593658d6ba59
jakobsandberg <jakobsandberg95@gmail.com>
2016-10-08 23:37:41 -0700

    Added addPlot() method

--- src/Plots/Canvas.php
+++ src/Plots/Canvas.php
@@ -7,11 +7,18 @@ class Canvas
     protected $width;
     protected $height;
     protected $canvas;
+    protected $plot;
 
     public function __construct($width = 600, $height = 600)
     {
         $this->width  = $width;
         $this->height = $height;
         $this->canvas = imagecreate($width, $height);
+    }
+
+    public function addPlot($padding = 0)
+    {
+        $this->plot = new Plot($padding);
+        return $this->plot;
     }
 }
