d6b3bcad2aee3f6d58d4d01ec607d3077d33ae6f
jakobsandberg <jakobsandberg95@gmail.com>
2016-10-08 23:40:30 -0700

    Added basic draw() method

--- src/Plots/Plot.php
+++ src/Plots/Plot.php
@@ -4,5 +4,11 @@ namespace MathPHP\Plots;
 
 class Plot extends Canvas
 {
-
+    public function draw($canvas, $width, $height)
+    {
+        $black = imagecolorallocate($canvas, 0, 0, 0);
+        header('Content-type: image/png');
+        // Draw something here
+        return $canvas;
+    }
 }
