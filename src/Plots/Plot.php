acf8b41a59562c22a01ba6cc2728e454c6be1801
jakobsandberg <jakobsandberg95@gmail.com>
2016-10-08 23:51:31 -0700

    Added comments to map out development of Plot class

--- src/Plots/Plot.php
+++ src/Plots/Plot.php
@@ -18,10 +18,27 @@ class Plot extends Canvas
 
     public function draw($canvas, $width, $height)
     {
+        // Build convenience variables for canvas/plot measures
         $padding = $this->padding;
+
+        // Create axes
         $black = imagecolorallocate($canvas, 0, 0, 0);
-        header('Content-type: image/png');
         imagerectangle($canvas, $padding, $padding, $width - $padding, $height - $padding, $black);
+
+        // Define input function
+
+        // calculate canvas step size and function step size
+
+        // Calculate function values, min, and max
+
+        // Draw y-axis values, dashes
+
+        // Draw x-axis values, dashes
+
+        // Draw title, x-axis title, y-axis title
+
+        // Draw graph
+
         return $canvas;
     }
 }
