f8c1253bb007d606f7e6d45bc0bb9efa42001eb4
jakobsandberg <jakobsandberg95@gmail.com>
2016-10-09 00:15:05 -0700

    Added convenience variables for canvas/plot measures

--- src/Plots/Plot.php
+++ src/Plots/Plot.php
@@ -4,22 +4,33 @@ namespace MathPHP\Plots;
 
 class Plot extends Canvas
 {
-    private $padding;
-
-    public function __construct($padding)
+    public function __construct()
     {
-        $this->padding = $padding;
+        parent::__construct();
+        $this->x_label = "x-label";
+        $this->y_label = "y-label";
     }
 
-    public function setPadding($padding)
-    {
-        $this->padding = $padding;
-    }
-
-    public function draw($canvas, $width, $height)
+    public function draw($canvas)
     {
         // Build convenience variables for canvas/plot measures
-        $padding = $this->padding;
+        $width = $this->width;
+        $height = $this->height;
+        $padding = 50;
+        list($x_shift, $y_shift) = [
+            isset($this->y_label) ? 1 : 0,
+            isset($this->x_label) ? 1 : 0,
+        ];
+        list($graph_start_x, $graph_start_y, $graph_end_x, $graph_end_y) = [
+            (1 + $x_shift)*$padding,
+            imagesy($canvas) - (1 + $y_shift)*$padding,
+            imagesx($canvas) - $padding,
+            $padding
+        ];
+        list($graph_width, $graph_height) = [
+            imagesx($canvas) - (2 + $x_shift)*$padding,
+            imagesy($canvas) - (2 + $y_shift)*$padding
+        ];
 
         // Create axes
         $black = imagecolorallocate($canvas, 0, 0, 0);
