2b146bfe72fc22ed51379ceddd1c2ca851664e92
jakobsandberg <jakobsandberg95@gmail.com>
2016-10-09 22:04:25 -0700

    Refactored Plot to use imagestring instead of imagefttext

--- src/Plots/Plot.php
+++ src/Plots/Plot.php
@@ -8,8 +8,8 @@ class Plot extends Canvas
     {
         parent::__construct();
         $this->function = $function;
-        $this->start = $start;
-        $this->end = $end;
+        $this->start    = $start;
+        $this->end      = $end;
     }
 
     public function grid(bool $switch)
@@ -24,17 +24,16 @@ class Plot extends Canvas
 
     public function yLabel(string $label)
     {
-        $this->y_label = $label;
+        $this->yLabel = $label;
     }
 
     public function xLabel(string $label)
     {
-        $this->x_label = $label;
+        $this->xLabel = $label;
     }
 
     public function color(string $color)
     {
-        //$this->color = $color;
         switch($color) {
             case 'red':
                 $color = [255, 0, 0];
@@ -55,41 +54,38 @@ class Plot extends Canvas
     {
         // Set defaults
         $black   = imagecolorallocate($canvas, 0, 0, 0);
+        $white   = imagecolorallocate($canvas, 255, 255, 255);
         $padding = 50;
 
         // Grab parameters
-        $width   = $this->width;
-        $height  = $this->height;
-        $title   = $this->title ?? null;
-        $x_label = $this->x_label ?? null;
-        $y_label = $this->y_label ?? null;
-        $weight  = $this->weight ?? 3;
-        $grid    = $this->grid ?? false;
-        $color   = isset($this->color) ? imagecolorallocate($canvas, ... $this->color) : $black;
-
-        // Build convenience variables for graph measures
-        list($x_shift, $y_shift) = [
-            isset($y_label) ? 1 : 0,
-            isset($x_label) ? 1 : 0,
-        ];
-        list($graph_start_x, $graph_start_y, $graph_end_x, $graph_end_y) = [
-            (1 + $x_shift)*$padding,
-            imagesy($canvas) - (1 + $y_shift)*$padding,
-            imagesx($canvas) - $padding,
-            $padding
-        ];
-        list($graph_width, $graph_height) = [
-            imagesx($canvas) - (2 + $x_shift)*$padding,
-            imagesy($canvas) - (2 + $y_shift)*$padding
-        ];
-
-        // Create axes
-        imagerectangle($canvas, $graph_start_x, $graph_end_y, $graph_end_x, $graph_start_y, $black);
-
-        // Define input function and function domain
+        $width    = $this->width;
+        $height   = $this->height;
+        $title    = $this->title ?? null;
+        $xLabel   = $this->xLabel ?? null;
+        $yLabel   = $this->yLabel ?? null;
+        $weight   = $this->weight ?? 3;
+        $grid     = $this->grid ?? false;
+        $color    = isset($this->color) ? imagecolorallocate($canvas, ... $this->color) : $black;
         $function = $this->function;
         $start    = $this->start;
         $end      = $this->end;
+
+        // Determine if we need to add padding to make room for axis labels
+        $x_shift = isset($yLabel) ? 40 : 0;
+        $y_shift = isset($xLabel) ? 10 : 0;
+
+        // Measure start and end points of plot on canvas
+        $graph_start_x = $padding + $x_shift;
+        $graph_start_y = imagesy($canvas) - ($padding + $y_shift);
+        $graph_end_x   = imagesx($canvas) - $padding;
+        $graph_end_y   = $padding;
+
+        // Measure height and width of plot on canvas
+        $graph_width  = $graph_end_x - $graph_start_x;
+        $graph_height = $graph_start_y - $graph_end_y;
+
+        // Create axes
+        imagerectangle($canvas, $graph_start_x, $graph_end_y, $graph_end_x, $graph_start_y, $black);
 
         // Calculate graph step size and function step size
         $n             = 1000;
@@ -102,55 +98,39 @@ class Plot extends Canvas
         for ($i = 0; $i <= $n; $i++) {
             $image[] = $function($start + $i*$function_step);
         }
-        $min = min($image);
-        $max = max($image);
+        $min            = min($image);
+        $max            = max($image);
         $function_scale = $graph_height/($max - $min);
 
         // Draw y-axis values and grid
-        $fontpath = realpath('.'); //replace . with a different directory if needed
-        putenv('GDFONTPATH='.$fontpath);
-        $count = 9;
-        $font = 'arial.ttf';
-        $size = 10;
-        $angle = 0;
-        $length1 = 1;
-        $length2 = 5;
-        $white = imagecolorallocate($canvas, 255, 255, 255);
-        $style = array_merge(array_fill(0, $length1, $black), array_fill(0, $length2, $white));
+        $gridCountY = 10;
+        $style = array_merge(array_fill(0, 1, $black), array_fill(0, 5, $white));
         imagesetstyle($canvas, $style);
-        for ($i = 0; $i <= $count; $i++) {
-            imagettftext($canvas, $size, $angle, $graph_start_x - $padding*0.75, $size*0.5 + $graph_start_y - $i*($graph_height/$count), $black, $font, round(($min + $i*($max - $min)/$count), 1));
-            if ($i !== 0 and $i !== $count and $grid) {
-                imageline($canvas, $graph_start_x, $graph_start_y - $i*($graph_height/$count), $graph_end_x, $graph_start_y - $i*($graph_height/$count), IMG_COLOR_STYLED);
+        for ($i = 0; $i <= $gridCountY; $i++) {
+            imagestring($canvas, 2, $graph_start_x - 50, $graph_start_y - 8 - $i*($graph_height/$gridCountY), round(($min + $i*($max - $min)/$gridCountY), 1), $black);
+            if ($i !== 0 and $i !== $gridCountY and $grid) {
+                imageline($canvas, $graph_start_x, $graph_start_y - $i*($graph_height/$gridCountY), $graph_end_x, $graph_start_y - $i*($graph_height/$gridCountY), IMG_COLOR_STYLED);
             }
         }
 
         // Draw x-axis values and grid
-        $newcount = 9;
-        for ($i = 0; $i <= $newcount; $i++) {
-            imagettftext($canvas, $size, $angle, $graph_start_x + $i*($graph_width/$newcount), $graph_start_y + $padding/2, $black, $font, round(($start + $i*($end - $start)/$newcount), 1));
-            if ($i !== 0 and $i !== $newcount and $grid) {
-                imageline($canvas, $graph_start_x + $i*($graph_width/$newcount), $graph_start_y, $graph_start_x + $i*($graph_width/$newcount), $graph_end_y, IMG_COLOR_STYLED);
+        $gridCountX = 10;
+        for ($i = 0; $i <= $gridCountX; $i++) {
+            imagestring($canvas, 2, $graph_start_x + $i*($graph_width/$gridCountX), $graph_start_y + 8, round(($start + $i*($end - $start)/$gridCountX), 1), $black);
+            if ($i !== 0 and $i !== $gridCountX and $grid) {
+                imageline($canvas, $graph_start_x + $i*($graph_width/$gridCountX), $graph_start_y, $graph_start_x + $i*($graph_width/$gridCountX), $graph_end_y, IMG_COLOR_STYLED);
             }
         }
 
         // Draw title, x-axis title, y-axis title
-        $sizeTitle = 16;
-        $sizeAxis = 14;
         if (isset($title)) {
-            $p = imagettfbbox($sizeTitle, 0, $font, $title);
-            $title_x = ($width - ($p[2] - $p[0]))/2;
-            imagettftext($canvas, $sizeTitle, $angle, $title_x, 35, $black, $font, $title);
+            imagestring($canvas, 5, ($width - strlen($title)*9)/2, 18, $title, $black);
         }
-        if (isset($x_label)) {
-            $q = imagettfbbox($sizeAxis, 0, $font, $x_label);
-            $x_label_width = ($width - ($q[2] - $q[0]))/2;
-            imagettftext($canvas, $sizeAxis, $angle, $x_label_width, $height - 35, $black, $font, $x_label);
+        if (isset($xLabel)) {
+            imagestring($canvas, 4, ($width - strlen($xLabel)*8)/2, $height - 30, $xLabel, $black);
         }
-        if (isset($y_label)) {
-            $r = imagettfbbox($sizeAxis, 90, $font, $y_label);
-            $y_label_height = ($height - ($r[3] - $r[1]))/2;
-            imagettftext($canvas, $sizeAxis, 90, 40, $y_label_height, $black, $font, $y_label);
+        if (isset($yLabel)) {
+            imagestringup($canvas, 4, 10, ($height + strlen($yLabel)*8)/2, $yLabel, $black);
         }
 
         // Draw graph
