df279f42ae176182edb6a436f877b01c08e9043d
jakobsandberg <jakobsandberg95@gmail.com>
2016-10-13 02:24:25 -0700

    Added test to Plots for validateSize() exceptions

--- tests/Plots/CanvasTest.php
+++ tests/Plots/CanvasTest.php
@@ -4,5 +4,15 @@ namespace MathPHP\Plots;
 
 class CanvasTest extends \PHPUnit_Framework_TestCase
 {
+    public function testValidateSizeException()
+    {
+        // Giving a negative size for canvas dimensions
+        $this->setExpectedException('\Exception');
+        new Canvas(-100, 500);
 
+        // Adjust to a negative size for canvas dimensions
+        $this->setExpectedException('\Exception');
+        $canvas = new Canvas();
+        $canvas->size(-100, 500);
+    }
 }
