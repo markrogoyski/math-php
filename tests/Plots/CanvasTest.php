c120d76d6839ac65336e7067cb243ad5a2b5bab5
jakobsandberg <jakobsandberg95@gmail.com>
2016-10-13 02:30:09 -0700

    Split exception tests so there is only one exception in each

--- tests/Plots/CanvasTest.php
+++ tests/Plots/CanvasTest.php
@@ -4,31 +4,31 @@ namespace MathPHP\Plots;
 
 class CanvasTest extends \PHPUnit_Framework_TestCase
 {
-    public function testValidateSizeException()
+    public function testValidateSizeExceptionSet()
     {
         // Giving a negative size for canvas dimensions
         $this->setExpectedException('\Exception');
         new Canvas(-100, 500);
+    }
 
-        $this->setExpectedException('\Exception');
-        new Canvas(100, -500);
-
-        $this->setExpectedException('\Exception');
-        new Canvas(-100, -500);
-
+    public function testValidateSizeExceptionUpdate()
+    {
         // Adjust to a negative size for canvas dimensions
         $this->setExpectedException('\Exception');
         $canvas = new Canvas();
         $canvas->size(-100, 500);
     }
 
-    public function testValidateInterval()
+    public function testValidateIntervalSet()
     {
         // The input interval is set to single point (start = end)
         $this->setExpectedException('\Exception');
         $canvas = new Canvas();
         $canvas->addPlot(function ($x) { return 1; }, 0, 0);
+    }
 
+    public function testValidateIntervalUpdate()
+    {
         // The plot interval is adjusted to single point (start = end)
         $this->setExpectedException('\Exception');
         $canvas = new Canvas();
