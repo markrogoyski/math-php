b7acb7d5e2c3f1ad8792185dbac16d153382b2a3
jakobsandberg <jakobsandberg95@gmail.com>
2016-10-13 02:32:08 -0700

    Added test for thickness exception

--- tests/Plots/PlotTest.php
+++ tests/Plots/PlotTest.php
@@ -12,4 +12,13 @@ class PlotTest extends \PHPUnit_Framework_TestCase
         $plot = $canvas->addPlot(function ($x) { return 1; }, 0, 10);
         $plot->grid(true, -10, 5);
     }
+
+    public function testThicknessException()
+    {
+        // Giving a negative number
+        $this->setExpectedException('\Exception');
+        $canvas = new Canvas();
+        $plot = $canvas->addPlot(function ($x) { return 1; }, 0, 10);
+        $plot->thickness(-10);
+    }
 }
