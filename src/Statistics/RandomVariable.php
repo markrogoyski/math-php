cb0f42239b8758e576cdb457f8dd19333847fc6d
Mark Rogoyski <markrogoyski@gmail.com>
2016-12-13 19:38:26 +0000

    Add Kullback-Leibler divergence.

--- src/Statistics/RandomVariable.php
+++ src/Statistics/RandomVariable.php
@@ -439,4 +439,47 @@ class RandomVariable
 
         return -log($BC⟮p、q⟯);
     }
+
+    /**
+     * Kullback-Leibler divergence
+     * (also known as: discrimination information, information divergence, information gain, relative entropy, KLIC, KL divergence)
+     * A measure of the difference between two probability distributions P and Q.
+     * https://en.wikipedia.org/wiki/Kullback%E2%80%93Leibler_divergence
+     *
+     *                       P(i)
+     * Dkl(P‖Q) = ∑ P(i) log ----
+     *            ⁱ          Q(i)
+     *
+     *
+     *
+     * @param  array  $p distribution p
+     * @param  array  $q distribution q
+     *
+     * @return float difference between distributions
+     *
+     * @throws BadDataException if p and q do not have the same number of elements
+     * @throws BadDataException if p and q are not probability distributions that add up to 1
+     */
+    public static function kullbackLeiblerDivergence(array $p, array $q)
+    {
+        // Arrays must have the same number of elements
+        if (count($p) !== count($q)) {
+            throw new Exception\BadDataException('p and q must have the same number of elements');
+        }
+
+        // Probability distributions must add up to 1.0
+        if ((array_sum($p) != 1) || (array_sum($q) != 1)) {
+            throw new Exception\BadDataException('Distributions p anad q must add up to 1');
+        }
+
+        $Dkl⟮P‖Q⟯ = array_sum(array_map(
+            function ($P, $Q) use ($p, $q) {
+                return $P * log($P / $Q);
+            },
+            $p,
+            $q
+        ));
+
+        return $Dkl⟮P‖Q⟯;
+    }
 }
