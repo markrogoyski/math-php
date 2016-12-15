8fb1e5ffc56d0c5892900a5523f0c8a5a0816e29
Mark Rogoyski <markrogoyski@gmail.com>
2016-12-15 21:44:13 +0000

    Refactor entropy functions into information theory namespace.

--- src/Statistics/RandomVariable.php
+++ src/Statistics/RandomVariable.php
@@ -15,7 +15,6 @@ use MathPHP\Exception;
  * - Kurtosis
  * - Standard Error of the Mean (SEM)
  * - Confidence interval
- * - Bhattacharyya distance
  *
  * In probability and statistics, a random variable is a variable whose
  * value is subject to variations due to chance.
@@ -398,88 +397,5 @@ class RandomVariable
         ));
 
         return $∑⟮xᵢ − μ⟯²;
-    }
-
-    /**
-     * Bhattacharyya distance
-     * Measures the similarity of two discrete or continuous probability distributions.
-     * https://en.wikipedia.org/wiki/Bhattacharyya_distance
-     *
-     * For probability distributions p and q over the same domain X,
-     * the Bhattacharyya distance is defined as:
-     *
-     * DB(p,q) = -ln(BC(p,q))
-     *
-     * where BC is the Bhattacharyya coefficient:
-     *
-     * BC(p,q) = ∑ √(p(x) q(x))
-     *          x∈X
-     *
-     * @param array $p distribution p
-     * @param array $q distribution q
-     *
-     * @return float distance between distributions
-     *
-     * @throws BadDataException if p and q do not have the same number of elements
-     * @throws BadDataException if p and q are not probability distributions that add up to 1
-     */
-    public static function bhattacharyyaDistance(array $p, array $q)
-    {
-        // Arrays must have the same number of elements
-        if (count($p) !== count($q)) {
-            throw new Exception\BadDataException('p and q must have the same number of elements');
-        }
-
-        // Probability distributions must add up to 1.0
-        if ((array_sum($p) != 1) || (array_sum($q) != 1)) {
-            throw new Exception\BadDataException('Distributions p anad q must add up to 1');
-        }
-
-        $BC⟮p、q⟯ = array_sum(Map\Single::sqrt(Map\Multi::multiply($p, $q)));
-
-        return -log($BC⟮p、q⟯);
-    }
-
-    /**
-     * Kullback-Leibler divergence
-     * (also known as: discrimination information, information divergence, information gain, relative entropy, KLIC, KL divergence)
-     * A measure of the difference between two probability distributions P and Q.
-     * https://en.wikipedia.org/wiki/Kullback%E2%80%93Leibler_divergence
-     *
-     *                       P(i)
-     * Dkl(P‖Q) = ∑ P(i) log ----
-     *            ⁱ          Q(i)
-     *
-     *
-     *
-     * @param  array  $p distribution p
-     * @param  array  $q distribution q
-     *
-     * @return float difference between distributions
-     *
-     * @throws BadDataException if p and q do not have the same number of elements
-     * @throws BadDataException if p and q are not probability distributions that add up to 1
-     */
-    public static function kullbackLeiblerDivergence(array $p, array $q)
-    {
-        // Arrays must have the same number of elements
-        if (count($p) !== count($q)) {
-            throw new Exception\BadDataException('p and q must have the same number of elements');
-        }
-
-        // Probability distributions must add up to 1.0
-        if ((array_sum($p) != 1) || (array_sum($q) != 1)) {
-            throw new Exception\BadDataException('Distributions p anad q must add up to 1');
-        }
-
-        $Dkl⟮P‖Q⟯ = array_sum(array_map(
-            function ($P, $Q) use ($p, $q) {
-                return $P * log($P / $Q);
-            },
-            $p,
-            $q
-        ));
-
-        return $Dkl⟮P‖Q⟯;
     }
 }
