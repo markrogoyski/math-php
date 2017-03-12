4f55b5b6d49264568830c2d0e95846ec79ca002f
Mark Rogoyski <markrogoyski@gmail.com>
2017-03-12 22:45:58 +0000

    Add unit tests for NumberTheory perfectPower.

--- tests/NumberTheory/IntegerTest.php
+++ tests/NumberTheory/IntegerTest.php
@@ -4,7 +4,7 @@ namespace MathPHP\NumberTheory;
 class IntegerTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * @testCase     isPerfectPrime returns true if n is a perfect prime.
+     * @testCase     isPerfectPower returns true if n is a perfect prime.
      * @dataProvider dataProviderForIsPerfectPower
      * @param        int $n
      */
@@ -79,7 +79,7 @@ class IntegerTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @testCase     isPerfectPrime returns false if n is not a perfect prime.
+     * @testCase     isPerfectPower returns false if n is not a perfect prime.
      * @dataProvider dataProviderForIsNotPerfectPower
      * @param        int $n
      */
@@ -168,5 +168,81 @@ class IntegerTest extends \PHPUnit_Framework_TestCase
             [82],
             [83],
         ];
+    }
+
+    /**
+     * @testCase     perfectPower returns m and k for n such that mᵏ = n if n is a perfect power.
+     * @dataProvider dataProviderForPerfectPower
+     * @param        int $n
+     * @param        int $m
+     * @param        int $k
+     */
+    public function testPerfectPower(int $n, int $expected_m, int $expected_k)
+    {
+        list($m, $k) = Integer::perfectPower($n);
+        $this->assertEquals($expected_m, $m);
+        $this->assertEquals($expected_k, $k);
+    }
+
+    /**
+     * Perfect powers: m^k where m > 0 and k >= 2.
+     * @return array
+     */
+    public function dataProviderForPerfectPower(): array
+    {
+        return [
+            [4, 2, 2],
+            [8, 2, 3],
+            [9, 3, 2],
+            [16, 2, 4],
+            [25, 5, 2],
+            [27, 3, 3],
+            [32, 2, 5],
+            [36, 6, 2],
+            [49, 7, 2],
+            [64, 2, 6],
+            [81, 3, 4],
+            [100, 10, 2],
+            [121, 11, 2],
+            [125, 5, 3],
+            [128, 2, 7],
+            [144, 12, 2],
+            [169, 13, 2],
+            [196, 14, 2],
+            [216, 6, 3],
+            [225, 15, 2],
+            [243, 3, 5],
+            [256, 2, 8],
+            [1000, 10, 3],
+            [1024, 2, 10],
+        ];
+    }
+
+    /**
+     * @testCase     perfectPower returns a non-empty array comtaining numeric m and k both > 1 if n is a perfect power.
+     * @dataProvider dataProviderForIsPerfectPower
+     * @param        int $n
+     */
+    public function testPerfectPowerArray(int $n)
+    {
+        $perfect_power = Integer::perfectPower($n);
+        $this->assertNotEmpty($perfect_power);
+
+        $m = array_shift($perfect_power);
+        $k = array_shift($perfect_power);
+        $this->assertTrue(is_numeric($m));
+        $this->assertTrue(is_numeric($k));
+        $this->assertGreaterThan(1, $m);
+        $this->assertGreaterThan(1, $k);
+    }
+
+    /**
+     * @testCase     perfectPower returns an empty array if n is not a perfect power.
+     * @dataProvider dataProviderForIsNotPerfectPower
+     * @param        int $n
+     */
+    public function testEmptyPerfectPower(int $n)
+    {
+        $this->assertEmpty(Integer::perfectPower($n));
     }
 }
