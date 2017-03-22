5d7a9a830ffad596b4dcb3f928f88a5bb9956dcf
Mark Rogoyski <markrogoyski@gmail.com>
2017-03-22 05:10:36 +0000

    Add cubic equation.

--- src/Algebra.php
+++ src/Algebra.php
@@ -1,8 +1,12 @@
 <?php
 namespace MathPHP;
 
+use MathPHP\Arithmetic;
+
 class Algebra
 {
+    const ZERO_TOLERANCE = 0.000000000001;
+
     /**
      * Greatest common divisor - recursive Euclid's algorithm
      * The largest positive integer that divides the numbers without a remainder.
@@ -202,5 +206,137 @@ class Algebra
     public static function discriminant($a, $b, $c)
     {
         return $b**2 - (4 * $a * $c);
+    }
+
+    /**
+     * Cubic equation
+     * An equation having the form: z³ + a₂z² + a₁z + a₀ = 0
+     *
+     * The coefficient a₃ of z³ may be taken as 1 without loss of generality by dividing the entire equation through by a₃.
+     *
+     * If a₃ ≠ 0, then divide a₂, a₁, and a₀ by a₃.
+     *
+     *     3a₁ - a₂²
+     * Q ≡ ---------
+     *         9
+     *
+     *     9a₂a₁ - 27a₀ - 2a₂³
+     * R ≡ -------------------
+     *             54
+     *
+     * Polynomial discriminant D
+     * D ≡ Q³ + R²
+     *
+     * If D > 0, one root is real, and two are are complex conjugates.
+     * If D = 0, all roots are real, and at least two are equal.
+     * If D < 0, all roots are real and unequal.
+     *
+     * If D < 0:
+     *
+     *                    R
+     * Define θ = cos⁻¹  ----
+     *                   √-Q³
+     *
+     * Then the real roots are:
+     *
+     *        __      /θ\
+     * z₁ = 2√-Q cos | - | - ⅓a₂
+     *                \3/
+     *
+     *        __      /θ + 2π\
+     * z₂ = 2√-Q cos | ------ | - ⅓a₂
+     *                \   3  /
+     *
+     *        __      /θ + 4π\
+     * z₃ = 2√-Q cos | ------ | - ⅓a₂
+     *                \   3  /
+     *
+     * If D = 0 or D > 0:
+     *       ______
+     * S ≡ ³√R + √D
+     *       ______
+     * T ≡ ³√R - √D
+     *
+     * If D = 0:
+     *
+     *      -a₂   S + T
+     * z₁ = --- - -----
+     *       3      2
+     *
+     *      S + T - a₂
+     * z₂ = ----------
+     *           3
+     *
+     *      -a₂   S + T
+     * z₃ = --- - -----
+     *       3      2
+     *
+     * If D > 0:
+     *
+     *      S + T - a₂
+     * z₁ = ----------
+     *           3
+     *
+     * z₂ = Complex conjugate; therefore, NAN
+     * z₃ = Complex conjugate; therefore, NAN
+     *
+     * @param  int $a₃  z³ coefficient
+     * @param  int $a₂  z² coefficient
+     * @param  int $a₁  z  coefficient
+     * @param  int $a₀     constant coefficient
+     *
+     * @return array of roots (three real roots, or one real root and two NANs because complex numbers not yet supported)
+     *                        (If $a₃ = 0, then only two roots of quadratic equation)
+     */
+    public static function cubic($a₃, $a₂, $a₁, $a₀): array
+    {
+        if ($a₃ === 0) {
+            return self::quadratic($a₂, $a₁, $a₀);
+        }
+
+        // Take coefficient a₃ of z³ to be 1
+        $a₂ = $a₂ / $a₃;
+        $a₁ = $a₁ / $a₃;
+        $a₀ = $a₀ / $a₃;
+
+        // Intermediate variables
+        $Q = (3*$a₁ - $a₂**2) / 9;
+        $R = (9*$a₂*$a₁ - 27*$a₀ - 2*$a₂**3) / 54;
+
+        // Polynomial discriminant
+        $D = $Q**3 + $R**2;
+
+        // All roots are real and unequal
+        if ($D < 0) {
+            $θ     = acos($R / sqrt((-$Q)**3));
+            $２√−Q = 2 * sqrt(-$Q);
+            $π     = \M_PI;
+
+            $z₁    = $２√−Q * cos($θ / 3) - ($a₂ / 3);
+            $z₂    = $２√−Q * cos(($θ + 2*$π) / 3) - ($a₂ / 3);
+            $z₃    = $２√−Q * cos(($θ + 4*$π) / 3) - ($a₂ / 3);
+
+            return [$z₁, $z₂, $z₃];
+        }
+
+        // Intermediate calculations
+        $S = Arithmetic::cubeRoot($R + sqrt($D));
+        $T = Arithmetic::cubeRoot($R - sqrt($D));
+
+        // All roots are real, and at least two are equal
+        if ($D === 0 || ($D > -self::ZERO_TOLERANCE && $D < self::ZERO_TOLERANCE)) {
+            $z₁ = -$a₂ / 3 - ($S + $T) / 2;
+            $z₂ = $S + $T - $a₂ / 3;
+            $z₃ = -$a₂ / 3 - ($S + $T) / 2;
+
+            return [$z₁, $z₂, $z₃];
+        }
+
+        // One root is real, and two are are complex conjugates
+        if ($D > 0) {
+            $z₁ = $S + $T - $a₂ / 3;
+
+            return [$z₁, \NAN, \NAN];
+        }
     }
 }
