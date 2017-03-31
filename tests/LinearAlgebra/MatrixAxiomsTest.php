6732e64b012e4dabf3756cd10fc0f1eda52f5fa4
Mark Rogoyski <markrogoyski@gmail.com>
2017-03-31 06:28:16 +0000

    Add matrix axiom unit tests for positive and negative definite and semidefiniteness functions.

--- tests/LinearAlgebra/MatrixAxiomsTest.php
+++ tests/LinearAlgebra/MatrixAxiomsTest.php
@@ -57,6 +57,17 @@ namespace MathPHP\LinearAlgebra;
  *    - A⊕B = A⊗Ib + I⊗aB
  *  - Covariance matrix
  *    - S = Sᵀ
+ *  - Positive definiteness
+ *    - A is PD ⇔ -A is ND
+ *    - A is PSD ⇔ -A is NSD
+ *    - A is PD ⇒ A is PSD
+ *    - A is ND ⇒ A is NSD
+ *    - A is PD ⇔ A⁻¹ is PD
+ *    - A is ND ⇔ A⁻¹ is ND
+ *    - A is PD and r > 0 ⇒ rA is PD
+ *    - A and B are PD ⇒ A + B is PD
+ *    - A and B are PD ⇒ ABA is PD
+ *    - A and B are PD ⇒ BAB is PD
  */
 class MatrixAxiomsTest extends \PHPUnit_Framework_TestCase
 {
@@ -1734,6 +1745,7 @@ class MatrixAxiomsTest extends \PHPUnit_Framework_TestCase
      * Axiom: Covariance matrix S = Sᵀ
      * Covariance matrix is symmetric so it is the same as its transpose
      * @dataProvider dataProviderForCovarianceSymmetric
+     * @param        array $A
      */
     public function testCovarianceMatrixIsSymmetric(array $A)
     {
@@ -1772,6 +1784,504 @@ class MatrixAxiomsTest extends \PHPUnit_Framework_TestCase
                 [
                     [2.5, 0.5, 2.2, 1.9, 3.1, 2.3, 2, 1, 1.5, 1.1],
                     [2.4, 0.7, 2.9, 2.2, 3.0, 2.7, 1.6, 1.1, 1.6, 0.9],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @testCase Axiom: Positive definiteness A is PD ⇔ -A is ND
+     * If A is positive definite, then -A is negative definite.
+     * @dataProvider dataProviderForPositiveDefiniteMatrix
+     * @param        array $A
+     */
+    public function testPositiveDefiniteNegativeisNegativeDefinite(array $A)
+    {
+        $A  = MatrixFactory::create($A);
+        $−A = $A->scalarMultiply(-1);
+
+        $this->assertTrue($A->isPositiveDefinite());
+        $this->assertTrue($−A->isNegativeDefinite());
+    }
+
+    public function dataProviderForPositiveDefiniteMatrix(): array
+    {
+        return [
+            [
+                [
+                    [2, -1],
+                    [-1, 2],
+                ],
+            ],
+            [
+                [
+                    [1, -1],
+                    [-1, 4],
+                ],
+            ],
+            [
+                [
+                    [5, 2],
+                    [2, 3],
+                ],
+            ],
+            [
+                [
+                    [6, 4],
+                    [4, 5],
+                ],
+            ],
+            [
+                [
+                    [12, -12],
+                    [-12, 96],
+                ],
+            ],
+            [
+                [
+                    [2, -1, 0],
+                    [-1, 2, -1],
+                    [0, -1, 2],
+                ],
+            ],
+            [
+                [
+                    [2, -1, 1],
+                    [-1, 2, -1],
+                    [1, -1, 2],
+                ],
+            ],
+            [
+                [
+                    [1, 0, 0],
+                    [0, 3, 0],
+                    [0, 0, 2],
+                ],
+            ],
+            [
+                [
+                    [3, -2, 0],
+                    [-2, 2, 0],
+                    [0, 0, 2],
+                ],
+            ],
+            [
+                [
+                    [4, 1, -1],
+                    [1, 2, 1],
+                    [-1, 1, 2],
+                ],
+            ],
+            [
+                [
+                    [9, -3, 3, 9],
+                    [-3, 17, -1, -7],
+                    [3, -1, 17, 15],
+                    [9, -7, 15, 44],
+                ],
+            ],
+            [
+                [
+                    [14, 4, 9],
+                    [4, 14, -7],
+                    [9, -7, 14],
+                ],
+            ],
+            [
+                [
+                    [13, 0, -3],
+                    [0, 9, 9],
+                    [-3, 9, 10],
+                ],
+            ],
+            [
+                [
+                    [14, -7, -13],
+                    [-7, 6, 5],
+                    [-13, 5, 14],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @testCase Axiom: Positive semidefiniteness A is PSD ⇔ -A is NSD
+     * If A is positive semidefinite, then -A is negative definite.
+     * @dataProvider dataProviderForPositiveSemidefiniteMatrix
+     * @param        array $A
+     */
+    public function testPositiveSemidefiniteNegativeisNegativeSemidefinite(array $A)
+    {
+        $A  = MatrixFactory::create($A);
+        $−A = $A->scalarMultiply(-1);
+
+        $this->assertTrue($A->isPositiveSemidefinite());
+        $this->assertTrue($−A->isNegativeSemidefinite());
+    }
+
+    public function dataProviderForPositiveSemidefiniteMatrix(): array
+    {
+        return [
+            [
+                [
+                    [0, 0],
+                    [0, 0],
+                ],
+            ],
+            [
+                [
+                    [1, 0],
+                    [0, 1],
+                ],
+            ],
+            [
+                [
+                    [1, 0],
+                    [0, 2],
+                ],
+            ],
+            [
+                [
+                    [1, 1],
+                    [1, 1],
+                ],
+            ],
+            [
+                [
+                    [2, -1],
+                    [-1, 2],
+                ],
+            ],
+            [
+                [
+                    [0, 0, 0],
+                    [0, 3, 0],
+                    [0, 0, 3],
+                ],
+            ],
+            [
+                [
+                    [2, -1, -1],
+                    [-1, 2, -1],
+                    [-1, -1, 2],
+                ],
+            ],
+            [
+                [
+                    [2, -1, 0],
+                    [-1, 2, -1],
+                    [0, -1, 2],
+                ],
+            ],
+            [
+                [
+                    [2, -1, 1],
+                    [-1, 2, -1],
+                    [1, -1, 2],
+                ],
+            ],
+            [
+                [
+                    [2, -1, 2],
+                    [-1, 2, -1],
+                    [2, -1, 2],
+                ],
+            ],
+            [
+                [
+                    [9, -3, 3, 9],
+                    [-3, 17, -1, -7],
+                    [3, -1, 17, 15],
+                    [9, -7, 15, 44],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @testCase Axiom: Positive definiteness A is PD ⇒ A is PSD
+     * If A is positive definite, then A is also positive semidefinite.
+     * @dataProvider dataProviderForPositiveDefiniteMatrix
+     * @param        array $A
+     */
+    public function testPositiveDefiniteIsAlsoPositiveSemidefinite(array $A)
+    {
+        $A  = MatrixFactory::create($A);
+
+        $this->assertTrue($A->isPositiveDefinite());
+        $this->assertTrue($A->isPositiveSemidefinite());
+    }
+
+    /**
+     * @testCase Axiom: Negative definiteness A is ND ⇒ A is NSD
+     * If A is negative definite, then A is also negative semidefinite.
+     * @dataProvider dataProviderForNegativeDefiniteMatrix
+     * @param        array $A
+     */
+    public function testNegativeDefiniteIsAlsoNegativeSemidefinite(array $A)
+    {
+        $A  = MatrixFactory::create($A);
+
+        $this->assertTrue($A->isNegativeDefinite());
+        $this->assertTrue($A->isNegativeSemidefinite());
+    }
+
+    public function dataProviderForNegativeDefiniteMatrix(): array
+    {
+        return [
+            [
+                [
+                    [-1, 1],
+                    [1, -2],
+                ],
+            ],
+            [
+                [
+                    [-3, 0, 0],
+                    [0, -2, 0],
+                    [0, 0, -1],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @testCase Axiom: Positive definiteness A is PD ⇔ A⁻¹ is PD
+     * If A is positive definite, then A⁻¹ is positive definite.
+     * @dataProvider dataProviderForPositiveDefiniteMatrix
+     * @param        array $A
+     */
+    public function testPositiveDefiniteInverseIsPositiveDefinite(array $A)
+    {
+        $A   = MatrixFactory::create($A);
+        $A⁻¹ = $A->inverse();
+
+        // Floating point adjustment
+        $A⁻¹ = $A⁻¹->map(function ($x) {
+            return round($x, 7);
+        });
+
+        $this->assertTrue($A->isPositiveDefinite());
+        $this->assertTrue($A⁻¹->isPositiveDefinite());
+    }
+
+    /**
+     * @testCase Axiom: Negative definiteness A is ND ⇔ A⁻¹ is ND
+     * If A is negative definite, then A⁻¹ is negative definite.
+     * @dataProvider dataProviderForNegativeDefiniteMatrix
+     * @param        array $A
+     */
+    public function testNegativeDefiniteInverseIsNegativeDefinite(array $A)
+    {
+        $A   = MatrixFactory::create($A);
+        $A⁻¹ = $A->inverse();
+
+        // Floating point adjustment
+        $A⁻¹ = $A⁻¹->map(function ($x) {
+            return round($x, 7);
+        });
+
+        $this->assertTrue($A->isNegativeDefinite());
+        $this->assertTrue($A⁻¹->isNegativeDefinite());
+    }
+
+    /**
+     * @testCase Axiom: Positive definiteness A is PD and r > 0 ⇒ rA is PD
+     * If A is positive definite and r > 0, then rA is positive definite.
+     * @dataProvider dataProviderForPositiveDefiniteMatrix
+     * @param        array $A
+     */
+    public function testPositiveDefiniteThenScalarMultiplyWithPositiveNumberIsPositiveDefinite(array $A)
+    {
+        $A = MatrixFactory::create($A);
+        $this->assertTrue($A->isPositiveDefinite());
+
+        foreach (range(1, 10) as $r) {
+            $rA = $A->scalarMultiply($r);
+            $this->assertTrue($rA->isPositiveDefinite());
+        }
+        $A⁻¹ = $A->inverse();
+    }
+
+    /**
+     * @testCase Axiom: Positive definiteness A and B are PD ⇒ A + B is PD
+     * If A and B are positive definite then A + B is positive definite.
+     * @dataProvider dataProviderForPositiveDefiniteMatrix
+     * @param        array $A
+     */
+    public function testPositiveDefiniteAPlusAIsPositiveDefinite(array $M)
+    {
+        $A = MatrixFactory::create($M);
+        $B = MatrixFactory::create($M);
+        $A＋B = $A->add($B);
+
+        $this->assertTrue($A->isPositiveDefinite());
+        $this->assertTrue($B->isPositiveDefinite());
+        $this->assertTrue($A＋B->isPositiveDefinite());
+    }
+
+    /**
+     * @testCase Axiom: Positive definiteness A and B are PD ⇒ A + B is PD
+     * If A and B are positive definite then A + B is positive definite.
+     * @dataProvider dataProviderForTwoPositiveDefiniteMatrixes
+     * @param  array $A
+     * @param  array $B
+     */
+    public function testPositiveDefiniteAPlusBIsPositiveDefinite(array $A, array $B)
+    {
+        $A = MatrixFactory::create($A);
+        $B = MatrixFactory::create($B);
+        $A＋B = $A->add($B);
+
+        $this->assertTrue($A->isPositiveDefinite());
+        $this->assertTrue($B->isPositiveDefinite());
+        $this->assertTrue($A＋B->isPositiveDefinite());
+    }
+
+    /**
+     * @testCase Axiom: Positive definiteness A and B are PD ⇒ ABA is PD
+     * If A and B are positive definite then ABA is positive definite.
+     * @dataProvider dataProviderForPositiveDefiniteMatrix
+     * @param  array $A
+     */
+    public function testPositiveDefiniteAAAIsPositiveDefinite(array $M)
+    {
+        $A = MatrixFactory::create($M);
+        $B = MatrixFactory::create($M);
+        $ABA = $A->multiply($B)->multiply($A);
+
+        $this->assertTrue($A->isPositiveDefinite());
+        $this->assertTrue($B->isPositiveDefinite());
+        $this->assertTrue($ABA->isPositiveDefinite());
+    }
+
+    /**
+     * @testCase Axiom: Positive definiteness A and B are PD ⇒ ABA is PD
+     * If A and B are positive definite then ABA is positive definite.
+     * @dataProvider dataProviderForTwoPositiveDefiniteMatrixes
+     * @param  array $A
+     * @param  array $B
+     */
+    public function testPositiveDefiniteABAIsPositiveDefinite(array $A, array $B)
+    {
+        $A = MatrixFactory::create($A);
+        $B = MatrixFactory::create($B);
+        $ABA = $A->multiply($B)->multiply($A);
+
+        $this->assertTrue($A->isPositiveDefinite());
+        $this->assertTrue($B->isPositiveDefinite());
+        $this->assertTrue($ABA->isPositiveDefinite());
+    }
+
+    /**
+     * @testCase Axiom: Positive definiteness A and B are PD ⇒ BAB is PD
+     * If A and B are positive definite then BAB is positive definite.
+     * @dataProvider dataProviderForTwoPositiveDefiniteMatrixes
+     * @param  array $A
+     * @param  array $B
+     */
+    public function testPositiveDefiniteBABIsPositiveDefinite(array $A, array $B)
+    {
+        $A = MatrixFactory::create($A);
+        $B = MatrixFactory::create($B);
+        $BAB = $B->multiply($A)->multiply($B);
+
+        $this->assertTrue($A->isPositiveDefinite());
+        $this->assertTrue($B->isPositiveDefinite());
+        $this->assertTrue($BAB->isPositiveDefinite());
+    }
+
+    public function dataProviderForTwoPositiveDefiniteMatrixes(): array
+    {
+        return [
+            [
+                [
+                    [2, -1],
+                    [-1, 2],
+                ],
+                [
+                    [1, -1],
+                    [-1, 4],
+                ],
+            ],
+            [
+                [
+                    [5, 2],
+                    [2, 3],
+                ],
+                [
+                    [6, 4],
+                    [4, 5],
+                ],
+            ],
+            [
+                [
+                    [12, -12],
+                    [-12, 96],
+                ],
+                [
+                    [6, 4],
+                    [4, 5],
+                ],
+            ],
+            [
+                [
+                    [2, -1, 0],
+                    [-1, 2, -1],
+                    [0, -1, 2],
+                ],
+                [
+                    [2, -1, 1],
+                    [-1, 2, -1],
+                    [1, -1, 2],
+                ],
+            ],
+            [
+                [
+                    [1, 0, 0],
+                    [0, 3, 0],
+                    [0, 0, 2],
+                ],
+                [
+                    [3, -2, 0],
+                    [-2, 2, 0],
+                    [0, 0, 2],
+                ],
+            ],
+            [
+                [
+                    [4, 1, -1],
+                    [1, 2, 1],
+                    [-1, 1, 2],
+                ],
+                [
+                    [3, -2, 0],
+                    [-2, 2, 0],
+                    [0, 0, 2],
+                ],
+            ],
+            [
+                [
+                    [14, 4, 9],
+                    [4, 14, -7],
+                    [9, -7, 14],
+                ],
+                [
+                    [13, 0, -3],
+                    [0, 9, 9],
+                    [-3, 9, 10],
+                ],
+            ],
+            [
+                [
+                    [14, -7, -13],
+                    [-7, 6, 5],
+                    [-13, 5, 14],
+                ],
+                [
+                    [13, 0, -3],
+                    [0, 9, 9],
+                    [-3, 9, 10],
                 ],
             ],
         ];
