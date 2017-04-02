0abc99f1395dc401959542ae0f0ac541120b3ebc
Mark Rogoyski <markrogoyski@gmail.com>
2017-04-02 04:08:17 +0000

    Add digitSum and digitalRoot.

--- src/Arithmetic.php
+++ src/Arithmetic.php
@@ -20,4 +20,56 @@ class Arithmetic
 
         return -pow(abs($x), 1/3);
     }
+
+    /**
+     * Digit sum
+     * Sum of all an integer's digits.
+     * https://en.wikipedia.org/wiki/Digit_sum
+     *
+     * log x  1
+     *   ∑    -- (x mod bⁿ⁺¹ - x mod bⁿ)
+     *  ⁿ⁼⁰   bⁿ
+     *
+     * Example (base 10): 5031   = 5 + 0 + 3 + 1 = 9
+     * Example (base 2):  0b1010 = 1 + 0 + 1 + 0 = 2
+     *
+     * @param  int $x
+     * @param  int $b Base (Default is base 10)
+     *
+     * @return int
+     */
+    public static function digitSum(int $x, int $b = 10): int
+    {
+        $logx                        = log($x, $b);
+        $∑1／bⁿ⟮x mod bⁿ⁺¹ − x mod bⁿ⟯ = 0;
+
+        for ($n = 0; $n <= $logx; $n++) {
+            $∑1／bⁿ⟮x mod bⁿ⁺¹ − x mod bⁿ⟯ += (($x % pow($b, $n+1)) - ($x % $b**$n)) / ($b**$n);
+        }
+
+        return $∑1／bⁿ⟮x mod bⁿ⁺¹ − x mod bⁿ⟯;
+    }
+
+    /**
+     * Digital root (iterated digit sum, repeated digital sum)
+     * The single digit value obtained by an iterative process of summing digits,
+     * on each iteration using the result from the previous iteration to compute a digit sum.
+     * The process continues until a single-digit number is reached.
+     * https://en.wikipedia.org/wiki/Digital_root
+     *
+     * Example: 65,536 is 7, because 6 + 5 + 5 + 3 + 6 = 25 and 2 + 5 = 7
+     *
+     * @param  int    $x [description]
+     * @return [type]    [description]
+     */
+    public static function digitalRoot(int $x): int
+    {
+        $root = $x;
+
+        while ($root >= 10) {
+            $root = self::digitSum($root);
+        }
+
+        return $root;
+    }
 }
