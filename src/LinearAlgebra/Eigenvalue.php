0aae32c1eeb8af4d96241b72453bdfcb94571f06
Mark Rogoyski <markrogoyski@gmail.com>
2017-04-05 05:52:34 +0000

    Refactor Eigenvalue and add additional unit tests.

--- src/LinearAlgebra/Eigenvalue.php
+++ src/LinearAlgebra/Eigenvalue.php
@@ -6,7 +6,7 @@ use MathPHP\Exception;
 
 class Eigenvalue
 {
-    /*
+    /**
      * Produces the Eigenvalues for a 2x2 or 3x3 matrix
      *
      * Given a matrix
@@ -18,35 +18,49 @@ class Eigenvalue
      *      [c,    d-λ] = 0
      *
      * or ad - λ(a+d) + λ² - cb = 0
+     *
+     * @param Matrix $A
+     *
+     * @return DiagonalMatrix of eigenvalues
+     *
+     * @throws Exception\BadDataException if the matrix is not square
+     * @throws Exception\BadDataException if the matrix is not 2x2 or 3x3
      */
-    public static function quadratic(Matrix $M): Matrix
+    public static function quadratic(Matrix $A): DiagonalMatrix
     {
-        if ($M->getN() != $M->getM() || ($M->getM() != 2 && $M->getM() != 3)) {
-            throw new Exception\BadDataException("Matrix must be 2x2 or 3x3");
+        if (!$A->isSquare()) {
+            throw new Exception\BadDataException('Matrix must be square');
         }
-        $A = $M->getMatrix();
-        if ($M->getM() == 2) {
+
+        $m = $A->getM();
+        if ($m < 2 || $m > 3) {
+            throw new Exception\BadDataException("Matrix must be 2x2 or 3x3. $m x $m given");
+        }
+
+        $A = $A->getMatrix();
+
+        if ($m === 2) {
             $a = -1;
             $b = $A[0][0] + $A[1][1];
             $c = $A[1][0] * $A[0][1] - $A[0][0] * $A[1][1];
             $eigenvalues = Algebra::quadratic($a, $b, $c);
         } else {
-            $a = $A[0][0];
-            $b = $A[0][1];
-            $c = $A[0][2];
-            $d = $A[1][0];
-            $e = $A[1][1];
-            $f = $A[1][2];
-            $g = $A[2][0];
-            $h = $A[2][1];
-            $i = $A[2][2];
+            $a  = $A[0][0];
+            $b  = $A[0][1];
+            $c  = $A[0][2];
+            $d  = $A[1][0];
+            $e  = $A[1][1];
+            $f  = $A[1][2];
+            $g  = $A[2][0];
+            $h  = $A[2][1];
+            $i  = $A[2][2];
             $qa = -1;
             $qb = $a + $e + $i;
             $qc = $c * $g + $h * $f + $d * $b - $a * $e - $a * $i - $e * $i;
             $qd = $a * $e * $i + $b * $f * $g + $c * $d * $h - $g * $c * $e - $h * $f * $a - $d * $b * $i;
             $eigenvalues = Algebra::cubic($qa, $qb, $qc, $qd);
         }
-        // return a diagonal matrix
+
         return MatrixFactory::create($eigenvalues);
     }
 }
--- tests/LinearAlgebra/EigenvalueTest.php
+++ tests/LinearAlgebra/EigenvalueTest.php
@@ -1,15 +1,17 @@
 <?php
 namespace MathPHP\LinearAlgebra;
 
+use MathPHP\Exception;
+
 class EigenvalueTest extends \PHPUnit_Framework_TestCase
 {
     /**
      * @dataProvider dataProviderForEigenvalues
      */
-    public function testEigenvalues(array $A, array $S)
+    public function testQuadratic(array $A, array $S)
     {
         $A = MatrixFactory::create($A);
-        $this->assertEquals($S, Eigenvalue::quadratic($A)->getMatrix(), '', 0.0001);
+        $this->assertEquals($S, Eigenvalue::quadratic($A)->getDiagonalElements(), '', 0.0001);
     }
 
     public function dataProviderForEigenvalues(): array
@@ -20,10 +22,21 @@ class EigenvalueTest extends \PHPUnit_Framework_TestCase
                     [0, 1],
                     [-2, -3],
                 ],
+                [-1, -2],
+            ],
+            [
                 [
-                    [-1, 0],
-                    [0, -2],
+                    [6, -1],
+                    [2, 3],
                 ],
+                [5, 4],
+            ],
+            [
+                [
+                    [1, -2],
+                    [-2, 0],
+                ],
+                [(1 + sqrt(17))/2, (1 - sqrt(17))/2],
             ],
             [
                 [
@@ -31,10 +44,75 @@ class EigenvalueTest extends \PHPUnit_Framework_TestCase
                     [-2, 1, 2],
                     [4, 2, 5],
                 ],
+                [6, -5, 3],
+            ],
+            [
                 [
-                    [6, 0, 0],
-                    [0, -5, 0],
-                    [0, 0, 3],
+                    [2, 0, 0],
+                    [1, 2, 1],
+                    [-1, 0, 1],
+                ],
+                [2, 1, 2],
+            ],
+            [
+                [
+                    [1, 2, 1],
+                    [6, -1, 0],
+                    [-1, -2, -1],
+                ],
+                [3, -4, 0],
+            ],
+            [
+                [
+                    [1, 2, 3],
+                    [4, 5, 6],
+                    [7, 8, 9],
+                ],
+                [(3*(5 + sqrt(33)))/2, (-3*(sqrt(33) - 5))/2, 0],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderForQuadraticException
+     */
+    public function testQuadraticExceptionMatrixNotCorrectSize(array $A)
+    {
+        $A = MatrixFactory::create($A);
+
+        $this->setExpectedException(Exception\BadDataException::class);
+        Eigenvalue::quadratic($A);
+    }
+
+    public function dataProviderForQuadraticException(): array
+    {
+        return [
+            '1x1' => [
+                [
+                    [1],
+                ],
+            ],
+            '4x4' => [
+                [
+                    [1, 2, 3, 4],
+                    [2, 3, 4, 5],
+                    [3, 4, 5, 6],
+                    [4, 5, 6, 7],
+                ]
+            ],
+            '5x5' => [
+                [
+                    [1, 2, 3, 4, 5],
+                    [2, 3, 4, 5, 6],
+                    [3, 4, 5, 6, 7],
+                    [4, 5, 6, 7, 8],
+                    [5, 6, 7, 8, 9],
+                ]
+            ],
+            'not_square' => [
+                [
+                    [1, 2, 3],
+                    [2, 3, 4],
                 ],
             ],
         ];
