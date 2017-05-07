a23c536ec684b646c0bd31e0f13d5f81d0c95fa4
Kevin Nowaczyk <beakerboy99@yahoo.com>
2017-05-07 18:38:22 -0400

    Create MatrixObjectInterface.php

---
+++ src/Number/MatrixObjectInterface.php
@@ -0,0 +1,9 @@
+<?php
+namespace MathPHP\Number;
+
+interface MatrixObjectInterface
+{
+    public function add($object);
+    public function subtract($object);
+    public function multiply($object);
+}
