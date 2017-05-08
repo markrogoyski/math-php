13baa8bf43d1dd699840a9763b30aa91069dcc05
Kevin Nowaczyk <beakerboy99@yahoo.com>
2017-05-08 08:02:44 -0400

    Changed interface name and added comments

---
+++ src/Number/ObjectArithmetic.php
@@ -0,0 +1,32 @@
+<?php
+namespace MathPHP\Number;
+
+interface ObjectArithmetic
+{
+    /**
+     * Add two objects together
+     *
+     * @param mixed $object_or_scalar the value to be added
+     *
+     * @return ObjectArithmetic sum.
+     */
+    public function add($object_or_scaler);
+
+    /*
+     * Subtract one objects from another
+     *
+     * @param mixed $object_or_scalar the value to be subtracted
+     *
+     * @return ObjectArithmetic result.
+     */
+    public function subtract($object_or_scaler);
+
+    /*
+     * Multiply two objects together
+     *
+     * @param mixed $object_or_scalar value to be multiplied
+     *
+     * @return ObjectArithmetic product.
+     */
+    public function multiply($object_or_scaler);
+}
