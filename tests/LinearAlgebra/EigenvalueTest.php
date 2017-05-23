43caad34a31ab554e1b8b8b9d3ce54e7c9f6c2b5
Kevin Nowaczyk <beakerboy99@yahoo.com>
2017-05-23 15:34:11 -0400

    Update EigenvalueTest.php

--- tests/LinearAlgebra/EigenvalueTest.php
+++ tests/LinearAlgebra/EigenvalueTest.php
@@ -1,9 +1,9 @@
 <?php
 namespace MathPHP\Tests\LinearAlgebra;
 
+use MathPHP\Exception;
 use MathPHP\LinearAlgebra\MatrixFactory;
 use MathPHP\LinearAlgebra\Eigenvalue;
-use MathPHP\Exception;
 
 class EigenvalueTest extends \PHPUnit_Framework_TestCase
 {
@@ -11,10 +11,10 @@ class EigenvalueTest extends \PHPUnit_Framework_TestCase
      * @testCase     quadratic returns the expected eigenvalues
      * @dataProvider dataProviderForEigenvalues
      */
-    public function testQuadratic(array $A, array $S)
+    public function testEigenvalues(array $A, array $S)
     {
         $A = MatrixFactory::create($A);
-        $this->assertEquals($S, Eigenvalue::quadratic($A)->getDiagonalElements(), '', 0.0001);
+        $this->assertEquals($S, Eigenvalue::eigenvalue($A), '', 0.0001);
     }
 
     public function dataProviderForEigenvalues(): array
@@ -25,21 +25,21 @@ class EigenvalueTest extends \PHPUnit_Framework_TestCase
                     [0, 1],
                     [-2, -3],
                 ],
-                [-1, -2],
+                [-2, -1],
             ],
             [
                 [
                     [6, -1],
                     [2, 3],
                 ],
-                [5, 4],
+                [4, 5],
             ],
             [
                 [
                     [1, -2],
                     [-2, 0],
                 ],
-                [(1 + sqrt(17))/2, (1 - sqrt(17))/2],
+                [(1 - sqrt(17))/2, (1 + sqrt(17))/2],
             ],
             [
                 [
@@ -78,31 +78,23 @@ class EigenvalueTest extends \PHPUnit_Framework_TestCase
 
     /**
      * @testCase     quadratic throws a BadDataException if the matrix is not the correct size (2x2 or 3x3)
-     * @dataProvider dataProviderForQuadraticException
+     * @dataProvider dataProviderForEigenvalueException
      */
     public function testQuadraticExceptionMatrixNotCorrectSize(array $A)
     {
         $A = MatrixFactory::create($A);
 
-        $this->expectException(Exception\BadDataException::class);
-        Eigenvalue::quadratic($A);
+        $this->setExpectedException(Exception\BadDataException::class);
+        Eigenvalue::eigenvalue($A);
     }
 
-    public function dataProviderForQuadraticException(): array
+    public function dataProviderForEigenvalueException(): array
     {
         return [
             '1x1' => [
                 [
                     [1],
                 ],
-            ],
-            '4x4' => [
-                [
-                    [1, 2, 3, 4],
-                    [2, 3, 4, 5],
-                    [3, 4, 5, 6],
-                    [4, 5, 6, 7],
-                ]
             ],
             '5x5' => [
                 [
@@ -118,6 +110,102 @@ class EigenvalueTest extends \PHPUnit_Framework_TestCase
                     [1, 2, 3],
                     [2, 3, 4],
                 ],
+            ],
+        ];
+    }
+
+    /**
+     * @testCase     quadratic returns the expected eigenvalues
+     * @dataProvider dataProviderForEigenvector
+     */
+    public function testEigenvectors(array $A, array $S)
+    {
+        $A = MatrixFactory::create($A);
+        $this->assertEquals($S, Eigenvalue::eigenvector($A)->getMatrix(), '', 0.0001);
+    }
+
+    public function dataProviderForEigenvector(): array
+    {
+        return [
+            [
+                [
+                    [0, 1],
+                    [-2, -3],
+                ],
+                [
+                    [1/sqrt(5), \M_SQRT1_2],
+                    [-2/sqrt(5), -\M_SQRT1_2],
+                ]
+            ],
+            [
+                [
+                    [6, -1],
+                    [2, 3],
+                ],
+                [
+                    [1/sqrt(5), \M_SQRT1_2],
+                    [2/sqrt(5), \M_SQRT1_2],
+                ]
+            ],
+            [
+                [
+                    [-2, -4, 2],
+                    [-2, 1, 2],
+                    [4, 2, 5],
+                ],
+                [
+                    [1/sqrt(293), 2/sqrt(6), 2/sqrt(14)],
+                    [6/sqrt(293), 1/sqrt(6), -3/sqrt(14)],
+                    [16/sqrt(293), -1/sqrt(6), -1/sqrt(14)],
+                ]
+            ],
+            [ // RREF is a zero matrix
+                [
+                    [1, 0, 0],
+                    [0, 1, 0],
+                    [0, 0, 1],
+                ],
+                [
+                    [1, 0, 0],
+                    [0, 1, 0],
+                    [0, 0, 1],
+                ]
+            ],
+            [ // Matrix has duplicate eigenvalues. One vector is on an axis.
+                [
+                    [2, 0, 1],
+                    [2, 1, 2],
+                    [3, 0, 4],
+                ],
+                [
+                    [0, 1/sqrt(14), \M_SQRT1_2],
+                    [1, 2/sqrt(14), 0],
+                    [0, 3/sqrt(14), -1 * \M_SQRT1_2],
+                ]
+            ],
+            [ // Matrix has duplicate eigenvalues. no solution on the axis
+                [
+                    [2, 2, -3],
+                    [2, 5, -6],
+                    [3, 6, -8],
+                ],
+                [
+                    [1/\M_SQRT3, 1/sqrt(14), 5/sqrt(42)],
+                    [1/\M_SQRT3, 2/sqrt(14), -4/sqrt(42)],
+                    [1/\M_SQRT3, 3/sqrt(14), -1/sqrt(42)],
+                ]
+            ],
+            [ // The top row of the rref has a solitary 1 in position 0,0
+                [
+                    [4, 1, 2],
+                    [0, 0, -2],
+                    [2, 2, 5],
+                ],
+                [
+                    [ 5/sqrt(65), 0, 1/3],
+                    [-2/sqrt(65), -2/sqrt(5), 2/3],
+                    [6/sqrt(65), 1/sqrt(5), -2/3],
+                ]
             ],
         ];
     }
