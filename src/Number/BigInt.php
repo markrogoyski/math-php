b393089bccef17da2aa3092235ff1c264bb4f85e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2017-06-05 17:08:08 -0400

    Update BigInt.php

--- src/Number/BigInt.php
+++ src/Number/BigInt.php
@@ -149,8 +149,9 @@ class BigInt implements ObjectArithmetic
      */
     private function bitwiseAdd(int $a, int $b): array
     {
-        if (is_int($a + $b)) {
-            return ['overflow'=> false, 'value' => $a + $b];
+        if (is_int($a + $b) && $a >= 0 || $b >= 0) {
+            $sum = $a + $b;
+            return ['overflow'=> $a < 0 || $b < 0 && $sum >= 0, 'value' => $sum];
         } else {
             $c = (\PHP_INT_MAX - $a - $b) * -1 + \PHP_INT_MIN;
             return ['overflow'=> true, 'value' => $c];
