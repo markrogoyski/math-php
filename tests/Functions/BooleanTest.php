c79cb09fd1c5bd6399644bae0897d542d12e9448
Kevin Nowaczyk <beakerboy99@yahoo.com>
2017-06-05 19:43:06 -0400

    Create BooleanTest.php

---
+++ tests/Functions/BooleanTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace MathPHP\Tests\Functions;
+
+use MathPHP\Functions\Support;
+use MathPHP\Exception;
+
+class BooleanTest extends \PHPUnit_Framework_TestCase
+{
+    public function testBooleanAdd()
+    {
+    }
+
+    public function dataProviderForBooleanADD()
+    {
+    }
+}
