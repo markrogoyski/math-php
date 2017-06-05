01c78d96f52e312c3e09c31fcd94b7ceae09a655
Kevin Nowaczyk <beakerboy99@yahoo.com>
2017-06-05 19:31:53 -0400

    Update Support.php

--- src/Functions/Support.php
+++ src/Functions/Support.php
@@ -112,18 +112,4 @@ class Support
     {
         return ($x != 0 && abs($x) > self::ε);
     }
-    
-    /**
-     * Bitwise add two ints and return the result and if it overflows.
-     */
-    public static function bitwiseAdd(int $a, int $b): array
-    {
-        if (is_int($a + $b) && ($a >= 0 || $b >= 0)) {
-            $sum = $a + $b;
-            return ['overflow'=> ($a < 0 || $b < 0) && $sum >= 0, 'value' => $sum];
-        } else {
-            $c = (\PHP_INT_MAX - $a - $b) * -1 + \PHP_INT_MIN;
-            return ['overflow'=> true, 'value' => $c];
-        }
-    }
 }
