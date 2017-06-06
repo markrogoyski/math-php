3cde3987d7242f7fe5c0b373d513e19619091582
Kevin Nowaczyk <beakerboy99@yahoo.com>
2017-06-06 09:22:25 -0400

    Update Boolean.php

--- src/Functions/Boolean.php
+++ src/Functions/Boolean.php
@@ -34,7 +34,7 @@ class Boolean
             $sum = $a + $b;
             return ['overflow'=> !($a > 0 && $b > 0), 'value' => $sum];
         } elseif ($a > 0 && $b > 0) {
-            $c = (\PHP_INT_MAX - $a - $b) * -1 + \PHP_INT_MIN;
+            $c = $a - \PHP_INT_MAX + $b - 1 + \PHP_INT_MIN;
             return ['overflow'=> false, 'value' => $c];
         } else {
             $a = $a + \PHP_INT_MAX + 1;
