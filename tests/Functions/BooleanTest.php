94556849cfc07fc30ff227b38b5cf0296d8540f9
Kevin Nowaczyk <beakerboy99@yahoo.com>
2017-06-05 22:14:16 -0400

    Update BooleanTest.php

--- tests/Functions/BooleanTest.php
+++ tests/Functions/BooleanTest.php
@@ -9,22 +9,27 @@ class BooleanTest extends \PHPUnit_Framework_TestCase
     /**
      * @dataProvider dataProviderForBooleanAdd
      */
-    public function testBooleanAdd(int $a, int $b, array $expected)
+    public function testBooleanAdd(int $a, int $b, array $e)
     {
         $results = Boolean::bitwiseAdd($a, $b);
+        $ezpected = ['overflow'=> $e[0], 'value'=>$e[1]];
+        $this->assertEquals($expected, $results);
     }
 
     public function dataProviderForBooleanAdd()
     {
         return [
             [
-                1, 1, [false, 2]
+                0b01, 0b01, [false, 0b10]
             ],
             [
-                1, -1, [true, 0]
+                0b1, -1, [true, 0]
             ],
             [
-                \PHP_INT_MAX, 1, [false, \PHP_INT_MIN]
+                \PHP_INT_MAX, 0b1, [false, \PHP_INT_MIN]
+            ],
+            [
+                -1, -1, [true, -2]
             ],
         ];
     }
