03efb6549ec1eac5682644bb7fc67f2068d153db
Kevin Nowaczyk <beakerboy99@yahoo.com>
2017-06-06 09:02:14 -0400

    Update Boolean.php

--- src/Functions/Boolean.php
+++ src/Functions/Boolean.php
@@ -6,7 +6,27 @@ class Boolean
 {
 
     /**
-     * Bitwise add two ints and return the result and if it overflows.
+     * Add two ints ignoring the signing bit.
+     *
+     * 8 bit examples:
+     * 0d15 + 0d1 = 0d16
+     * 0b00001111 + 0b00000001 = 0b00010000
+     *
+     * 0d127 + 0d1 = 0d-128
+     * 0b01111111 + 0b00000001 = 0b10000000
+     *
+     * 0d-1 + 0d1 = 0d0
+     * 0b11111111 + 0b00000001 = 0b00000000 :overflow = true
+     *
+     * 0d-1 + 0d-1 = 0d-2
+     * 0b11111111 + 0b11111111 = 0b11111110: overflow = true
+     *
+     * @param int $a
+     * @param int $b
+     *
+     * @return array
+     *    'overflow' is true if the result is larger than the bits in an int
+     *    'value' is the result of the addition ignoring any overflow.
      */
     public static function bitwiseAdd(int $a, int $b): array
     {
