c2ba32933c04a33210c6e0b8574d1010e56764ea
Kevin Nowaczyk <beakerboy99@yahoo.com>
2017-06-06 13:47:15 -0400

    Update BigInt.php

--- src/Number/BigInt.php
+++ src/Number/BigInt.php
@@ -110,7 +110,7 @@ class BigInt implements ObjectArithmetic
     public function toInt(): int
     {
         // If the sign of the two values are the same, 
-        if ($this->value[1] >= 0 === $this->value[0] >= 0 && ($this->value[1] == -1 || $this->value[1] ==0) {
+        if ($this->value[1] >= 0 === $this->value[0] >= 0 && ($this->value[1] == -1 || $this->value[1] == 0)) {
             return $this->value[0];
         } else {
             return \NAN;
