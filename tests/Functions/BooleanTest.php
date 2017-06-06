33b0b30f00b4d8826d8ca7450a0ca8706df46665
Kevin Nowaczyk <beakerboy99@yahoo.com>
2017-06-05 20:17:25 -0400

    Update BooleanTest.php

--- tests/Functions/BooleanTest.php
+++ tests/Functions/BooleanTest.php
@@ -2,26 +2,29 @@
 
 namespace MathPHP\Tests\Functions;
 
-use MathPHP\Functions\Support;
-use MathPHP\Exception;
+use MathPHP\Functions\Boolean;
 
 class BooleanTest extends \PHPUnit_Framework_TestCase
 {
-    public function testBooleanAdd()
+    /**
+     * @dataProvider dataProviderForBooleanAdd
+     */
+    public function testBooleanAdd(int $a, int $b, array $expected)
     {
+        $results = Boolean::booleanAdd($a, $b);
     }
 
-    public function dataProviderForBooleanADD()
+    public function dataProviderForBooleanAdd()
     {
         return [
             [
-                1, 1, false, 2
+                1, 1, [false, 2]
             ],
             [
-                1, -1, true, 0
+                1, -1, [true, 0]
             ],
             [
-                \PHP_INT_MAX, 1, false, \PHP_INT_MIN
+                \PHP_INT_MAX, 1, [false, \PHP_INT_MIN]
             ],
         ];
     }
