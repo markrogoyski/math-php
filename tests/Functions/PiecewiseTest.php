d98ca497d55ad8ef581ccbc6c18c2bf35319369c
Mark Rogoyski <markrogoyski@gmail.com>
2017-07-06 05:13:26 +0000

    Add/reorganize Piecewise unit tests.

--- tests/Functions/PiecewiseTest.php
+++ tests/Functions/PiecewiseTest.php
@@ -10,18 +10,24 @@ class PiecewiseTest extends \PHPUnit_Framework_TestCase
     /**
      * @dataProvider dataProviderForEval
      */
-    public function testEval(array $intervals, array $polynomial_args, $input, $expected)
+    public function testEval(array $intervals, array $polynomial_args, array $inputs, array $expected)
     {
+        if (count($inputs) !== count($expected)) {
+            $this->fail('Number of inputs and expected outputs must match');
+        }
+
         $functions = array_map(
             function ($args) {
                 return new Polynomial($args);
             },
             $polynomial_args
         );
-
         $piecewise = new Piecewise($intervals, $functions);
-        $evaluated = $piecewise($input);
-        $this->assertEquals($expected, $evaluated);
+
+        $n = count($inputs);
+        for ($i = 0; $i < $n; $i++) {
+            $this->assertEquals($expected[$i], $piecewise($inputs[$i]));
+        }
     }
 
     public function dataProviderForEval()
@@ -35,9 +41,22 @@ class PiecewiseTest extends \PHPUnit_Framework_TestCase
                 [
                     [1, 0],       // new Polynomial([1, 0])  // f(x) = x
                 ],
-                25, 25            // p(25) = f(25) = 25
+                [
+                    -100, // p(-100) = f(-100) = -100
+                    0,    // p(0) = f(0) = 0
+                    1,    // p(1) = f(1) = 1
+                    25,   // p(25) = f(25) = 25
+                    100,  // p(100) = f(100) = 100
+                ],
+                [
+                    -100, // p(-100) = f(-100) = -100
+                    0,    // p(0) = f(0) = 0
+                    1,    // p(1) = f(1) = 1
+                    25,   // p(25) = f(25) = 25
+                    100,  // p(100) = f(100) = 100
+                ],
             ],
-            // Test eveluation in 1st piece given 3 intervals, functions
+            // Test evaluation in 3 intervals, functions
             [
                 [
                     [-100, -2, false, true], // f interval: [-100, -2)
@@ -49,37 +68,32 @@ class PiecewiseTest extends \PHPUnit_Framework_TestCase
                     [2],     // new Polynomial([2]),          // g(x) = 2
                     [1, 0],  // new Polynomial([1, 0])        // h(x) = x
                 ],
-                -27, 27      // p(-27) = f(-27) = -(-27) = 27
+                [
+                    -27,   // p(-27) = f(-27) = -(-27) = 27
+                    -3,    // p(-3) = f(-3) = -(-3) = 3
+                    -2,    // p(-2) = g(-2) = 2
+                    -1,    // p(-1) = g(-1) = 2
+                    0,     // p(0) = g(0) = 2
+                    1,     // p(1) = g(1) = 2
+                    2,     // p(2) = g(2) = 2
+                    3,     // p(3) = h(3) = 3
+                    20,    // p(20) = h(20) = 20
+                    100,   // p(100) = h(100) = 100
+                ],
+                [
+                    27,    // p(-27) = f(-27) = -(-27) = 27
+                    3,     // p(-3) = f(-3) = -(-3) = 3
+                    2,     // p(-2) = g(-2) = 2
+                    2,     // p(-1) = g(-1) = 2
+                    2,     // p(0) = g(0) = 2
+                    2,     // p(1) = g(1) = 2
+                    2,     // p(2) = g(2) = 2
+                    3,     // p(3) = h(3) = 3
+                    20,    // p(20) = h(20) = 20
+                    100,   // p(100) = h(100) = 100
+                ]
             ],
-            // Test eveluation in 2nd piece given 3 intervals, functions
-            [
-                [
-                    [-100, -2, false, true], // f interval: [-100, -2)
-                    [-2, 2],                 // g interval: [-2, 2]
-                    [2, 100, true, false]    // h interval: (2, 100]
-                ],
-                [
-                    [-1, 0], // new Polynomial([-1, 0]),      // f(x) = -x
-                    [2],     // new Polynomial([2]),          // g(x) = 2
-                    [1, 0],  // new Polynomial([1, 0])        // h(x) = x
-                ],
-                1, 2         // p(1) = g(1) = 2
-            ],
-            // Test eveluation in 3rd piece given 3 intervals, functions
-            [
-                [
-                    [-100, -2, false, true], // f interval: [-100, -2)
-                    [-2, 2],                 // g interval: [-2, 2]
-                    [2, 100, true, false]    // h interval: (2, 100]
-                ],
-                [
-                    [-1, 0], // new Polynomial([-1, 0]),      // f(x) = -x
-                    [2],     // new Polynomial([2]),          // g(x) = 2
-                    [1, 0],  // new Polynomial([1, 0])        // h(x) = x
-                ],
-                20, 20       // p(20) = h(20) = 20
-            ],
-            // Test eveluation at discountinuous, intermediate point
+            // Test evaluation of 3 intervals, and at discountinuous, intermediate point
             [
                 [
                     [-100, -2, false, true], // f interval: [-100, -2)
@@ -91,9 +105,32 @@ class PiecewiseTest extends \PHPUnit_Framework_TestCase
                     [100],   // new Polynomial([2]),          // g(x) = 100
                     [1, 0],  // new Polynomial([1, 0])        // h(x) = x
                 ],
-                2, 100       // p(2) = h(2) = 100
+                [
+                    -27,   // p(-27) = f(-27) = -(-27) = 27
+                    -3,    // p(-3) = f(-3) = -(-3) = 3
+                    -2,    // p(-2) = g(-2) = 100
+                    -1,    // p(-1) = g(-1) = 100
+                    0,     // p(0) = g(0) = 100
+                    1,     // p(1) = g(1) = 100
+                    2,     // p(2) = g(2) = 100
+                    3,     // p(3) = h(3) = 3
+                    20,    // p(20) = h(20) = 20
+                    100,   // p(100) = h(100) = 100
+                ],
+                [
+                    27,    // p(-27) = f(-27) = -(-27) = 27
+                    3,     // p(-3) = f(-3) = -(-3) = 3
+                    100,   // p(-2) = g(-2) = 2
+                    100,   // p(-1) = g(-1) = 2
+                    100,   // p(0) = g(0) = 2
+                    100,   // p(1) = g(1) = 2
+                    100,   // p(2) = g(2) = 100
+                    3,     // p(3) = h(3) = 3
+                    20,    // p(20) = h(20) = 20
+                    100,   // p(100) = h(100) = 100
+                ]
             ],
-            // Test eveluation when intervals are given out of order
+            // Test evaluation when intervals are given out of order
             [
                 [
                     [-2, 2],                 // g interval: [-2, 2]
@@ -105,7 +142,7 @@ class PiecewiseTest extends \PHPUnit_Framework_TestCase
                     [-1, 0], // new Polynomial([-1, 0]),      // f(x) = -x
                     [1, 0],  // new Polynomial([1, 0])        // h(x) = x
                 ],
-                0, 2       // p(0) = g(0) = 2
+                [0], [2]       // p(0) = g(0) = 2
             ],
             // Test eveluation at "jump" located at a single point
             [
@@ -121,7 +158,7 @@ class PiecewiseTest extends \PHPUnit_Framework_TestCase
                     [0],     // new Polynomial([0]),          // z(x) = 0
                     [1, 0],  // new Polynomial([1, 0])        // h(x) = x
                 ],
-                2, 0         // p(2) = z(2) = 0
+                [2], [0]         // p(2) = z(2) = 0
             ],
         ];
     }
