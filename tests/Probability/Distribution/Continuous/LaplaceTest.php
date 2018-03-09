7410be19737bf08807b71b8fef37136f99850d00
Mark Rogoyski <markrogoyski@gmail.com>
2018-03-09 02:23:27 +0000

    Refactor and add unit tests for Logistic and Laplace probability distributions.

--- tests/Probability/Distribution/Continuous/LaplaceTest.php
+++ tests/Probability/Distribution/Continuous/LaplaceTest.php
@@ -7,61 +7,61 @@ class LaplaceTest extends \PHPUnit\Framework\TestCase
 {
     /**
      * @testCase     pdf
-     * @dataProvider dataProviderForPDF
+     * @dataProvider dataProviderForPdf
      * @param        number $x
      * @param        number $μ
      * @param        number $b
      * @param        number $pdf
      */
-    public function testPDF($x, $μ, $b, $pdf)
+    public function testPdf($x, $μ, $b, $pdf)
     {
         $laplace = new Laplace($μ, $b);
-        $this->assertEquals($pdf, $laplace->pdf($x), '', 0.001);
+        $this->assertEquals($pdf, $laplace->pdf($x), '', 0.000001);
     }
 
     /**
-     * @return array
+     * @return array [x, μ, b, pdf]
      */
-    public function dataProviderForPDF(): array
+    public function dataProviderForPdf(): array
     {
         return [
-            [ 1, 0, 1, 0.1839397206 ],
-            [ 1.1, 0, 1, 0.1664355418 ],
-            [ 1.2, 0, 1, 0.150597106 ],
-            [ 5, 0, 1, 0.0033689735 ],
-            [ 1, 2, 1.4, 0.174836307 ],
-            [ 1.1, 2, 1.4, 0.1877814373 ],
-            [ 2.9, 2, 1.4, 0.1877814373 ],
+            [1, 0, 1, 0.1839397206],
+            [1.1, 0, 1, 0.1664355418],
+            [1.2, 0, 1, 0.150597106],
+            [5, 0, 1, 0.0033689735],
+            [1, 2, 1.4, 0.174836307],
+            [1.1, 2, 1.4, 0.1877814373],
+            [2.9, 2, 1.4, 0.1877814373],
         ];
     }
 
     /**
      * @testCase     cdf
-     * @dataProvider dataProviderForCDF
+     * @dataProvider dataProviderForCdf
      * @param        number $x
      * @param        number $μ
      * @param        number $b
      * @param        number $cdf
      */
-    public function testCDF($x, $μ, $b, $cdf)
+    public function testCdf($x, $μ, $b, $cdf)
     {
         $laplace = new Laplace($μ, $b);
-        $this->assertEquals($cdf, $laplace->cdf($x), '', 0.001);
+        $this->assertEquals($cdf, $laplace->cdf($x), '', 0.000001);
     }
 
     /**
-     * @return array
+     * @return array [x, μ, b, cdf]
      */
-    public function dataProviderForCDF(): array
+    public function dataProviderForCdf(): array
     {
         return [
-            [ 1, 0, 1, 0.8160602794 ],
-            [ 1.1, 0, 1, 0.8335644582 ],
-            [ 1.2, 0, 1, 0.849402894 ],
-            [ 5, 0, 1, 0.9966310265 ],
-            [ 1, 2, 1.4, 0.2447708298 ],
-            [ 1.1, 2, 1.4, 0.2628940122 ],
-            [ 2.9, 2, 1.4, 0.7371059878 ],
+            [1, 0, 1, 0.8160602794],
+            [1.1, 0, 1, 0.8335644582],
+            [1.2, 0, 1, 0.849402894],
+            [5, 0, 1, 0.9966310265],
+            [1, 2, 1.4, 0.2447708298],
+            [1.1, 2, 1.4, 0.2628940122],
+            [2.9, 2, 1.4, 0.7371059878],
         ];
     }
 
--- tests/Probability/Distribution/Continuous/LogisticTest.php
+++ tests/Probability/Distribution/Continuous/LogisticTest.php
@@ -6,17 +6,42 @@ use MathPHP\Probability\Distribution\Continuous\Logistic;
 class LogisticTest extends \PHPUnit\Framework\TestCase
 {
     /**
-     * @dataProvider dataProviderForPDF
+     * @testCase     pdf
+     * @dataProvider dataProviderForPdf
+     * @param        float $x
+     * @param        float $μ
+     * @param        float $s
+     * @param        float $pdf
      */
-    public function testPDF($x, $μ, $s, $pdf)
+    public function testPdf(float $x, float $μ, float $s, float $pdf)
     {
         $logistic = new Logistic($μ, $s);
-        $this->assertEquals($pdf, $logistic->pdf($x), '', 0.001);
+        $this->assertEquals($pdf, $logistic->pdf($x), '', 0.000001);
     }
 
-    public function dataProviderForPDF()
+    /**
+     * @return array [x, μ, s, pdf]
+     * Generated with R dlogis(x, μ, s)
+     */
+    public function dataProviderForPdf(): array
     {
         return [
+            [0, 0, 1, 0.25],
+            [1, 0, 1, 0.1966119],
+            [2, 0, 1, 0.1049936],
+            [3, 0, 1, 0.04517666],
+            [4, 0, 1, 0.01766271],
+            [5, 0, 1, 0.006648057],
+            [10, 0, 1, 4.539581e-05],
+
+            [0, 1, 1, 0.1966119],
+            [1, 1, 1, 0.25],
+            [2, 1, 1, 0.1966119],
+            [3, 1, 1, 0.1049936],
+            [4, 1, 1, 0.04517666],
+            [5, 1, 1, 0.01766271],
+            [10, 1, 1, 0.0001233793],
+
             [-5, 0, 0.7, 0.001127488648],
             [-4.2, 0, 0.7, 0.003523584702],
             [-3.5, 0, 0.7, 0.009497223815],
@@ -33,24 +58,61 @@ class LogisticTest extends \PHPUnit\Framework\TestCase
             [-3.7, 2, 1.5, 0.01426832061],
             [0, 2, 1.5, 0.1100606731],
             [3.7, 2, 1.5, 0.1228210582],
-            [5, 2, 1.5, 0.0699957236],
+            [5, 2, 1.5, 0.06999572],
         ];
     }
 
     /**
-     * @dataProvider dataProviderForCDF
+     * @testCase     cdf
+     * @dataProvider dataProviderForCdf
+     * @param        float $x
+     * @param        float $μ
+     * @param        float $s
+     * @param        float $cdf
      */
-    public function testCDF($x, $μ, $s, $cdf)
+    public function testCdf(float $x, float $μ, float $s, float $cdf)
     {
         $logistic = new Logistic($μ, $s);
-        $p = $logistic->cdf($x, $μ, $s);
-        $this->assertEquals($cdf, $p, '', 0.001);
-        $this->assertEquals($x, $logistic->inverse($p), '', 0.001);
+        $this->assertEquals($cdf, $logistic->cdf($x, $μ, $s), '', 0.000001);
     }
 
-    public function dataProviderForCDF()
+    /**
+     * @testCase     inverse of cdf is x
+     * @dataProvider dataProviderForCdf
+     * @param        float $x
+     * @param        float $μ
+     * @param        float $s
+     */
+    public function testInverse(float $x, float $μ, float $s)
+    {
+        $logistic = new Logistic($μ, $s);
+        $cdf = $logistic->cdf($x, $μ, $s);
+        $this->assertEquals($x, $logistic->inverse($cdf), '', 0.000001);
+    }
+
+    /**
+     * @return array [x, μ, s, cdf]
+     * Generated with R plogis(x, μ, s)
+     */
+    public function dataProviderForCdf(): array
     {
         return [
+            [0, 0, 1, 0.5],
+            [1, 0, 1, 0.7310586],
+            [2, 0, 1, 0.8807971],
+            [3, 0, 1, 0.9525741],
+            [4, 0, 1, 0.9820138],
+            [5, 0, 1, 0.9933071],
+            [10, 0, 1, 0.9999546],
+
+            [0, 1, 1, 0.2689414],
+            [1, 1, 1, 0.5],
+            [2, 1, 1, 0.7310586],
+            [3, 1, 1, 0.8807971],
+            [4, 1, 1, 0.9525741],
+            [5, 1, 1, 0.9820138],
+            [10, 1, 1, 0.9998766],
+
             [-4.8, 0, 0.7, 0.001050809752],
             [-3.5, 0, 0.7, 0.006692850924],
             [-3.0, 0, 0.7, 0.01357691694],
@@ -70,6 +132,9 @@ class LogisticTest extends \PHPUnit\Framework\TestCase
         ];
     }
 
+    /**
+     * @testCase mean
+     */
     public function testMean()
     {
         $μ = 5;
