07c6c5c4ee92a2ee2dda5327fed945ff2efbc7db
Kevin Nowaczyk <beakerboy99@yahoo.com>
2018-11-15 20:18:24 -0500

    Create OutlierTest.php

---
+++ tests/Statistics/OutlierTest.php
@@ -0,0 +1,36 @@
+<?php
+namespace MathPHP\Tests\Statistics;
+
+use MathPHP\Statistics\Outlier;
+
+class OutlierTest extends \PHPUnit\Framework\TestCase
+{
+
+    /**
+     * @testCase     GrubbsTest
+     */
+    public function GrubbsTest()
+    {
+        $data = [199.31, 199.53, 200.19, 200.82, 201.92, 201.95, 202.18, 245.57];
+        $G = Outlier::GrubbsStatistic($data, "upper");
+        $this->assertEquals(2.4687, $G, '', 0.0001);
+        $Gcrit = Outlier::CriticalGrubbs(.05, count($data), 1);
+        $this->assertEquals(2.032, $Gcrit, '', 0.001);
+    }
+
+    /**
+     * @testCase    TietjenMooreTest
+     */
+    public function TietjenMooreTest()
+    {
+        $data = [-1.40, -0.44, -0.30, -0.24, -0.22, -0.13, -0.05, 0.06, 0.10, 0.18, 0.20, 0.39, 0.48, 0.63, 1.01];
+        $Ek = Outlier::TietjenMooreStatistic($data, 2, "two");
+        $Eku = Outlier::TietjenMooreStatistic($data, 2, "upper");
+        $Ekl = Outlier::TietjenMooreStatistic($data, 1, "lower");
+        $this->assertEquals(.292, $Ek, '', 0.001);
+        $this->assertEquals(.634, $Eku, '', 0.001);
+        $this->assertEquals(.493, $Ekl, '', 0.001);
+        $Ekcrit = Outlier::CriticalTietjenMoore(.05, count($data), 2, "two");
+        $this->assertEquals(.315, $Ekcrit, '', 0.005);
+    }
+}
