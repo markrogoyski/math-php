c518badaee2a9a4e70bcbbaca27406efcff26eec
Kevin Nowaczyk <beakerboy99@yahoo.com>
2018-12-13 11:53:15 -0500

    Update TheilSen.php

--- src/Statistics/Regression/TheilSen.php
+++ src/Statistics/Regression/TheilSen.php
@@ -38,9 +38,9 @@ class TheilSen extends ParametricRegression
         $n      = count($this->points);
         for ($i = 0; $i < $n; $i++) {
             for ($j = $i + 1; $j < $n; $j++) {
+                $pointi   = $this->points[$i];
+                $pointj   = $this->points[$j];
                 if ($pointj[0] != $pointi[0]) {
-                    $pointi   = $this->points[$i];
-                    $pointj   = $this->points[$j];
                     $slopes[] = ($pointj[1] - $pointi[1]) / ($pointj[0] - $pointi[0]);
                 }
             }
