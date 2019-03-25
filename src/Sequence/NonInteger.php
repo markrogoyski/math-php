b9952e0d2d51180f529d3f17fa3042fa4e657f5e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-03-25 11:42:30 -0400

    Harmonic Number Sequence

---
+++ src/Sequence/NonInteger.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace MathPHP\Sequence;
+
+use MathPHP\Exception\OutOfBoundsException;
+use MathPHP\NumberTheory\Integer;
+
+/**
+ * Non-integer sequences
+ *  - Harmonic
+ *
+ * All sequences return an array of numbers in the sequence.
+ * The array index starting point depends on the sequence type.
+ */
+class NonInteger
+{
+
+    /**
+     * Harmonic Numbers
+     *
+     *       n
+     * Hn = sum(1/n)
+     *      k=1
+     *
+     * https://en.wikipedia.org/wiki/Harmonic_number
+     */
+    public static function Harmonic(int $n)
+    {
+        $result = [];
+        $sum = 0;
+        for($i = 1; $i < $n; $i++) {
+            $sum += 1 / $i;
+            $result[] = $sum; 
+        }
+        return $result;
+    }
+}
