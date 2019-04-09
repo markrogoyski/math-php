c153dba7c0ea230e9529b3d6dcd65ef361729c98
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-04-09 11:05:55 -0400

    Update NonInteger.php

--- src/Sequence/NonInteger.php
+++ src/Sequence/NonInteger.php
@@ -2,7 +2,7 @@
 
 namespace MathPHP\Sequence;
 
-use MathPHP\Exception\OutOfBoundsException;
+use MathPHP\Exception\BadParameterException;
 use MathPHP\NumberTheory\Integer;
 
 /**
@@ -26,13 +26,27 @@ class NonInteger
      */
     public static function Harmonic(int $n)
     {
+        return self::Hyperharmonic($n, 1);
+    }
+
+    /**
+     * Hyperharmonic Numbers
+     *
+     *       n
+     * Hᵢₛ = sum(1/iˢ)
+     *      i=1
+     *
+     * https://en.wikipedia.org/wiki/Harmonic_series_(mathematics)#p-series
+     */
+    public static function Hyperharmonic(int $n, int $s)
+    {
         if ($n < 1) {
             throw new Exception\BadParameterException('n must be 1 or greater');
         }
         $result = [];
         $sum = 0;
         for ($i = 1; $i < $n; $i++) {
-            $sum += 1 / $i;
+            $sum += 1 / $i ** $s;
             $result[$i] = $sum;
         }
         return $result;
