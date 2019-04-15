b673685ed3b4667b7859d3ada6b1c053fda0e787
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-04-15 08:49:08 -0400

    Create Base Class

---
+++ src/LinearAlgebra/MatrixBase.php
@@ -0,0 +1,180 @@
+<?php
+
+namespace MathPHP\LinearAlgebra;
+
+use MathPHP\Exception;
+
+/**
+ * m x n Matrix
+ */
+abstract class Matrix implements \ArrayAccess, \JsonSerializable
+{
+    /** @var int Number of rows */
+    protected $m;
+    
+    /** @var int Number of columns */
+    protected $n;
+    
+    /** @var array Matrix array of arrays */
+    protected $A;
+
+    /**************************************************************************
+     * BASIC MATRIX GETTERS
+     *  - getMatrix
+     *  - getM
+     *  - getN
+     *  - getRow
+     *  - getColumn
+     *  - get
+     *  - getDiagonalElements
+     **************************************************************************/
+ 
+    /**
+     * Get matrix
+     * @return array of arrays
+     */
+    public function getMatrix(): array
+    {
+        return $this->A;
+    }
+
+    /**
+     * Get row count (m)
+     * @return int number of rows
+     */
+    public function getM(): int
+    {
+        return $this->m;
+    }
+    /**
+     * Get column count (n)
+     * @return int number of columns
+     */
+    public function getN(): int
+    {
+        return $this->n;
+    }
+
+    /**
+     * Get single row from the matrix
+     *
+     * @param  int    $i row index (from 0 to m - 1)
+     * @return array
+     *
+     * @throws Exception\MatrixException if row i does not exist
+     */
+    public function getRow(int $i): array
+    {
+        if ($i >= $this->m) {
+            throw new Exception\MatrixException("Row $i does not exist");
+        }
+        return $this->A[$i];
+    }
+
+    /**
+     * Get single column from the matrix
+     *
+     * @param  int   $j column index (from 0 to n - 1)
+     * @return array
+     *
+     * @throws Exception\MatrixException if column j does not exist
+     */
+    public function getColumn(int $j): array
+    {
+        if ($j >= $this->n) {
+            throw new Exception\MatrixException("Column $j does not exist");
+        }
+        return array_column($this->A, $j);
+    }
+
+    /**
+     * Get a specific value at row i, column j
+     *
+     * @param  int    $i row index
+     * @param  int    $j column index
+     * @return number
+     *
+     * @throws Exception\MatrixException if row i or column j does not exist
+     */
+    public function get(int $i, int $j)
+    {
+        if ($i >= $this->m) {
+            throw new Exception\MatrixException("Row $i does not exist");
+        }
+        if ($j >= $this->n) {
+            throw new Exception\MatrixException("Column $j does not exist");
+        }
+        return $this->A[$i][$j];
+    }
+
+    /**
+     * Returns the elements on the diagonal of a square matrix as an array
+     *     [1 2 3]
+     * A = [4 5 6]
+     *     [7 8 9]
+     *
+     * getDiagonalElements($A) = [1, 5, 9]
+     *
+     * @return array
+     */
+    public function getDiagonalElements(): array
+    {
+        $diagonal = [];
+        for ($i = 0; $i < min($this->m, $this->n); $i++) {
+            $diagonal[] = $this->A[$i][$i];
+        }
+        return $diagonal;
+    }
+    
+    /**************************************************************************
+     * ArrayAccess INTERFACE
+     **************************************************************************/
+     
+    /**
+     * @param mixed $i
+     * @return bool
+     */
+    public function offsetExists($i): bool
+    {
+        return isset($this->A[$i]);
+    }
+    
+    /**
+     * @param mixed $i
+     * @return mixed
+     */
+    public function offsetGet($i)
+    {
+        return $this->A[$i];
+    }
+    
+    /**
+     * @param  mixed $i
+     * @param  mixed $value
+     * @throws Exception\MatrixException
+     */
+    public function offsetSet($i, $value)
+    {
+        throw new Exception\MatrixException('Matrix class does not allow setting values');
+    }
+    
+    /**
+     * @param  mixed $i
+     * @throws Exception\MatrixException
+     */
+    public function offsetUnset($i)
+    {
+        throw new Exception\MatrixException('Matrix class does not allow unsetting values');
+    }
+    
+    /**************************************************************************
+     * JsonSerializable INTERFACE
+     **************************************************************************/
+    /**
+     * @return array
+     */
+    public function jsonSerialize()
+    {
+        return $this->A;
+    }
+}
