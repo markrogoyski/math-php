f11d16cba7c20cf83d5b9e96787fb1cc5f1b10cc
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-08-27 19:59:01 -0400

    Update PCA.php

--- src/Statistics/Multivariate/PCA.php
+++ src/Statistics/Multivariate/PCA.php
@@ -70,7 +70,7 @@ class PCA
             }
             $this->scale = new Vector($scaleArray);
         } else {
-            $this->scale = new Vector(array_fill(1, $this->data->getN()));
+            $this->scale = new Vector(array_fill(1, $this->data->getN(), 1));
         }
         // Save the source data to the class
         $this->data = $M;
