6b6ddd01c6a9e5496ba3746b2a76393d59c39c84
Mark Rogoyski <markrogoyski@gmail.com>
2019-09-04 23:47:14 -0700

    Change unit test assertion.

--- tests/LinearAlgebra/VectorInterfaceTest.php
+++ tests/LinearAlgebra/VectorInterfaceTest.php
@@ -155,7 +155,7 @@ class VectorInterfaceTest extends \PHPUnit\Framework\TestCase
         // When
         foreach ($this->V as $element) {
             // Then
-            $this->assertIsInt($element);
+            $this->assertTrue(is_int($element));
         }
 
         // When Rewinding
