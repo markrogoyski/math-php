06c49dd4dbac91ac41e2b18cf7de6f7af11ce6c3
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-09-25 16:33:13 -0400

    Arbitrary integer coverage (#95)
    * Added Coverage

--- tests/Number/ArbitraryIntegerTest.php
+++ tests/Number/ArbitraryIntegerTest.php
@@ -68,6 +68,25 @@ class ArbitraryIntegerTest extends \PHPUnit\Framework\TestCase
     }
 
     /**
+     * @dataProvider dataProviderForToBase
+     */
+    public function testToBase(string $number, int $base, $alphabet, string $expected)
+    {
+        $obj = new ArbitraryInteger($number);
+
+        $this->assertSame($expected, $obj->toBase($base, $alphabet));
+    }
+
+    public function dataProviderForToBase()
+    {
+        return [
+            ['200', 10, null, '200'],
+            ['123456', 10, 'ABCDEFGHIJ', 'BCDEFG'],
+            ['123456', 64, ArbitraryInteger::RFC3548_BASE64_FILE_SAFE, 'eJA'],
+        ];
+    }
+
+    /**
      * @test         Constructor determines number base from the alphabet
      * @dataProvider dataProviderForTestGetBaseFromAlphabet
      */
