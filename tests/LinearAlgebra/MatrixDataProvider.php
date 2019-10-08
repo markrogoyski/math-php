036ba57e0cff1ce3e95c4b1e250a3d88e212f95e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-10-08 19:48:06 -0400

    Update MatrixDataProvider.php

--- tests/LinearAlgebra/MatrixDataProvider.php
+++ tests/LinearAlgebra/MatrixDataProvider.php
@@ -5116,6 +5116,34 @@ trait MatrixDataProvider
         ];
     }
 
+    public function dataProviderForNotNilpotentMatrix(): array
+    {
+        return [
+            [
+                [
+                    [0, 1],
+                    [1, 0],
+                ],
+            ],
+            [
+                [
+                    [1, 2, 3],
+                    [4, 5, 6],
+                    [7, 8, 9],
+                ],
+            ],
+            [
+                [
+                    [2, 2, 2, 2, -4],
+                    [7, 1, 1, 1, -5],
+                    [1, 7, 1, 1, -5],
+                    [1, 1, 7, 1, -5],
+                    [0, 1, 1, 7, -5],
+                ],
+            ],
+        ];
+    }
+
     public function dataProviderForInvolutoryMatrix(): array
     {
         return [
