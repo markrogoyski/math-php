a668349b3fbaf8e9c9ddbabd8cc5a45d7039000d
Mark Rogoyski <markrogoyski@gmail.com>
2019-11-04 23:21:58 -0800

    Minor improvements to ArbitraryInteger.

--- src/Functions/ArbitraryInteger.php
+++ src/Functions/ArbitraryInteger.php
@@ -3,46 +3,32 @@
 namespace MathPHP\Functions;
 
 use MathPHP\Exception;
-use MathPHP\Number\ArbitraryInteger as Integer;
+use MathPHP\Number;
 
 /**
  * Functions that operate with ArbitraryInteger objects
  */
 class ArbitraryInteger
 {
-    
-    /**
-     * Prepare input value for construction
-     *
-     * @param  int|string|ArbitraryInteger $number
-     * @return ArbitraryInteger
-     */
-    private static function prepareParameter($number): Integer
-    {
-        if (!is_object($number)) {
-            return new Integer($number);
-        }
-        $class = get_class($number);
-        if ($class == Integer::class) {
-            return $number;
-        }
-        throw new Exception\IncorrectTypeException("Class of type $class is not supported.");
-    }
-
     /**
      * Ackermann Function
      * A well known highly recursive function which produces very large numbers
      *
      * https://en.wikipedia.org/wiki/Ackermann_function
      *
-     * @param $m
-     * @param $n
-     * @return ArbitraryInteger
+     * @param int|string|Number\ArbitraryInteger $m
+     * @param int|string|Number\ArbitraryInteger $n
+     *
+     * @return Number\ArbitraryInteger
+     *
+     * @throws Exception\BadParameterException
+     * @throws Exception\IncorrectTypeException
      */
-    public static function ackermann($m, $n): Integer
+    public static function ackermann($m, $n): Number\ArbitraryInteger
     {
         $m = self::prepareParameter($m);
         $n = self::prepareParameter($n);
+
         if ($m->equals(0)) {
             return $n->add(1);
         } elseif ($m->equals(1)) {
@@ -50,7 +36,7 @@ class ArbitraryInteger
         } elseif ($m->equals(2)) {
             return $n->leftShift(1)->add(3);
         } elseif ($m->equals(3)) {
-            $one = new Integer(1);
+            $one = new Number\ArbitraryInteger(1);
             // 2^(n+3) - 3
             return $one->leftShift($n->add(3))->subtract(3);
         } elseif ($n->equals(0)) {
@@ -64,13 +50,42 @@ class ArbitraryInteger
      * Create a random ArbitraryInteger
      *
      * @param int $bytes
-     * @return ArbitraryInteger
+     * 
+     * @return Number\ArbitraryInteger
+     *
+     * @throws Exception\BadParameterException
+     * @throws Exception\IncorrectTypeException
      */
-    public static function rand(int $bytes): Integer
+    public static function rand(int $bytes): Number\ArbitraryInteger
     {
         if ($bytes <= 0) {
             throw new Exception\BadParameterException('Cannot produce a random number with zero or negative bytes.');
         }
-        return Integer::fromBinary(random_bytes($bytes), mt_rand(0, 1) === 0);
+
+        return Number\ArbitraryInteger::fromBinary(random_bytes($bytes), mt_rand(0, 1) === 0);
+    }
+
+    /**
+     * Prepare input value for construction
+     *
+     * @param  int|string|Number\ArbitraryInteger $number
+     *
+     * @return Number\ArbitraryInteger
+     *
+     * @throws Exception\BadParameterException
+     * @throws Exception\IncorrectTypeException
+     */
+    private static function prepareParameter($number): Number\ArbitraryInteger
+    {
+        if (!is_object($number)) {
+            return new Number\ArbitraryInteger($number);
+        }
+
+        $class = get_class($number);
+        if ($class == Number\ArbitraryInteger::class) {
+            return $number;
+        }
+
+        throw new Exception\IncorrectTypeException("Class of type $class is not supported.");
     }
 }
