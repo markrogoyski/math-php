1d76c77c2b54ff846b1f1b2d8e75583e2cb8c0bd
Mark Rogoyski <markrogoyski@gmail.com>
2019-11-14 08:08:44 -0800

    Unit test improvements.

--- tests/Number/ArbitraryIntegerTest.php
+++ tests/Number/ArbitraryIntegerTest.php
@@ -276,25 +276,58 @@ class ArbitraryIntegerTest extends \PHPUnit\Framework\TestCase
     }
 
     /**
-     * @test         pow() returns the proper result
+     * @test         pow()
      * @dataProvider dataProviderForPow
-     * @param        number $int
+     * @param        int $int
+     * @param        int $exponent
      * @param        string $expected
+     * @throws       \Exception
      */
-    public function testPow($int, $exponent, $expected)
+    public function testPow(int $int, int $exponent, string $expected)
     {
+        // Given
         $int =  new ArbitraryInteger($int);
+
+        // When
         $pow = $int->pow($exponent);
+
+        // Then
         $this->assertSame($expected, (string) $pow);
     }
 
-    public function dataProviderForPow()
+    public function dataProviderForPow(): array
     {
         return [
+            [1, 0, '1'],
+            [1, 1, '1'],
+            [1, 2, '1'],
+            [2, 0, '1'],
+            [2, 1, '2'],
+            [2, 2, '4'],
             [
                 1000000,
                 0,
                 '1',
+            ],
+            [
+                1000000,
+                1,
+                '1000000',
+            ],
+            [
+                1000000,
+                2,
+                '1000000000000',
+            ],
+            [
+                1000000000,
+                0,
+                '1',
+            ],
+            [
+                1000000000,
+                1,
+                '1000000000',
             ],
             [
                 1000000000,
@@ -309,60 +342,95 @@ class ArbitraryIntegerTest extends \PHPUnit\Framework\TestCase
      * @dataProvider dataProviderForAbs
      * @param        string $int
      * @param        string $expected
+     * @throws       \Exception
      */
     public function testAbs(string $int, string $expected)
     {
-        $abs = new ArbitraryInteger($int);
-        $this->assertEquals($expected, (string) $abs->abs());
+        // Given
+        $int = new ArbitraryInteger($int);
+
+        // When
+        $abs = $int->abs();
+
+        // Then
+        $this->assertEquals($expected, (string) $abs);
     }
 
-    public function dataProviderForAbs()
+    public function dataProviderForAbs(): array
     {
         return [
+            [0, '0'],
+            [1, '1'],
+            [-1, '1'],
             ['-12345678910', '12345678910'],
+            ['12345678910', '12345678910'],
+            ['-798273948792837498273948289', '798273948792837498273948289'],
         ];
     }
 
     /**
-     * @test         negate() returns the proper result
+     * @test         negate()
      * @dataProvider dataProviderForNegate
      * @param        string $int
      * @param        string $expected
+     * @throws       \Exception
      */
     public function testNegate(string $int, string $expected)
     {
-        $neg = new ArbitraryInteger($int);
-        $this->assertEquals($expected, (string) $neg->negate());
+        // Given
+        $int = new ArbitraryInteger($int);
+
+        // When
+        $neg = $int->negate();
+
+        // Then
+        $this->assertEquals($expected, (string) $neg);
     }
 
-    public function dataProviderForNegate()
+    public function dataProviderForNegate(): array
     {
         return [
             ['-123456789101112', '123456789101112'],
             ['123456789101112', '-123456789101112'],
             ['0', '0'],
+            ['1', '-1'],
+            ['-1', '1'],
         ];
     }
 
     /**
-     * @test         fact() returns the proper result
+     * @test         fact()
      * @dataProvider dataProviderForFact
-     * @param        number $int
+     * @param        int    $int
      * @param        string $expected
+     * @throws       \Exception
      */
-    public function testFact($int, $expected)
+    public function testFact(int $int, string $expected)
     {
-        $int = new ArbitraryInteger($int);
+        // Given
+        $int         = new ArbitraryInteger($int);
+        $expectedInt = new ArbitraryInteger($expected);
+
+        // When
         $fact = $int->fact();
-        $expected_obj = new ArbitraryInteger($expected);
-        $this->assertEquals($expected_obj->toBinary(), $fact->toBinary());
-        $this->assertTrue($expected_obj->equals($fact));
+
+        // Then
+        $this->assertEquals($expectedInt->toBinary(), $fact->toBinary());
+        $this->assertTrue($expectedInt->equals($fact));
         $this->assertSame($expected, (string) $fact);
     }
 
-    public function dataProviderForFact()
+    public function dataProviderForFact(): array
     {
         return [
+            [-1, '1'],
+            [0, '1'],
+            [1, '1'],
+            [2, '2'],
+            [3, '6'],
+            [4, '24'],
+            [5, '120'],
+            [20, '2432902008176640000'],
             [
                 1000,
                 '402387260077093773543702433923003985719374864210714632543799910429938512398629020592044208486969404800479988610197196058631666872994808558901323829669944590997424504087073759918823627727188732519779505950995276120874975462497043601418278094646496291056393887437886487337119181045825783647849977012476632889835955735432513185323958463075557409114262417474349347553428646576611667797396668820291207379143853719588249808126867838374559731746136085379534524221586593201928090878297308431392844403281231558611036976801357304216168747609675871348312025478589320767169132448426236131412508780208000261683151027341827977704784635868170164365024153691398281264810213092761244896359928705114964975419909342221566832572080821333186116811553615836546984046708975602900950537616475847728421889679646244945160765353408198901385442487984959953319101723355556602139450399736280750137837615307127761926849034352625200015888535147331611702103968175921510907788019393178114194545257223865541461062892187960223838971476088506276862967146674697562911234082439208160153780889893964518263243671616762179168909779911903754031274622289988005195444414282012187361745992642956581746628302955570299024324153181617210465832036786906117260158783520751516284225540265170483304226143974286933061690897968482590125458327168226458066526769958652682272807075781391858178889652208164348344825993266043367660176999612831860788386150279465955131156552036093988180612138558600301435694527224206344631797460594682573103790084024432438465657245014402821885252470935190620929023136493273497565513958720559654228749774011413346962715422845862377387538230483865688976461927383814900140767310446640259899490222221765904339901886018566526485061799702356193897017860040811889729918311021171229845901641921068884387121855646124960798722908519296819372388642614839657382291123125024186649353143970137428531926649875337218940694281434118520158014123344828015051399694290153483077644569099073152433278288269864602789864321139083506217095002597389863554277196742822248757586765752344220207573630569498825087968928162753848863396909959826280956121450994871701244516461260379029309120889086942028510640182154399457156805941872748998094254742173582401063677404595741785160829230135358081840096996372524230560855903700624271243416909004153690105933983835777939410970027753472000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000'
@@ -371,20 +439,34 @@ class ArbitraryIntegerTest extends \PHPUnit\Framework\TestCase
     }
 
     /**
-     * @test         isqrt calculates the correct value
+     * @test         isqrt
      * @dataProvider dataProviderForIsqrt
      * @param        string $number
      * @param        string $expected
+     * @throws       \Exception
      */
     public function testIsqrt(string $number, string $expected)
     {
+        // Given
         $obj = new ArbitraryInteger($number);
-        $this->assertEquals($expected, (string) $obj->isqrt());
+
+        // When
+        $isqrt = $obj->isqrt();
+
+        // Then
+        $this->assertEquals($expected, (string) $isqrt);
     }
 
-    public function dataProviderForIsqrt()
+    public function dataProviderForIsqrt(): array
     {
         return [
+            ['1', '1'],
+            ['2', '1'],
+            ['3', '1'],
+            ['4', '2'],
+            ['5', '2'],
+            ['6', '2'],
+            ['7', '2'],
             ['8', '2'],
             ['9', '3'],
             ['10', '3'],
@@ -399,22 +481,30 @@ class ArbitraryIntegerTest extends \PHPUnit\Framework\TestCase
     }
 
     /**
-     * @test Test that greaterThan () works as it should
+     * @test         greaterThan
      * @dataProvider dataProviderForTestGreaterThan
+     * @param        string $int1
+     * @param        string $int2
+     * @param        bool $expected
+     * @throws       \Exception
      */
     public function testGreaterThan(string $int1, string $int2, bool $expected)
     {
+        // Given
         $int1 = new ArbitraryInteger($int1);
         $int2 = new ArbitraryInteger($int2);
+
+        // Then
         $this->assertEquals($expected, $int1->greaterThan($int2));
     }
 
-    public function dataProviderForTestGreaterThan()
+    public function dataProviderForTestGreaterThan(): array
     {
         return [
             ['0', '-1', true],
             ['-1', '0', false],
-            ['0', '0', false],
+            ['2432902008176640000', '123456789101112', true],
+            ['123456789101112', '2432902008176640000', false]
         ];
     }
 
