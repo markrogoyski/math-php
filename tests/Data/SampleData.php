d61dc3c6a85c87102364b3699c64ed308a33596f
Mark Rogoyski <markrogoyski@gmail.com>
2019-12-09 19:23:07 -0800

    Minor correction.

--- tests/Data/SampleData.php
+++ tests/Data/SampleData.php
@@ -5,7 +5,7 @@ namespace MathPHP\Tests\Data;
 class SampleData
 {
     /**
-     * mtcards dataset (Motor Trend Car Road Tests)
+     * mtcars dataset (Motor Trend Car Road Tests)
      *
      * The data was extracted from the 1974 Motor Trend US magazine, and comprises fuel consumption and 10 aspects of
      * automobile design and performance for 32 automobiles (1973–74 models).
