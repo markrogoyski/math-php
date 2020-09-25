d9b0fee757a65fbbfe1df703263cca491e334a01
Kevin Nowaczyk <beakerboy99@yahoo.com>
2020-09-25 11:53:35 -0400

    Update PLS.php

--- src/Statistics/Multivariate/PLS.php
+++ src/Statistics/Multivariate/PLS.php
@@ -180,7 +180,7 @@ class PLS
      */
     public function predict(Matrix $X)
     {
-        if ($X->getN() !== $this->Xcenter->getN();) {
+        if ($X->getN() !== $this->Xcenter->getN()) {
             throw new Exception\BadDataException("Data does not have the same number of columns. Expecting {$expecting}, given $given");
         }
         
