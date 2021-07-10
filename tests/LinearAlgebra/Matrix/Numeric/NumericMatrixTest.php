3d69be64268e6cb503ca2be2cf1b8b39690b6ac7
Mark Rogoyski <markrogoyski@gmail.com>
2021-07-10 15:44:48 -0700

    t Add unit test for Matrix string representation.

--- tests/LinearAlgebra/Matrix/Numeric/NumericMatrixTest.php
+++ tests/LinearAlgebra/Matrix/Numeric/NumericMatrixTest.php
@@ -7,7 +7,7 @@ use MathPHP\LinearAlgebra\NumericMatrix;
 class NumericMatrixTest extends \PHPUnit\Framework\TestCase
 {
     /**
-     * @test   Object type of numeric matrix
+     * @test Object type of numeric matrix
      */
     public function testGetObjectType()
     {
@@ -23,5 +23,27 @@ class NumericMatrixTest extends \PHPUnit\Framework\TestCase
 
         // Then
         $this->assertSame('number', $objectType);
+    }
+
+    /**
+     * @test string representation
+     */
+    public function testGetStringRepresentation()
+    {
+        // Given
+        $A = new NumericMatrix([
+            [1, 2, 3],
+            [2, 3, 4],
+            [3, 4, 5],
+        ]);
+
+        // And
+        $expected = "[1, 2, 3]\n[2, 3, 4]\n[3, 4, 5]";
+
+        // When
+        $stringRepresentation = (string) $A;
+
+        // Then
+        $this->assertEquals($expected, $stringRepresentation);
     }
 }
