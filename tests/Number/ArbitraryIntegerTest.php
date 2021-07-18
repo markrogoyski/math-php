a598f1902cc9f2bf4070adacdc56be84ec4399e8
Kevin Nowaczyk <beakerboy99@yahoo.com>
2021-07-18 19:19:08 -0400

    Update ArbitraryIntegerTest.php

--- tests/Number/ArbitraryIntegerTest.php
+++ tests/Number/ArbitraryIntegerTest.php
@@ -612,6 +612,8 @@ class ArbitraryIntegerTest extends \PHPUnit\Framework\TestCase
         return [
             [1, 0, '1'],
             [1, 1, '1'],
+            [1, -1, '1'],
+            [-1, -1, '-1'],
             [1, 2, '1'],
             [2, 0, '1'],
             [2, 1, '2'],
