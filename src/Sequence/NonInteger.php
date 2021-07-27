0634528d2839a43a5f932c8175378cddd5014784
Kevin Nowaczyk <beakerboy99@yahoo.com>
2021-07-26 20:30:47 -0400

    Develop (#159)

--- src/Sequence/NonInteger.php
+++ src/Sequence/NonInteger.php
@@ -2,9 +2,13 @@
 
 namespace MathPHP\Sequence;
 
+use MathPHP\Exception;
+use MathPHP\Number\Rational;
+
 /**
  * Non-integer sequences
  *  - Harmonic
+ *  - Generalized Harmonic
  *  - Hyperharmonic
  *
  * All sequences return an array of numbers in the sequence.
@@ -23,29 +27,28 @@ class NonInteger
      *
      * @param int $n the length of the sequence to calculate
      *
-     * @return array
+     * @return float[]
      */
-    public static function Harmonic(int $n): array
+    public static function harmonic(int $n): array
     {
-        return self::Hyperharmonic($n, 1);
+        return self::generalizedHarmonic($n, 1);
     }
 
     /**
-     * Hyperharmonic Numbers (p-series)
+     * Generalized Harmonic Numbers
      *
-     *       n  1
-     * Hᵢp = ∑  --
-     *      ⁱ⁼¹ iᵖ
+     *       ₙ  1
+     * Hₙₘ = ∑  --
+     *      ⁱ⁼¹ iᵐ
      *
-     * https://en.wikipedia.org/wiki/Harmonic_series_(mathematics)#p-series
-     * https://en.wikipedia.org/wiki/Hyperharmonic_number
+     * https://en.wikipedia.org/wiki/Harmonic_number#Generalized_harmonic_numbers
      *
      * @param int   $n the length of the sequence to calculate
-     * @param float $p the exponent
+     * @param float $m the exponent
      *
-     * @return array
+     * @return float[]
      */
-    public static function Hyperharmonic(int $n, float $p): array
+    public static function generalizedHarmonic(int $n, float $m): array
     {
         if ($n <= 0) {
             return [];
@@ -55,10 +58,65 @@ class NonInteger
         $∑        = 0;
 
         for ($i = 1; $i <= $n; $i++) {
-            $∑ += 1 / $i ** $p;
+            $∑ += 1 / $i ** $m;
             $sequence[$i] = $∑;
         }
 
         return $sequence;
+    }
+
+    /**
+     * Hyperharmonic Numbers
+     *
+     *         ₙ
+     * Hₙ⁽ʳ⁾ = ∑  Hₖ⁽ʳ⁻¹⁾
+     *        ᵏ⁼¹
+     *
+     * https://en.wikipedia.org/wiki/Hyperharmonic_number
+     *
+     * @param int  $n the length of the sequence to calculate
+     * @param int  $r the depth of recursion
+     * @param bool $rational return results as a Rational object
+     *
+     * @return float[]|Rational[]
+     */
+    public static function hyperharmonic(int $n, int $r, $rational = false): array
+    {
+        if ($r < 0) {
+            throw new Exception\OutOfBoundsException('Recursion depth cannot be less than 0');
+        }
+        if ($n <= 0) {
+            return [];
+        }
+        $sequence = [];
+
+        try {
+            if ($r == 0) {
+                for ($k = 1; $k <= $n; $k++) {
+                    $sequence[$k] = new Rational(0, 1, $k);
+                }
+            } else {
+                /** @var Rational[] $array */
+                $array = self::hyperharmonic($n, $r - 1, true);
+                $∑     = Rational::createZeroValue();
+                for ($k = 1; $k <= $n; $k++) {
+                    $∑ = $∑->add($array[$k]);
+                    $sequence[$k] = $∑;
+                }
+            }
+        } catch (\TypeError $e) {
+            throw new Exception\OutOfBoundsException('Numbers too large to maintain integer precision', -1, $e);
+        }
+
+        if ($rational == true) {
+            return $sequence;
+        }
+
+        return array_map(
+            function (Rational $r) {
+                return $r->toFloat();
+            },
+            $sequence
+        );
     }
 }
--- tests/Sequence/NonIntegerTest.php
+++ tests/Sequence/NonIntegerTest.php
@@ -2,6 +2,7 @@
 
 namespace MathPHP\Tests\Sequence;
 
+use MathPHP\Exception;
 use MathPHP\Sequence\NonInteger;
 
 class NonIntegerTest extends \PHPUnit\Framework\TestCase
@@ -15,7 +16,7 @@ class NonIntegerTest extends \PHPUnit\Framework\TestCase
     public function testHarmonicNumbers(int $n, array $expectedSequence)
     {
         // When
-        $harmonicSequence = NonInteger::Harmonic($n);
+        $harmonicSequence = NonInteger::harmonic($n);
 
         // Then
         $this->assertEquals($expectedSequence, $harmonicSequence);
@@ -35,16 +36,16 @@ class NonIntegerTest extends \PHPUnit\Framework\TestCase
     }
 
     /**
-     * @test         Hyperharmonic produces the expected sequence
-     * @dataProvider dataProviderForHyperharmonicNumbers
+     * @test         generalizedHarmonic produces the expected sequence
+     * @dataProvider dataProviderForGeneralizedHarmonicNumbers
      * @param        int   $n
      * @param        float $p
      * @param        array $expectedSequence
      */
-    public function testHyperharmonicNumbers(int $n, float $p, array $expectedSequence)
+    public function testGeneralizedHarmonicNumbers(int $n, float $p, array $expectedSequence)
     {
         // When
-        $harmonicSequence = NonInteger::Hyperharmonic($n, $p);
+        $harmonicSequence = NonInteger::generalizedHarmonic($n, $p);
 
         // Then
         $this->assertEquals($expectedSequence, $harmonicSequence);
@@ -53,7 +54,7 @@ class NonIntegerTest extends \PHPUnit\Framework\TestCase
     /**
      * @return array
      */
-    public function dataProviderForHyperharmonicNumbers(): array
+    public function dataProviderForGeneralizedHarmonicNumbers(): array
     {
         return [
             [-1, 2, []],
@@ -62,5 +63,67 @@ class NonIntegerTest extends \PHPUnit\Framework\TestCase
             [4, 2, [1 => 1, 5 / 4, 49 / 36, 205 / 144]],
             [3, 1.01, [1 => 1, 1.4965462477, 1.8262375824425]],
         ];
+    }
+
+    /**
+     * @test         HyperharmonicNumber produces the expected sequence
+     * @dataProvider dataProviderForHyperharmonicNumbers
+     * @param        int   $n
+     * @param        int   $r
+     * @param        array $expectedSequence
+     */
+    public function testHyperharmonicNumbers(int $n, int $r, array $expectedSequence)
+    {
+        // When
+        $hyperharmonicSequence = NonInteger::hyperharmonic($n, $r);
+
+        // Then
+        $this->assertEqualsWithDelta($expectedSequence, $hyperharmonicSequence, .0001);
+    }
+
+    /**
+     * @return array
+     */
+    public function dataProviderForHyperharmonicNumbers(): array
+    {
+        return [
+            [-1, 5, []],
+            [0, 2, []],
+            [10, 0, [1 => 1, 1/2, 1/3, 1/4, 1/5, 1/6, 1/7, 1/8, 1/9, 1/10]],
+            [10, 1, [1 => 1, 3/2, 11/6, 25/12, 137/60, 49/20, 363/140, 761/280, 7129/2520, 7381/2520]],
+            [10, 2, [1 => 1, 5/2, 26/6, 77/12, 87/10, 223/20, 481/35, 4609/280, 4861/252, 55991/2520]],
+        ];
+    }
+
+    /**
+     * @test hyperharmonic throws a OutOfBoundsException when r is less than zero
+     */
+    public function testHyperharmonicSeriesException()
+    {
+        // Given
+        $r = -1;
+
+        // Then
+        $this->expectException(Exception\OutOfBoundsException::class);
+
+        // When
+        NonInteger::hyperharmonic(10, $r);
+    }
+
+    /**
+     * @test hyperharmonic arithmetic operations exceed the bounds of the max integer precision
+     *       and produce a controlled MathException rather than a TypeError.
+     */
+    public function testHyperharmonicSequenceTypeError()
+    {
+        // Given
+        $n = 10000;
+        $r = 10000;
+
+        // Then
+        $this->expectException(Exception\OutOfBoundsException::class);
+
+        // When
+        NonInteger::hyperharmonic($n, $r);
     }
 }
